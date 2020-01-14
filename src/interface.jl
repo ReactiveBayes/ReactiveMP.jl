b9dd3daedd413504e49c8eb3d6739b90e003d654
Dmitri Bagaev <bvdmitri@gmail.com>
2020-01-14 18:23:33 +0100

    demo: Demos updated

--- src/interface.jl
+++ src/interface.jl
@@ -4,35 +4,47 @@ export AbstractInterface, InterfaceIn, InterfaceOut
 
 abstract type AbstractInterface end
 
-define_joint!(interface::I, observable) where { I <: AbstractInterface }       = set!(interface.joint_message, observable)
-define_sum_product!(interface::I, observable) where { I <: AbstractInterface } = set!(interface.sum_product_message, observable)
+define_joint!(interface::I, observable) where { I <: AbstractInterface }       = set!(interface.lazy_jm, observable)
+define_sum_product!(interface::I, observable) where { I <: AbstractInterface } = set!(interface.lazy_sp, observable)
 
 sum_product(interface::I) where { I <: AbstractInterface } = interface.sum_product_message
 
 struct InterfaceIn{S, J} <: AbstractInterface
     name :: String
 
-    sum_product_message :: LazyObservable{S}
-    joint_message       :: LazyObservable{J}
+    lazy_sp :: LazyObservable{S}
+    lazy_jm :: LazyObservable{J}
+
+    sum_product_message
+    joint_message
 
     InterfaceIn{S, J}(name::String) where { S <: AbstractMessage } where { J <: AbstractMessage } = begin
-        sum_product_message = LazyObservable{S}()
-        joint_message       = LazyObservable{J}()
+        lazy_sp = LazyObservable{S}()
+        lazy_jm = LazyObservable{J}()
 
-        return new(name, sum_product_message, joint_message)
+        sum_product_message = lazy_sp# |> replay(1) |> take(1)
+        joint_message       = lazy_jm# |> replay(1) |> take(1)
+
+        return new(name, lazy_sp, lazy_jm, sum_product_message, joint_message)
     end
 end
 
 struct InterfaceOut{S, J} <: AbstractInterface
     name :: String
 
-    sum_product_message :: LazyObservable{S}
-    joint_message       :: LazyObservable{J}
+    lazy_sp :: LazyObservable{S}
+    lazy_jm :: LazyObservable{J}
+
+    sum_product_message
+    joint_message
 
     InterfaceOut{S, J}(name::String) where { S <: AbstractMessage } where { J <: AbstractMessage } = begin
-        sum_product_message = LazyObservable{S}()
-        joint_message       = LazyObservable{J}()
+        lazy_sp = LazyObservable{S}()
+        lazy_jm = LazyObservable{J}()
 
-        return new(name, sum_product_message, joint_message)
+        sum_product_message = lazy_sp# |> replay(1) |> take(1)
+        joint_message       = lazy_jm# |> replay(1) |> take(1)
+
+        return new(name, lazy_sp, lazy_jm, sum_product_message, joint_message)
     end
 end
