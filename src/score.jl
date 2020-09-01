7c4e9845f9c3decdd004bd3bbe8a114249d3d7f3
Dmitri Bagaev <bvdmitri@gmail.com>
2020-09-01 17:38:00 +0200

    update(): update hgf demo

--- src/score.jl
+++ src/score.jl
@@ -13,11 +13,14 @@ struct BetheFreeEnergy end
 function score(::BetheFreeEnergy, model::Model, scheduler)
     average_energies = map(filter(isstochastic, getnodes(model))) do node
         marginals = combineLatest(map(cluster -> getmarginal!(node, cluster), clusters(node)), PushEach())
+        # return marginals |> schedule_on(scheduler) |> map(Float64, (m) -> score(AverageEnergy(), functionalform(node), m) - mapreduce(d -> score(DifferentialEntropy(), d), +, m, init = 0.0)) 
+        # below without H[q_a]
         return marginals |> schedule_on(scheduler) |> map(Float64, (m) -> score(AverageEnergy(), functionalform(node), m)) 
     end
 
     differential_entropies = map(getrandom(model)) do random 
-        return getmarginal(random) |> schedule_on(scheduler) |> map(Float64, (m) -> (degree(random) - 1) * score(DifferentialEntropy(), m))
+        d = degree(random)
+        return getmarginal(random) |> schedule_on(scheduler) |> map(Float64, (m) -> (d - 1) * score(DifferentialEntropy(), m))
     end
 
     energies_sum  = collectLatest(Float64, average_energies) |> map(Float64, energies -> reduce(+, energies))
