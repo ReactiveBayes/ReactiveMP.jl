ff2a294f409029255e67d612ef57c678f86a8761
Dmitri Bagaev <bvdmitri@gmail.com>
2020-12-08 19:12:58 +0100

    feat(): prototype of gaussian mixture node

--- src/rules/bernoulli/out.jl
+++ src/rules/bernoulli/out.jl
@@ -6,8 +6,8 @@
     marginals   => (q_p::Any, ),
     meta        => Nothing,
     begin
-        rho_1 = exp(logmean(q_p))
-        rho_2 = exp(mirroredlogmean(q_p))
+        rho_1 = clamp(exp(logmean(q_p)), tiny, huge)
+        rho_2 = clamp(exp(mirroredlogmean(q_p)), tiny, huge)
         return Bernoulli(rho_1 / (rho_1 + rho_2))
     end
 )
--- src/rules/normal_mixture/switch.jl
+++ src/rules/normal_mixture/switch.jl
@@ -6,8 +6,9 @@
     marginals   => (q_out::Any, q_m::NTuple{2, NormalMeanVariance}, q_p::NTuple{2, Gamma}),
     meta        => Nothing,
     begin
+        # TODO check
         U1 = -score(AverageEnergy(), NormalMeanPrecision, Val{ (:out, :μ, :τ) }, map(as_marginal, (q_out, q_m[1], q_p[1])), nothing)
         U2 = -score(AverageEnergy(), NormalMeanPrecision, Val{ (:out, :μ, :τ) }, map(as_marginal, (q_out, q_m[2], q_p[2])), nothing)
-        return Bernoulli(softmax((U1, U2))[1])
+        return Bernoulli(clamp(softmax((U1, U2))[1], tiny, 1.0 - tiny))
     end
 )
