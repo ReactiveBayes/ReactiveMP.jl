e1fd0e38d30d46b88bcba513de87ae4c8ac32eae
Dmitri Bagaev <bvdmitri@gmail.com>
2021-02-11 17:11:56 +0100

    test(): Add pdf/logpdf cross checks for different Gamma distributions

--- test/distributions/test_gamma.jl
+++ test/distributions/test_gamma.jl
@@ -119,12 +119,16 @@ using Random
         @test convert(GammaShapeRate{Float64}, 1.0, 0.1) == GammaShapeRate{Float64}(1.0, 0.1)
 
         check_basic_statistics = (left, right) -> begin
-            @test mean(left)    ≈ mean(right)
-            @test var(left)     ≈ var(right)
-            @test shape(left)   ≈ shape(right)
-            @test scale(left)   ≈ scale(right)
-            @test rate(left)    ≈ rate(right)
-            @test entropy(left) ≈ entropy(right)
+            @test mean(left)         ≈ mean(right)
+            @test var(left)          ≈ var(right)
+            @test shape(left)        ≈ shape(right)
+            @test scale(left)        ≈ scale(right)
+            @test rate(left)         ≈ rate(right)
+            @test entropy(left)      ≈ entropy(right)
+            @test pdf(left, 1.0)     ≈ pdf(right, 1.0)
+            @test pdf(left, 10.0)    ≈ pdf(right, 10.0)
+            @test logpdf(left, 1.0)  ≈ logpdf(right, 1.0)
+            @test logpdf(left, 10.0) ≈ logpdf(right, 10.0)
         end
 
         types = ReactiveMP.union_types(GammaDistributionsFamily{Float64})
