0acd2548e23ad91acb7cef67da3a6b9109440746
albertpod <albert.podusenko@gmail.com>
2021-02-26 14:44:06 +0100

    Add various rules

--- src/nodes/matrix_dirichlet.jl
+++ src/nodes/matrix_dirichlet.jl
@@ -1,2 +1,19 @@
+import SpecialFunctions: loggamma
 
 @node MatrixDirichlet Stochastic [ out, a ]
+
+@average_energy MatrixDirichlet (q_out::MatrixDirichlet, q_a::PointMass) = begin
+
+    log_mean_marg_out = logmean(q_out)
+
+    H = 0.0
+    for k = 1:ndims(q_a)[2] # For all columns
+        a_sum = sum(mean(q_a)[:,k])
+
+        H += -loggamma(a_sum) +
+        sum(loggamma.(mean(q_a)[:,k])) -
+        sum( (mean(q_a)[:,k] .- 1.0).*log_mean_marg_out[:,k] )
+    end
+
+    return H
+end
--- src/nodes/transition.jl
+++ src/nodes/transition.jl
@@ -7,3 +7,7 @@ struct Transition end
 @average_energy Transition (q_out::Categorical, q_in::Categorical, q_a::MatrixDirichlet) = begin
     return probvec(marg_out)' * logmean(marg_a) * probvec(marg_in1)
 end
+
+@average_energy Transition (q_out_in::Any, q_a::MatrixDirichlet) = begin
+    return -tr(contingency_matrix(q_out_in)'*logmean(q_a))
+end
