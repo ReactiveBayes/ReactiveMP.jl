5ca657ac980cf28d2bc99716ee642895a8136c6d
Dmitri Bagaev <bvdmitri@gmail.com>
2021-03-09 12:19:57 +0100

    2prev

--- src/nodes/categorical.jl
+++ src/nodes/categorical.jl
@@ -1,4 +1,4 @@
 
 @node Categorical Stochastic [ out, p ]
 
-@average_energy Categorical (q_out::Categorical, q_p::Any) = -sum(mean(q_out) .* logmean(q_p))
+@average_energy Categorical (q_out::Categorical, q_p::Any) = -sum(probvec(q_out) .* logmean(q_p))
