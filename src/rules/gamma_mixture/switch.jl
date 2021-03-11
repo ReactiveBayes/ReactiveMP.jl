361cbb6380c1bdeb1008f0f58d8bb8ea68c6db79
Albert Podusenko <a.podusenko@tue.nl>
2021-03-11 11:33:07 +0100

    Add EM optimization

--- src/rules/gamma_mixture/switch.jl
+++ src/rules/gamma_mixture/switch.jl
@@ -1,6 +1,6 @@
 
-@rule GammaMixture{N}(:switch, Marginalisation) (q_out::Any, q_a::NTuple{N, GammaDistributionsFamily }, q_b::NTuple{N, GammaDistributionsFamily }) where { N } = begin
-    
+@rule GammaMixture{N}(:switch, Marginalisation) (q_out::Any, q_a::NTuple{N, Any }, q_b::NTuple{N, GammaDistributionsFamily }) where { N } = begin
+
     U = map(zip(q_a, q_b)) do (a, b)
         return -score(AverageEnergy(), GammaShapeRate, Val{ (:out, :α, :β) }, map(as_marginal, (q_out, a, b)), nothing)
     end
