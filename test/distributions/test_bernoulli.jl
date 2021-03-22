b624b8092bc8f6dd0287270cbc6ac4196a7dfa02
Bagaev Dmitry <bvdmitri@gmail.com>
2021-03-22 15:25:55 +0100

    test(): Bernoulli tests added

---
+++ test/distributions/test_bernoulli.jl
@@ -0,0 +1,39 @@
+module BernoulliTest
+
+using Test
+using ReactiveMP
+using Distributions
+using Random
+
+@testset "Bernoulli" begin
+
+    # Bernoulli comes from Distributions.jl and most of the things should be covered there
+    # Here we test some extra ReactiveMP.jl specific functionality
+
+    @testset "vague" begin
+        d = vague(Bernoulli)
+
+        @test typeof(d) <: Bernoulli
+        @test mean(d) === 0.5
+        @test succprob(d) === 0.5
+        @test failprob(d) === 0.5
+    end
+
+    @testset "prod" begin
+        @test prod(ProdPreserveParametrisation(), Bernoulli(0.5), Bernoulli(0.5))       ≈ Bernoulli(0.5)
+        @test prod(ProdBestSuitableParametrisation(), Bernoulli(0.5), Bernoulli(0.5))   ≈ Bernoulli(0.5)
+        @test prod(ProdPreserveParametrisation(), Bernoulli(0.1), Bernoulli(0.6))       ≈ Bernoulli(0.14285714285714285)
+        @test prod(ProdBestSuitableParametrisation(), Bernoulli(0.1), Bernoulli(0.6))   ≈ Bernoulli(0.14285714285714285)
+        @test prod(ProdPreserveParametrisation(), Bernoulli(0.78), Bernoulli(0.05))     ≈ Bernoulli(0.1572580645161291)
+        @test prod(ProdBestSuitableParametrisation(), Bernoulli(0.78), Bernoulli(0.05)) ≈ Bernoulli(0.1572580645161291)
+    end
+
+    @testset "probvec" begin
+        @test probvec(Bernoulli(0.5)) === (0.5, 0.5)
+        @test probvec(Bernoulli(0.3)) === (0.3, 0.7)
+        @test probvec(Bernoulli(0.6)) === (0.6, 0.4)
+    end
+
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -43,6 +43,7 @@ using .ReactiveMPTestingHelpers
     end
 
     addtests("test_distributions.jl")
+    addtests("distributions/test_bernoulli.jl.jl")
     addtests("distributions/test_normal_mean_variance.jl")
     addtests("distributions/test_normal_mean_precision.jl")
     addtests("distributions/test_normal_weighted_mean_precision.jl")
