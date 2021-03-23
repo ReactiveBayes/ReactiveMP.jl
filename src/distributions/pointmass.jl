cba6cb59ad0be849cc123921663788c47b006c6a
Bagaev Dmitry <bvdmitri@gmail.com>
2021-03-23 10:03:26 +0100

    test(): PointMass tests added

--- src/distributions/pointmass.jl
+++ src/distributions/pointmass.jl
@@ -18,54 +18,66 @@ getpointmass(distribution::PointMass) = distribution.point
 
 ##
 
-Base.getindex(distribution::PointMass, index) = Base.getindex(getpointmass(distribution), index)
+Base.getindex(distribution::PointMass, index...) = Base.getindex(getpointmass(distribution), index...)
 
-Distributions.insupport(distribution::PointMass, x::Real) = x == getpointmass(distribution)
-Distributions.pdf(distribution::PointMass, x::Real)       = Distributions.insupport(distribution, x) ? 1.0 : 0.0
-Distributions.logpdf(distribution::PointMass, x::Real)    = Distributions.insupport(distribution, x) ? 0.0 : -Inf
 Distributions.entropy(::PointMass) = -∞
 
 # Real-based univariate point mass
+
+Distributions.insupport(distribution::PointMass{T}, x::Real) where { T <: Real } = x == getpointmass(distribution)
+Distributions.pdf(distribution::PointMass{T}, x::Real)       where { T <: Real } = Distributions.insupport(distribution, x) ? 1.0 : 0.0
+Distributions.logpdf(distribution::PointMass{T}, x::Real)    where { T <: Real } = Distributions.insupport(distribution, x) ? 0.0 : -Inf
 
 Distributions.mean(distribution::PointMass{T}) where { T <: Real } = getpointmass(distribution)
 Distributions.var(distribution::PointMass{T})  where { T <: Real } = zero(T)
 Distributions.std(distribution::PointMass{T})  where { T <: Real } = zero(T)
 Distributions.cov(distribution::PointMass{T})  where { T <: Real } = zero(T)
 
-probvec(distribution::PointMass{T})      where { T <: Real } = error("probvec() for univariate point mass is not defined")
-logmean(distribution::PointMass{T})      where { T <: Real } = log(mean(distribution))
-inversemean(distribution::PointMass{T})  where { T <: Real } = inv(mean(distribution))
-loggammamean(distribution::PointMass{T}) where { T <: Real } = loggamma(mean(distribution))
+probvec(distribution::PointMass{T})         where { T <: Real } = error("probvec(::PointMass{ <: Real }) is not defined")
+logmean(distribution::PointMass{T})         where { T <: Real } = log(mean(distribution))
+inversemean(distribution::PointMass{T})     where { T <: Real } = inv(mean(distribution))
+mirroredlogmean(distribution::PointMass{T}) where { T <: Real } = log(one(T) - mean(distribution))
+loggammamean(distribution::PointMass{T})    where { T <: Real } = loggamma(mean(distribution))
 
 Base.precision(::PointMass{T}) where { T <: Real } = Inf
 Base.ndims(::PointMass{T})     where { T <: Real } = 1
 
 # AbstractVector-based multivariate point mass
 
+Distributions.insupport(distribution::PointMass{V}, x::AbstractVector) where { T, V <: AbstractVector{T} } = x == getpointmass(distribution)
+Distributions.pdf(distribution::PointMass{V}, x::AbstractVector)       where { T, V <: AbstractVector{T} } = Distributions.insupport(distribution, x) ? 1.0 : 0.0
+Distributions.logpdf(distribution::PointMass{V}, x::AbstractVector)    where { T, V <: AbstractVector{T} } = Distributions.insupport(distribution, x) ? 0.0 : -Inf
+
 Distributions.mean(distribution::PointMass{V}) where { T, V <: AbstractVector{T} } = getpointmass(distribution)
 Distributions.var(distribution::PointMass{V})  where { T, V <: AbstractVector{T} } = zeros(T, (ndims(distribution), ))
 Distributions.std(distribution::PointMass{V})  where { T, V <: AbstractVector{T} } = zeros(T, (ndims(distribution), ))
 Distributions.cov(distribution::PointMass{V})  where { T, V <: AbstractVector{T} } = zeros(T, (ndims(distribution), ndims(distribution)))
 
-probvec(distribution::PointMass{V})      where { T, V <: AbstractVector{T} } = mean(distribution)
-logmean(distribution::PointMass{V})      where { T, V <: AbstractVector{T} } = log.(mean(distribution))
-inversemean(distribution::PointMass{V})  where { T, V <: AbstractVector{T} } = inv.(mean(distribution))
-loggammamean(distribution::PointMass{V}) where { T, V <: AbstractVector{T} } = loggamma.(mean(distribution))
+probvec(distribution::PointMass{V})         where { T, V <: AbstractVector{T} } = mean(distribution)
+logmean(distribution::PointMass{V})         where { T, V <: AbstractVector{T} } = log.(mean(distribution))
+inversemean(distribution::PointMass{V})     where { T, V <: AbstractVector{T} } = error("inversemean(::PointMass{ <: AbstractVector }) is not defined")
+mirroredlogmean(distribution::PointMass{V}) where { T, V <: AbstractVector{T} } = error("mirroredlogmean(::PointMass{ <: AbstractVector }) is not defined")
+loggammamean(distribution::PointMass{V})    where { T, V <: AbstractVector{T} } = loggamma.(mean(distribution))
 
-Base.precision(distribution::PointMass{V}) where { T, V <: AbstractVector{T} } = one(T) ./ var(distribution)
+Base.precision(distribution::PointMass{V}) where { T, V <: AbstractVector{T} } = one(T) ./ cov(distribution)
 Base.ndims(distribution::PointMass{V})     where { T, V <: AbstractVector{T} } = length(mean(distribution))
 
 # AbstractMatrix-based matrixvariate point mass
 
+Distributions.insupport(distribution::PointMass{M}, x::AbstractMatrix) where { T, M <: AbstractMatrix{T} } = x == getpointmass(distribution)
+Distributions.pdf(distribution::PointMass{M}, x::AbstractMatrix)       where { T, M <: AbstractMatrix{T} } = Distributions.insupport(distribution, x) ? 1.0 : 0.0
+Distributions.logpdf(distribution::PointMass{M}, x::AbstractMatrix)    where { T, M <: AbstractMatrix{T} } = Distributions.insupport(distribution, x) ? 0.0 : -Inf
+
 Distributions.mean(distribution::PointMass{M}) where { T, M <: AbstractMatrix{T} } = getpointmass(distribution)
 Distributions.var(distribution::PointMass{M})  where { T, M <: AbstractMatrix{T} } = zeros(T, ndims(distribution))
 Distributions.std(distribution::PointMass{M})  where { T, M <: AbstractMatrix{T} } = zeros(T, ndims(distribution))
-Distributions.cov(distribution::PointMass{M})  where { T, M <: AbstractMatrix{T} } = throw("Distributions.cov(::PointMass{ <: AbstractMatrix }) is not implemented")
+Distributions.cov(distribution::PointMass{M})  where { T, M <: AbstractMatrix{T} } = error("Distributions.cov(::PointMass{ <: AbstractMatrix }) is not defined")
 
-probvec(distribution::PointMass{M})      where { T, M <: AbstractMatrix{T} } = error("probvec() for matrix variate point mass is not implemented")
-logmean(distribution::PointMass{M})      where { T, M <: AbstractMatrix{T} } = log.(mean(distribution))
-inversemean(distribution::PointMass{M})  where { T, M <: AbstractMatrix{T} } = inv.(mean(distribution))
-loggammamean(distribution::PointMass{M}) where { T, M <: AbstractMatrix{T} } = loggamma.(mean(distribution))
+probvec(distribution::PointMass{M})         where { T, M <: AbstractMatrix{T} } = error("probvec(::PointMass{ <: AbstractMatrix }) is not defined")
+logmean(distribution::PointMass{M})         where { T, M <: AbstractMatrix{T} } = log.(mean(distribution))
+inversemean(distribution::PointMass{M})     where { T, M <: AbstractMatrix{T} } = cholinv(mean(distribution))
+mirroredlogmean(distribution::PointMass{M}) where { T, M <: AbstractMatrix{T} } = error("mirroredlogmean(::PointMass{ <: AbstractMatrix }) is not defined")
+loggammamean(distribution::PointMass{M})    where { T, M <: AbstractMatrix{T} } = loggamma.(mean(distribution))
 
-Base.precision(distribution::PointMass{M}) where { T, M <: AbstractMatrix{T} } = one(T) ./ var(distribution)
+Base.precision(distribution::PointMass{M}) where { T, M <: AbstractMatrix{T} } = one(T) ./ cov(distribution)
 Base.ndims(distribution::PointMass{M})     where { T, M <: AbstractMatrix{T} } = size(mean(distribution))
--- test/distributions/test_pointmass.jl
+++ test/distributions/test_pointmass.jl
@@ -10,14 +10,14 @@ import ReactiveMP: ∞
 
 @testset "PointMass" begin
 
-    @testset "Scalar-based PointMass" begin
+    @testset "Real-based PointMass" begin
 
         scalar = rand()
         dist   = PointMass(scalar)
 
         @test variate_form(dist) === Univariate
         @test_throws BoundsError dist[2]
-        @test_throws MethodError dist[2, 2]
+        @test_throws BoundsError dist[2, 2]
 
         @test insupport(dist, scalar)
         @test !insupport(dist, scalar + 1e-6)
@@ -31,6 +31,13 @@ import ReactiveMP: ∞
         @test logpdf(dist, scalar + 1e-6) === -Inf
         @test logpdf(dist, scalar - 1e-6) === -Inf
 
+        @test_throws MethodError insupport(dist, [ 1.0, 1.0 ])
+        @test_throws MethodError insupport(dist, [ 1.0 1.0; 1.0 1.0 ])
+        @test_throws MethodError pdf(dist, [ 1.0, 1.0 ])
+        @test_throws MethodError pdf(dist, [ 1.0 1.0; 1.0 1.0 ])
+        @test_throws MethodError logpdf(dist, [ 1.0, 1.0 ])
+        @test_throws MethodError logpdf(dist, [ 1.0 1.0; 1.0 1.0 ])
+
         @test entropy(dist) === -∞
 
         @test mean(dist)       === scalar
@@ -41,9 +48,104 @@ import ReactiveMP: ∞
         @test ndims(dist)      === 1
 
         @test_throws ErrorException probvec(dist) 
-        @test logmean(dist)      === log(scalar)
-        @test inversemean(dist)  === inv(scalar)
-        @test loggammamean(dist) === loggamma(scalar)
+        @test logmean(dist)         === log(scalar)
+        @test inversemean(dist)     === inv(scalar)
+        @test mirroredlogmean(dist) === log(1.0 - scalar)
+        @test loggammamean(dist)    === loggamma(scalar)
+    end
+
+    @testset "Vector-based PointMass" begin
+
+        vector = rand(5)
+        dist   = PointMass(vector)
+
+        @test variate_form(dist) === Multivariate
+        @test dist[2] === vector[2]
+        @test dist[3] === vector[3]
+        @test_throws BoundsError dist[6]
+        @test_throws BoundsError dist[3, 5]
+
+        @test insupport(dist, vector)
+        @test !insupport(dist, vector .+ 1e-6)
+        @test !insupport(dist, vector .- 1e-6)
+
+        @test pdf(dist, vector)         === 1.0
+        @test pdf(dist, vector .+ 1e-6) === 0.0
+        @test pdf(dist, vector .- 1e-6) === 0.0
+
+        @test logpdf(dist, vector)         === 0.0
+        @test logpdf(dist, vector .+ 1e-6) === -Inf
+        @test logpdf(dist, vector .- 1e-6) === -Inf
+
+        @test_throws MethodError insupport(dist, 1.0)
+        @test_throws MethodError insupport(dist, [ 1.0 1.0; 1.0 1.0 ])
+        @test_throws MethodError pdf(dist, 1.0)
+        @test_throws MethodError pdf(dist, [ 1.0 1.0; 1.0 1.0 ])
+        @test_throws MethodError logpdf(dist, 1.0)
+        @test_throws MethodError logpdf(dist, [ 1.0 1.0; 1.0 1.0 ])
+
+        @test entropy(dist) === -∞
+
+        @test mean(dist)       == vector
+        @test var(dist)        == zeros(5)
+        @test std(dist)        == zeros(5)
+        @test cov(dist)        == zeros(5, 5)
+        @test precision(dist)  == fill(Inf, (5, 5))
+        @test ndims(dist)      == 5
+
+        @test probvec(dist)      == vector
+        @test logmean(dist)      == log.(vector)
+        @test_throws ErrorException inversemean(dist)
+        @test_throws ErrorException mirroredlogmean(dist)
+        @test loggammamean(dist) == loggamma.(vector)
+    end
+
+    @testset "Matrix-based PointMass" begin
+
+        matrix = rand(5, 5)
+        dist   = PointMass(matrix)
+
+        @test variate_form(dist) === Matrixvariate
+        @test dist[2]    === matrix[2]
+        @test dist[3]    === matrix[3]
+        @test dist[3, 3] === matrix[3, 3]
+        @test_throws BoundsError dist[100]
+        @test_throws BoundsError dist[100, 100]
+
+        @test insupport(dist, matrix)
+        @test !insupport(dist, matrix .+ 1e-6)
+        @test !insupport(dist, matrix .- 1e-6)
+
+        @test pdf(dist, matrix)         === 1.0
+        @test pdf(dist, matrix .+ 1e-6) === 0.0
+        @test pdf(dist, matrix .- 1e-6) === 0.0
+
+        @test logpdf(dist, matrix)         === 0.0
+        @test logpdf(dist, matrix .+ 1e-6) === -Inf
+        @test logpdf(dist, matrix .- 1e-6) === -Inf
+
+        @test_throws MethodError insupport(dist, 1.0)
+        @test_throws MethodError insupport(dist, [ 1.0, 1.0 ])
+        @test_throws MethodError pdf(dist, 1.0)
+        @test_throws MethodError pdf(dist, [ 1.0, 1.0 ])
+        @test_throws MethodError logpdf(dist, 1.0)
+        @test_throws MethodError logpdf(dist, [ 1.0, 1.0 ])
+
+        @test entropy(dist) === -∞
+
+        @test mean(dist)       == matrix
+        @test var(dist)        == zeros(5, 5)
+        @test std(dist)        == zeros(5, 5)
+        @test ndims(dist)      == (5, 5)
+
+        @test_throws ErrorException cov(dist)
+        @test_throws ErrorException precision(dist)
+
+        @test_throws ErrorException probvec(dist)
+        @test logmean(dist)      == log.(matrix)
+        @test inversemean(dist)  ≈ cholinv(matrix)
+        @test_throws ErrorException mirroredlogmean(dist)
+        @test loggammamean(dist) == loggamma.(matrix)
     end
 
 end
