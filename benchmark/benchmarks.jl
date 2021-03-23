0059c319fc65495e88f00fd32e7989812a16242d
Bagaev Dmitry <bvdmitri@gmail.com>
2021-03-23 11:40:18 +0100

    benchmark(): fixes 2prev

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -12,7 +12,8 @@ SUITE["models"]["lgssm1"] = BenchmarkGroup([ "linear", "gaussian", "ssm" ])
 
 rng = MersenneTwister(1234)
 
-SUITE["models"]["lgssm1"]["n_50"]  = @benchmarkable LGSSM1Benchmark.benchmark(input) setup=(input=LGSSM1Benchmark.generate_input(rng, 50))
 SUITE["models"]["lgssm1"]["n_100"] = @benchmarkable LGSSM1Benchmark.benchmark(input) setup=(input=LGSSM1Benchmark.generate_input(rng, 100))
 SUITE["models"]["lgssm1"]["n_200"] = @benchmarkable LGSSM1Benchmark.benchmark(input) setup=(input=LGSSM1Benchmark.generate_input(rng, 200))
+SUITE["models"]["lgssm1"]["n_300"] = @benchmarkable LGSSM1Benchmark.benchmark(input) setup=(input=LGSSM1Benchmark.generate_input(rng, 300))
+SUITE["models"]["lgssm1"]["n_400"] = @benchmarkable LGSSM1Benchmark.benchmark(input) setup=(input=LGSSM1Benchmark.generate_input(rng, 400))
 SUITE["models"]["lgssm1"]["n_500"] = @benchmarkable LGSSM1Benchmark.benchmark(input) setup=(input=LGSSM1Benchmark.generate_input(rng, 500))
