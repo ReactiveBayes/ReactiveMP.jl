43c2b6970f5adaee8165fb3ab34f3d4c242c7d9f
Bagaev Dmitry <bvdmitri@gmail.com>
2021-04-08 11:54:35 +0200

    feat(): 2prev

--- src/approximations/importance.jl
+++ src/approximations/importance.jl
@@ -15,14 +15,12 @@ convenient functions to generate samples and weights to approximate expectations
 struct ImportanceSamplingApproximation{T, R}
     rng      :: R
     nsamples :: Int
-
-    bsamples     :: Vector{T}
-    btransformed :: Vector{T}
-    bweights     :: Vector{T}
+    bsamples :: Vector{T}
+    bweights :: Vector{T}
 end
 
 ImportanceSamplingApproximation(rng::R, nsamples::Int)            where { R }    = ImportanceSamplingApproximation(Float64, rng, nsamples) 
-ImportanceSamplingApproximation(::Type{T}, rng::R, nsamples::Int) where { T, R } = ImportanceSamplingApproximation{T, R}(rng, nsamples, Vector{T}(undef, nsamples), Vector{T}(undef, nsamples), Vector{T}(undef, nsamples))
+ImportanceSamplingApproximation(::Type{T}, rng::R, nsamples::Int) where { T, R } = ImportanceSamplingApproximation{T, R}(rng, nsamples, Vector{T}(undef, nsamples), Vector{T}(undef, nsamples))
 
 getsamples(approximation::ImportanceSamplingApproximation, distribution)           = getsamples(approximation, distribution, approximation.nsamples)
 getsamples(approximation::ImportanceSamplingApproximation, distribution, nsamples) = rand(approximation.rng, distribution, nsamples)
@@ -31,11 +29,11 @@ function approximate_meancov(approximation::ImportanceSamplingApproximation, g::
 
     # We use preallocated arrays to sample and compute transformed samples and weightd
     rand!(approximation.rng, distribution, approximation.bsamples)
-    map!(g, approximation.btransformed, approximation.bsamples)
+    map!(g, approximation.bweights, approximation.bsamples)
 
-    normalization = sum(approximation.btransformed)
+    normalization = sum(approximation.bweights)
 
-    map!(Base.Fix2(/, normalization), approximation.bweights, approximation.btransformed)
+    map!(Base.Fix2(/, normalization), approximation.bweights, approximation.bweights)
     
     m = mapreduce(prod, +, zip(approximation.bweights, approximation.bsamples))
 
@@ -46,4 +44,5 @@ function approximate_meancov(approximation::ImportanceSamplingApproximation, g::
     v = mapreduce(_v, +, zip(approximation.bweights, approximation.bsamples))
 
     return m, v
+
 end
