62d18c4c6d71673a079d4d94a5004ebc8d1aff0a
Bart van Erp <b.v.erp@tue.nl>
2021-04-09 16:39:23 +0200

    update update rules complex normal

--- src/rules/complex_normal/mean.jl
+++ src/rules/complex_normal/mean.jl
@@ -1,7 +1,7 @@
 export rule
 
-@rule ComplexNormal(:μ, Marginalisation) (q_out::PointMass, q_Γ::PointMass, q_C::PointMass) = ComplexNormal(mean(q_μ), mean(q_Γ), mean(q_C))
+@rule ComplexNormal(:μ, Marginalisation) (q_out::PointMass, q_Γ::PointMass, q_C::PointMass) = ComplexNormal(mean(q_out), mean(q_Γ), mean(q_C))
 
-@rule ComplexNormal(:μ, Marginalisation) (q_out::Any, q_Γ::Any, q_C::Any) = ComplexNormal(mean(q_μ), mean(q_Γ), mean(q_C))
+@rule ComplexNormal(:μ, Marginalisation) (q_out::Any, q_Γ::Any, q_C::Any) = ComplexNormal(mean(q_out), mean(q_Γ), mean(q_C))
 
-@rule ComplexNormal(:μ, Marginalisation) (q_out::ComplexNormal, q_Γ::Any, q_C::Any) = ComplexNormal(mean(q_μ), cov(q_μ) + mean(q_Γ), mean(q_C))
+@rule ComplexNormal(:μ, Marginalisation) (q_out::ComplexNormal, q_Γ::Any, q_C::Any) = ComplexNormal(mean(q_out), cov(q_out) + mean(q_Γ), mean(q_C))
