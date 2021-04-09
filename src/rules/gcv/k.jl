fb38515bb4ff2b32343ef378acc9690471172bab
ismail <i.senoz@tue.nl>
2021-04-09 17:34:00 +0300

    add rules for kappa and omega for gcv

--- src/rules/gcv/k.jl
+++ src/rules/gcv/k.jl
@@ -1,17 +1,17 @@
 export rule
 
-# TODO: double check
-# @rule GCV(:κ, Marginalisation) (q_y_x::Any, q_z::Any, q_ω::Any) = begin
-#     Λ = cov(q_y_x)
-#     m = mean(q_y_x)
+@rule GCV(:κ, Marginalisation) (q_y_x::Any, q_z::Any, q_ω::Any) = begin
+    
+    m, v = mean(q_y_x), cov(q_y_x)
 
-#     γ_3 = exp(-mean(q_ω) + 0.5 * var(q_ω))
-#     γ_4 = (m[1] - m[2]) ^ 2 + Λ[1, 1] + Λ[2, 2] - Λ[1, 2] - Λ[2, 1]
+    A = exp(-mean(q_ω) + 0.5 * var(q_ω))
+    psi = (m[1] - m[2]) ^ 2 + v[1, 1] + v[2, 2] - v[1, 2] - v[2, 1]
 
-#     a = mean(q_z)
-#     b = γ_4 * γ_3
-#     c = -a
-#     d = var(q_z)
+    a = mean(q_z)
+    b = psi * A
+    c = -a
+    d = var(q_z)
 
-#     return ExponentialLinearQuadratic(a, b, c, d)
-# end
+    return ExponentialLinearQuadratic(a, b, c, d)
+end
+
--- src/rules/gcv/w.jl
+++ src/rules/gcv/w.jl
@@ -1,18 +1,18 @@
 export rule
 
-# TODO: Double check
-# @rule GCV(:ω, Marginalisation) (q_y_x::Any, q_z::Any, q_κ::Any) = begin
-#     Λ = cov(q_y_x)
-#     m = mean(q_y_x)
 
-#     γ_1 = mean(q_z) ^ 2 * var(q_κ) + mean(q_κ) ^ 2 * var(q_z) + var(q_z) * var(q_κ)
-#     γ_2 = exp(-mean(q_κ) * mean(q_z) + 0.5 * γ_1)
-#     γ_4 = (m[1] - m[2]) ^ 2 + Λ[1, 1] + Λ[2, 2] - Λ[1, 2] - Λ[2, 1]
+@rule GCV(:ω, Marginalisation) (q_y_x::Any, q_z::Any, q_κ::Any) = begin
+    
+    m, v = mean(q_y_x), cov(q_y_x)
 
-#     a = one(typeof(γ_1))
-#     b = γ_4 * γ_2
-#     c = -one(typeof(γ_1))
-#     d = zero(typeof(γ_1))
+    γ = mean(q_z) ^ 2 * var(q_κ) + mean(q_κ) ^ 2 * var(q_z) + var(q_z) * var(q_κ)
+    A = exp(-mean(q_κ) * mean(q_z) + 0.5 * γ)
+    psi = (m[1] - m[2]) ^ 2 + v[1, 1] + v[2, 2] - v[1, 2] - v[2, 1]
 
-#     return ExponentialLinearQuadratic(a, b, c, d)
-# end
+    a = one(typeof(γ))
+    b = psi * A
+    c = -one(typeof(γ))
+    d = zero(typeof(γ))
+
+    return ExponentialLinearQuadratic(a, b, c, d)
+end
