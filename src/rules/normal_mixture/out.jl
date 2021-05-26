88c2a64c28e368f98c50e441bffac7c19ac63c06
Bagaev Dmitry <bvdmitri@gmail.com>
2021-05-26 16:04:03 +0300

    feat(): Multivariate out rule for gaussian mixture

--- src/rules/normal_mixture/out.jl
+++ src/rules/normal_mixture/out.jl
@@ -3,3 +3,11 @@
     πs = probvec(q_switch)
     return NormalMeanPrecision(sum(πs .* mean.(q_m)), sum(πs .* mean.(q_p)))
 end
+
+@rule NormalMixture{N}(:out, Marginalisation) (q_switch::Any, q_m::NTuple{N,  MultivariateNormalDistributionsFamily}, q_p::NTuple{N, Wishart }) where { N } = begin
+    πs = probvec(q_switch)
+    d  = ndims(first(q_m))
+    w  = mapreduce(x -> x[1] * mean(x[2]), +, zip(πs, q_p))
+    xi = mapreduce(x -> x[1] * mean(x[2]) * mean(x[3]), +, zip(πs, q_p, q_m))
+    return MvNormalWeightedMeanPrecision(xi, w)
+end
