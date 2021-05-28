4fd4fbfac40cab6f88d77018ddbef7f17d883cf4
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-05-28 09:46:07 +0200

    reorder rules

--- src/rules/mv_normal_mean_precision/mean.jl
+++ src/rules/mv_normal_mean_precision/mean.jl
@@ -2,11 +2,11 @@ export rule
 
 @rule MvNormalMeanPrecision(:μ, Marginalisation) (m_out::PointMass, m_Λ::PointMass) = MvNormalMeanPrecision(mean(m_out), mean(m_Λ))
 
-@rule MvNormalMeanPrecision(:μ, Marginalisation) (q_out::Any, q_Λ::Any) = MvNormalMeanPrecision(mean(q_out), mean(q_Λ))
-
-@rule MvNormalMeanPrecision(:μ, Marginalisation) (m_out::MvNormalMeanPrecision, q_Λ::Any) = MvNormalMeanPrecision(mean(m_out), cholinv(cov(m_out) + cholinv(mean(q_Λ))))
-
 @rule MvNormalMeanPrecision(:μ, Marginalisation) (m_out::MultivariateNormalDistributionsFamily, m_Λ::PointMass) = begin
     mout, vout = mean_cov(m_out)
     return MvNormalMeanCovariance(mout, v_out + cholinv(mean(m_Λ)))
 end
+
+@rule MvNormalMeanPrecision(:μ, Marginalisation) (q_out::Any, q_Λ::Any) = MvNormalMeanPrecision(mean(q_out), mean(q_Λ))
+
+@rule MvNormalMeanPrecision(:μ, Marginalisation) (m_out::MvNormalMeanPrecision, q_Λ::Any) = MvNormalMeanPrecision(mean(m_out), cholinv(cov(m_out) + cholinv(mean(q_Λ))))
--- src/rules/mv_normal_mean_precision/out.jl
+++ src/rules/mv_normal_mean_precision/out.jl
@@ -2,11 +2,11 @@ export rule
 
 @rule MvNormalMeanPrecision(:out, Marginalisation) (m_μ::PointMass, m_Λ::PointMass) = MvNormalMeanPrecision(mean(m_μ), mean(m_Λ))
 
-@rule MvNormalMeanPrecision(:out, Marginalisation) (q_μ::Any, q_Λ::Any) = MvNormalMeanPrecision(mean(q_μ), mean(q_Λ))
-
-@rule MvNormalMeanPrecision(:out, Marginalisation) (m_μ::MvNormalMeanPrecision, q_Λ::Any) = MvNormalMeanPrecision(mean(m_μ), cholinv(cov(m_μ) + cholinv(mean(q_Λ))))
-
 @rule MvNormalMeanPrecision(:out, Marginalisation) (m_μ::MultivariateNormalDistributionsFamily, m_Λ::PointMass) = begin
     mμ, vμ = mean_cov(m_μ)
     return MvNormalMeanCovariance(mμ, vμ + cholinv(mean(m_Λ)))
 end
+
+@rule MvNormalMeanPrecision(:out, Marginalisation) (q_μ::Any, q_Λ::Any) = MvNormalMeanPrecision(mean(q_μ), mean(q_Λ))
+
+@rule MvNormalMeanPrecision(:out, Marginalisation) (m_μ::MvNormalMeanPrecision, q_Λ::Any) = MvNormalMeanPrecision(mean(m_μ), cholinv(cov(m_μ) + cholinv(mean(q_Λ))))
