30abeabffff18361ce1b569c37ce9a9ba4e4bfa0
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-05-28 14:03:26 +0200

    add tests mvnormalmeanprecision:precision

---
+++ test/rules/mv_normal_mean_precision/test_precision.jl
@@ -0,0 +1,30 @@
+module RulesMvNormalMeanPrecisionPrecisionTest
+
+using Test
+using ReactiveMP
+using Random
+
+
+import ReactiveMP: @test_rules
+
+@testset "rules:MvNormalMeanPrecision:precision" begin
+
+    @testset "Variational: (q_out::PointMass, q_μ::MultivariateNormalDistributionsFamily)" begin
+
+        @test_rules [ with_float_conversions = false ] MvNormalMeanPrecision(:Λ, Marginalisation) [
+            (input = (q_out = PointMass([1.0; 2.0]), q_μ = MvNormalMeanPrecision([3.0; 5.0], [3.0 2.0; 2.0 4.0])), output = Wishart(4.0, cholinv(cholinv([3.0 2.0; 2.0 4.0]) + ([3.0; 5.0] - [1.0; 2.0])*([3.0; 5.0] - [1.0; 2.0])')))
+        ]
+        @test_rules [ with_float_conversions = false ] MvNormalMeanPrecision(:Λ, Marginalisation) [
+            (input = (q_out = PointMass([1.0; 2.0]), q_μ = MvNormalMeanCovariance([3.0; 5.0], inv([3.0 2.0; 2.0 4.0]))), output = Wishart(4.0, cholinv(cholinv([3.0 2.0; 2.0 4.0]) + ([3.0; 5.0] - [1.0; 2.0])*([3.0; 5.0] - [1.0; 2.0])')))
+        ]
+        @test_rules [ with_float_conversions = false ] MvNormalMeanPrecision(:Λ, Marginalisation) [
+            (input = (q_out = PointMass([1.0; 2.0]), q_μ = MvNormalWeightedMeanPrecision([19.0; 26.0], [3.0 2.0; 2.0 4.0])), output = Wishart(4.0, cholinv(cholinv([3.0 2.0; 2.0 4.0]) + ([3.0; 5.0] - [1.0; 2.0])*([3.0; 5.0] - [1.0; 2.0])')))
+        ]
+
+    end
+
+end
+
+
+
+end
