c0caf5865f746e3bfe388cfc94ed2cda89e523ce
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-05-28 11:08:54 +0200

    optimize rules

--- src/rules/mv_normal_mean_precision/mean.jl
+++ src/rules/mv_normal_mean_precision/mean.jl
@@ -4,9 +4,9 @@ export rule
 
 @rule MvNormalMeanPrecision(:μ, Marginalisation) (m_out::MultivariateNormalDistributionsFamily, m_Λ::PointMass) = begin
     mout, vout = mean_cov(m_out)
-    return MvNormalMeanCovariance(mout, v_out + cholinv(mean(m_Λ)))
+    return MvNormalMeanCovariance(mout, vout + cholinv(mean(m_Λ)))
 end
 
 @rule MvNormalMeanPrecision(:μ, Marginalisation) (q_out::Any, q_Λ::Any) = MvNormalMeanPrecision(mean(q_out), mean(q_Λ))
 
-@rule MvNormalMeanPrecision(:μ, Marginalisation) (m_out::MvNormalMeanPrecision, q_Λ::Any) = MvNormalMeanPrecision(mean(m_out), cholinv(cov(m_out) + cholinv(mean(q_Λ))))
+@rule MvNormalMeanPrecision(:μ, Marginalisation) (m_out::MvNormalMeanPrecision, q_Λ::Any) = MvNormalMeanCovariance(mean(m_out), cov(m_out) + cholinv(mean(q_Λ)))
