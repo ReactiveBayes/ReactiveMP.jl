2f29e9e6ec13ef9d5a2cb17f40057020d72c1fda
Dmitri Bagaev <bvdmitri@gmail.com>
2021-05-31 11:26:27 +0300

    test(): More tests

--- test/rules/mv_normal_mean_covariance/test_out.jl
+++ test/rules/mv_normal_mean_covariance/test_out.jl
@@ -11,10 +11,12 @@ import ReactiveMP: @test_rules
     @testset "Belief Propagation: (m_μ::PointMass, m_Σ::PointMass)" begin
         
         @test_rules [ with_float_conversions = true ] MvNormalMeanCovariance(:out, Marginalisation) [
-            (input = (m_μ = PointMass([-1.0]), m_Σ = PointMass([2.0])), output = MvNormalMeanCovariance([-1.0], [2.0])),
-            (input = (m_μ = PointMass([1.0]), m_Σ = PointMass([2.0])),  output = MvNormalMeanCovariance([1.0], [2.0])),
-            (input = (m_μ = PointMass([2.0]), m_Σ = PointMass([1.0])),  output = MvNormalMeanCovariance([2.0], [1.0])),
-            (input = (m_μ = PointMass([1.0; 3.0]), m_Σ = PointMass([3.0 2.0; 2.0 4.0])),  output = MvNormalMeanCovariance([1.0; 3.0], [3.0 2.0; 2.0 4.0]))
+            (input = (m_μ = PointMass([ -1.0 ]), m_Σ = PointMass([ 2.0 ])), output = MvNormalMeanCovariance([ -1.0 ], [ 2.0 ])),
+            (input = (m_μ = PointMass([  1.0 ]), m_Σ = PointMass([ 2.0 ])), output = MvNormalMeanCovariance([  1.0 ], [ 2.0 ])),
+            (input = (m_μ = PointMass([ 2.0 ]),  m_Σ = PointMass([ 1.0 ])), output = MvNormalMeanCovariance([  2.0 ], [ 1.0 ])),
+            (input = (m_μ = PointMass([ 1.0, 3.0 ]),  m_Σ = PointMass([ 3.0 2.0; 2.0 4.0 ])),   output = MvNormalMeanCovariance([ 1.0, 3.0 ], [ 3.0 2.0; 2.0 4.0 ])),
+            (input = (m_μ = PointMass([ -1.0, 2.0 ]), m_Σ = PointMass([ 7.0 -1.0; -1.0 9.0 ])), output = MvNormalMeanCovariance([ -1.0, 2.0 ], [ 7.0 -1.0; -1.0 9.0 ])),
+            (input = (m_μ = PointMass([ 0.0, 0.0 ]),  m_Σ = PointMass([ 1.0 0.0; 0.0 1.0 ])),   output = MvNormalMeanCovariance([ 0.0, 0.0 ], [ 1.0 0.0; 0.0 1.0 ]))
         ]
 
     end
@@ -22,15 +24,22 @@ import ReactiveMP: @test_rules
     @testset "Belief Propagation: (m_μ::MultivariateNormalDistributionsFamily, m_Σ::PointMass)" begin
 
         @test_rules [ with_float_conversions = true ] MvNormalMeanCovariance(:out, Marginalisation) [
-            (input = (m_μ = MvNormalMeanCovariance([2.0; 1.0], [3.0 2.0; 2.0 4.0]),  m_Σ = PointMass([6.0 4.0; 4.0 8.0])), output = MvNormalMeanCovariance([2.0; 1.0], [9.0 6.0; 6.0 12.0]))
+            (input = (m_μ = MvNormalMeanPrecision([ 2.0, 1.0 ], [ 3.0 2.0; 2.0 4.0 ]),   m_Σ = PointMass([ 6.0 4.0; 4.0 8.0 ])),    output = MvNormalMeanCovariance([ 2.0, 1.0 ], [ 13/2 15/4; 15/4 67/8 ])),
+            (input = (m_μ = MvNormalMeanPrecision([ 0.0, 0.0 ], [ 7.0 -1.0; -1.0 9.0 ]), m_Σ = PointMass([ 12.0 -2.0; -2.0 7.0 ])), output = MvNormalMeanCovariance([ 0.0, 0.0 ], [ 753/62 -123/62; -123/62 441/62 ])),
+            (input = (m_μ = MvNormalMeanPrecision([ 3.0, -1.0 ], [ 1.0 0.0; 0.0 1.0 ]),  m_Σ = PointMass([ 1.0 0.0; 0.0 1.0 ])),    output = MvNormalMeanCovariance([ 3.0, -1.0 ], [ 2.0 0.0; 0.0 2.0 ])),
+            
         ]
 
         @test_rules [ with_float_conversions = true ] MvNormalMeanCovariance(:out, Marginalisation) [
-            (input = (m_μ = MvNormalMeanPrecision([2.0; 1.0], [0.5 -0.25; -0.25 0.375]),  m_Σ = PointMass([6.0 4.0; 4.0 8.0])), output = MvNormalMeanCovariance([2.0; 1.0], [9.0 6.0; 6.0 12.0]))
+            (input = (m_μ = MvNormalMeanCovariance([ 2.0, 1.0 ], [ 3.0 2.0; 2.0 4.0 ]),  m_Σ = PointMass([ 6.0 4.0; 4.0 8.0 ])),     output = MvNormalMeanCovariance([ 2.0, 1.0 ], [ 9.0 6.0; 6.0 12.0 ])),
+            (input = (m_μ = MvNormalMeanCovariance([ 0.0, 0.0 ], [ 7.0 -1.0; -1.0 9.0 ]), m_Σ = PointMass([ 12.0 -2.0; -2.0 7.0 ])), output = MvNormalMeanCovariance([ 0.0, 0.0 ], [ 19.0 -3.0; -3.0 16.0 ])),
+            (input = (m_μ = MvNormalMeanCovariance([ 3.0, -1.0 ], [ 1.0 0.0; 0.0 1.0 ]),  m_Σ = PointMass([ 1.0 0.0; 0.0 1.0 ])),    output = MvNormalMeanCovariance([ 3.0, -1.0 ], [ 2.0 0.0; 0.0 2.0 ]))
         ]
 
         @test_rules [ with_float_conversions = true ] MvNormalMeanCovariance(:out, Marginalisation) [
-            (input = (m_μ = MvNormalWeightedMeanPrecision([0.75; -0.125], [0.5 -0.25; -0.25 0.375]),  m_Σ = PointMass([6.0 4.0; 4.0 8.0])), output = MvNormalMeanCovariance([2.0; 1.0], [9.0 6.0; 6.0 12.0])),
+            (input = (m_μ = MvNormalWeightedMeanPrecision([ 2.0, 1.0 ], [ 3.0 2.0; 2.0 4.0 ]),   m_Σ = PointMass([ 6.0 4.0; 4.0 8.0 ])),    output = MvNormalMeanCovariance([ 3/4, -1/8 ], [ 13/2 15/4; 15/4 67/8 ])),
+            (input = (m_μ = MvNormalWeightedMeanPrecision([ 0.0, 0.0 ], [ 7.0 -1.0; -1.0 9.0 ]), m_Σ = PointMass([ 12.0 -2.0; -2.0 7.0 ])), output = MvNormalMeanCovariance([ 0.0, 0.0 ], [ 753/62 -123/62; -123/62 441/62 ])),
+            (input = (m_μ = MvNormalWeightedMeanPrecision([ 3.0, -1.0 ], [ 1.0 0.0; 0.0 1.0 ]),  m_Σ = PointMass([ 1.0 0.0; 0.0 1.0 ])),    output = MvNormalMeanCovariance([ 3.0, -1.0 ], [ 2.0 0.0; 0.0 2.0 ])),
         ]
 
     end
