1e7b594bc974f15ec67d8f6c2a4a92a91b961171
Martindeq98 <m.g.d.quincey@student.tue.nl>
2021-06-21 21:39:35 +0200

    WIP: fix input marginals

--- src/rules/bifm/input.jl
+++ src/rules/bifm/input.jl
@@ -1,21 +1,26 @@
 export rule
 
-@rule BIFM(:input, Marginalisation) (m_output::MvNormalWeightedMeanPrecision, m_zprev::MarginalDistribution{MvNormalMeanCovariance}, m_znext::MvNormalWeightedMeanPrecision, meta::BIFMMeta) = begin
+@rule BIFM(:input, Marginalisation) (m_output::MultivariateNormalDistributionsFamily, m_zprev::MarginalDistribution{<:MultivariateNormalDistributionsFamily}, m_znext::MultivariateNormalDistributionsFamily, meta::BIFMMeta) = begin
     # todo: optimize for speed
 
     # fetch statistics
-    μu, Σu = getμu(meta), getΣu(meta)
-    mean_znext, V_znext = mean_cov(m_znext)
-    ξz, Wz = weightedmean_precision(m_zprev)
-    B = getB(meta)
+    A, B                = getA(meta), getB(meta)
+    μu, Σu              = getμu(meta), getΣu(meta)
+    mean_znext, V_znext = mean_cov(m_zprev)
+#    ξz, Wz              = weightedmean_precision(m_znext)
+    ξz                  = getξz(meta)
+    Wz                  = getWz(meta)
 
+    ξz2, Wz2            = weightedmean_precision(m_zprev)#
+
+    println(A, B, μu, Σu, mean_znext, V_znext, ξz, Wz, ξz2, Wz2)
     # calculate intermediate variables
     ξztilde = Wz * mean_znext - ξz
     Wztilde = Wz - Wz * V_znext * Wz
 
     # calculate marginals of input
-    mean_input = μu - Σu * B.T * ξztilde
-    V_input = Σu - Σu * B.T * Wztilde * B * Σu
+    mean_input = μu - Σu * B' * ξztilde
+    V_input = Σu - Σu * B' * Wztilde * B * Σu
 
     # return input marginal
     return MarginalDistribution(MvNormalMeanCovariance(mean_input, V_input))
