61595b6f10acd5698bf27a1ef4e5e207749a0439
Martindeq98 <m.g.d.quincey@student.tue.nl>
2021-06-21 21:32:41 +0200

    WIP: add μu, Σu, ξz to metadata

--- src/nodes/bifm.jl
+++ src/nodes/bifm.jl
@@ -9,9 +9,10 @@ mutable struct BIFMMeta{T}
     B :: Matrix{T}
     C :: Matrix{T}
     H :: Union{Matrix{T}, Nothing}
+    ξz :: Union{Vector{T}, Nothing}
     ξztilde :: Union{Vector{T}, Nothing}
     Wz :: Union{Matrix{T}, Nothing}
-    μu :: Union{Matrix{T}, Nothing}
+    μu :: Union{Vector{T}, Nothing}
     Σu :: Union{Matrix{T}, Nothing}
 end
 
@@ -22,7 +23,19 @@ function BIFMMeta(A::Array{T1, 2}, B::Array{T2, 2}, C::Array{T3, 2}) where { T1,
     @assert size(A,1) == size(C,2)
 
     # return default Meta data for BIFM node
-    return BIFMMeta{T}(A, B, C, nothing, nothing, nothing, nothing, nothing)
+    return BIFMMeta{T}(A, B, C, nothing, nothing, nothing, nothing, nothing, nothing)
+end
+
+function BIFMMeta(A::Array{T1, 2}, B::Array{T2, 2}, C::Array{T3, 2}, μu::Array{T4, 1}, Σu::Array{T5, 2}) where { T1, T2, T3, T4, T5 }
+    T = promote_type(T1, T2, T3, T4, T5)
+    # check whether the dimensionality of transition matrices makes sense
+    @assert size(A,1)  == size(B,1)
+    @assert size(A,1)  == size(C,2)
+    @assert size(μu,1) == size(Σu,1)
+    @assert size(μu,1) == size(B,2)
+
+    # return default Meta data for BIFM node
+    return BIFMMeta{T}(A, B, C, nothing, nothing, nothing, nothing, μu, Σu)
 end
 
 getA(meta::BIFMMeta)                = meta.A
@@ -33,6 +46,7 @@ getξztilde(meta::BIFMMeta)          = meta.ξztilde
 getWz(meta::BIFMMeta)               = meta.Wz
 getμu(meta::BIFMMeta)               = meta.μu
 getΣu(meta::BIFMMeta)               = meta.Σu
+getξz(meta::BIFMMeta)               = meta.ξz
 
 function setH!(meta::BIFMMeta, H)
     meta.H = H
@@ -40,6 +54,10 @@ end
 
 function setξztilde!(meta::BIFMMeta, ξztilde)
     meta.ξztilde = ξztilde
+end
+
+function setξz!(meta::BIFMMeta, ξz)
+    meta.ξz = ξz
 end
 
 function setWz!(meta::BIFMMeta, Wz)
@@ -53,6 +71,5 @@ end
 function setΣu!(meta::BIFMMeta, Σu)
     meta.Σu = Σu
 end
-
 
 default_meta(::Type{ BIFM }) = error("BIFM node requires meta flag explicitly specified")
