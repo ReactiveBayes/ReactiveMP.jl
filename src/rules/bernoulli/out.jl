434285bf7342c8e57b8d3477032a82dc73fcafde
Bart van Erp <b.v.erp@tue.nl>
2021-07-05 09:27:33 +0200

    Add missing Bernoulli rule

--- src/rules/bernoulli/out.jl
+++ src/rules/bernoulli/out.jl
@@ -5,3 +5,7 @@ export rule
     rho_2 = clamp(exp(mirroredlogmean(q_p)), tiny, huge)
     return Bernoulli(rho_1 / (rho_1 + rho_2))
 end
+
+@rule Bernoulli(:out, Marginalisation) (q_p::PointMass, ) = begin
+    return Bernoulli(softmax(mean(q_p)))
+end
