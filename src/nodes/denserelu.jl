92fc44a9cc80ad2da08e1e7a703938f2f7a87c89
Bart van Erp <b.v.erp@tue.nl>
2021-07-05 09:56:11 +0200

    update denserelu node

--- src/nodes/denserelu.jl
+++ src/nodes/denserelu.jl
@@ -9,15 +9,21 @@ mutable struct DenseReLUMeta{T}
     α :: T
     β :: T
     γ :: T
+    ξ :: T
 end
 
 function DenseReLUMeta(C::T1, α::T2, β::T3, γ::T4) where { T1, T2, T3, T4 }
-    T = promote_type(T1, T2, T3, T4)
-
-    return DenseReLUMeta{T}(C, α, β, γ)
+    T = promote_type(T1, T2, T3, T4, Float64)
+    ξ = 1.0
+    return DenseReLUMeta{T}(C, α, β, γ, ξ)
 end
 
 getC(meta::DenseReLUMeta)           = meta.C
 getα(meta::DenseReLUMeta)           = meta.α
 getβ(meta::DenseReLUMeta)           = meta.β
 getγ(meta::DenseReLUMeta)           = meta.γ
+getξ(meta::DenseReLUMeta)           = meta.ξ
+
+function setξ!(meta::DenseReLUMeta, ξ)
+    meta.ξ = ξ
+end
