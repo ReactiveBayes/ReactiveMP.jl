90a9d3e5868cdb435757bd5a480c99f6af8163ef
Bart van Erp <b.v.erp@tue.nl>
2021-07-06 15:29:46 +0200

    update output rule

--- src/rules/denserelu/output.jl
+++ src/rules/denserelu/output.jl
@@ -28,31 +28,30 @@ export rule
 
 end
 
-
-# @rule DenseReLU(:output, Marginalisation) (q_input::NormalDistributionsFamily, q_w::NormalDistributionsFamily, q_z::Bernoulli, q_f::UnivariateNormalDistributionsFamily, meta::DenseReLUMeta) where { N } = begin
+@rule DenseReLU(:output, Marginalisation) (q_input::NormalDistributionsFamily, q_w::NTuple{1, NormalDistributionsFamily}, q_z::NTuple{1, Bernoulli}, q_f::NTuple{1, UnivariateNormalDistributionsFamily}, meta::DenseReLUMeta) = begin
     
-#     # assert whether the dimensions are correct
-#     @assert sum(length(q_input) .!= length.(q_w)) == 0 """
-#         The dimensionality of the input vector does not correspond to the dimensionality of the random variables representing the weights.
+    # assert whether the dimensions are correct
+    @assert sum(length(q_input) .!= length.(q_w)) == 0 """
+        The dimensionality of the input vector does not correspond to the dimensionality of the random variables representing the weights.
 
-#         The input variable x of dimensionality $(length(q_input)) looks like
-#         $(q_input)  
-#         whereas the first random variables of the weights of dimensionality $(length(q_w[1])) looks like
-#         $(q_w[1]).
-#     """
+        The input variable x of dimensionality $(length(q_input)) looks like
+        $(q_input)  
+        whereas the first random variables of the weights of dimensionality $(length(q_w[1])) looks like
+        $(q_w[1]).
+    """
 
-#     # extract required statistics
-#     pz = mean.(q_z)
-#     mf = mean.(q_f)
+    # extract required statistics
+    pz = mean.(q_z)[1]
+    mf = mean.(q_f)[1]
 
-#     # extract parameters
-#     γ = getγ(meta)
+    # extract parameters
+    γ = getγ(meta)
     
-#     # calculate new statistics
-#     my = pz .* mf
-#     wy = diagm(ones(N)*γ)
+    # calculate new statistics
+    my = pz * mf
+    wy = γ
 
-#     # return message
-#     return MvNormalMeanPrecision([my...], wy)
+    # return message
+    return NormalMeanPrecision(my, wy)
 
-# end
+end
