baacd81dfa13cd1ef1a2e1b2adc1f253a7c83271
Bart van Erp <b.v.erp@tue.nl>
2021-07-09 16:41:54 +0200

    add test for average energy

--- test/nodes/test_probit.jl
+++ test/nodes/test_probit.jl
@@ -13,7 +13,7 @@ using Random
         @test sdtype(node)                  === Stochastic()
         @test name.(interfaces(node))       === (:out, :in)
         @test factorisation(node)           === ((1, 2), )
-        @test metadata(node)                === nothing
+        @test metadata(node)                === ProbitMeta(32)
 
         node = make_node(Probit, meta = 1)
 
@@ -22,6 +22,24 @@ using Random
         @test name.(interfaces(node))       === (:out, :in)
         @test factorisation(node)           === ((1, 2), )
         @test metadata(node)                === 1
+    end
+
+    @testset "Average energy" begin
+        
+        node = make_node(Probit)
+
+        @test score(AverageEnergy(),
+                    Probit,
+                    Val{(:out, :in)},
+                    (Marginal(Bernoulli(1), false, false), Marginal(NormalMeanVariance(0.0, 1.0), false, false)),
+                    ProbitMeta()) ≈ -1.0
+        
+        @test score(AverageEnergy(),
+                    Probit,
+                    Val{(:out, :in)},
+                    (Marginal(PointMass(1), false, false), Marginal(NormalMeanVariance(0.0, 1.0), false, false)),
+                    ProbitMeta(100)) ≈ -1.0
+                    
     end
 
 end
