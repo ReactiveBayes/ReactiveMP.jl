7b1230e2df1564387e6219fc2ecd0321a48f8629
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-08-09 17:09:02 +0200

    update BIFM helper with  functional dependencies

--- src/nodes/bifm_helper.jl
+++ src/nodes/bifm_helper.jl
@@ -1,6 +1,26 @@
-export BIFM_helper
+export BIFM_helper, functional_dependencies
 
 struct BIFM_helper <: AbstractFactorNode end
 
 @node BIFM_helper Stochastic [ output, input ]
 
+# specify custom functional dependencies for BIFM_helper node
+function functional_dependencies(factornode::FactorNode{ <: Type{ReactiveMP.BIFM_helper} }, iindex::Int)
+    cindex             = clusterindex(factornode, iindex)
+
+    nodeinterfaces     = interfaces(factornode)
+    nodeclusters       = factorisation(factornode)
+    nodelocalmarginals = localmarginals(factornode)
+
+    varcluster = @inbounds nodeclusters[ cindex ]
+    
+    # output
+    if iindex === 2
+        mdependencies = (nodeinterfaces[1], )
+        return tuple(mdependencies...), ()
+    elseif iindex === 1
+        qdependencies = TupleTools.deleteat(nodelocalmarginals, cindex)
+        return (), tuple(qdependencies...) 
+    end
+    
+end
