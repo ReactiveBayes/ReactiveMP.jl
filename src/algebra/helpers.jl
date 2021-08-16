476092602254cfd983233610bc277683ae358633
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-08-16 13:28:48 +0200

    update helpers

--- src/algebra/helpers.jl
+++ src/algebra/helpers.jl
@@ -7,3 +7,15 @@ xT_A_x(x::AbstractVector, A::AbstractMatrix) = dot(x, A * x)
 diageye(::Type{T}, n::Int) where { T <: Real } = Matrix{T}(I, n, n)
 diageye(n::Int)                                = diageye(Float64, n)
 
+function softmax(x::Array{Float64,1})
+
+    m = maximum(x)
+    map((i) -> exp(i-m), x) |> normalize_sum
+
+end
+function normalize_sum(x::Array{Float64,1}) 
+    x ./ sum(x)
+end
+sigmoid(x) = 1/(1+exp(-x))
+
+dtanh(x::Float64) = 1 - tanh(x)^2
