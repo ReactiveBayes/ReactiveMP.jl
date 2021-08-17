0bf6f4cc1c0b931b88ff9838459e269095e82f17
A-Vzer <amaanmv@gmail.com>
2021-08-17 13:09:43 +0200

    added subtraction tests to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -74,12 +74,18 @@ using .ReactiveMPTestingHelpers
 
     addtests("test_node.jl")
     addtests("nodes/test_addition.jl")
+	addtests("nodes/test_subtraction.jl")
     addtests("nodes/test_probit.jl")
 
     addtests("rules/addition/test_in1.jl")
     addtests("rules/addition/test_in2.jl")
     addtests("rules/addition/test_out.jl")
-
+	
+	
+	#addtests("rules/subtraction/test_in1.jl")
+    #addtests("rules/subtraction/test_in2.jl")
+    #addtests("rules/subtraction/test_out.jl")
+	
     addtests("rules/normal_mean_variance/test_out.jl")
     addtests("rules/normal_mean_variance/test_mean.jl")
     addtests("rules/normal_mean_variance/test_var.jl")
