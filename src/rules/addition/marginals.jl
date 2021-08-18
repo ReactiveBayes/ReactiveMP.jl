d7c1cb953e54eeed6aafa94a49524af2765c915a
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-08-18 10:35:59 +0200

    optimize marginal rules addition

--- src/rules/addition/marginals.jl
+++ src/rules/addition/marginals.jl
@@ -7,12 +7,34 @@ end
     return (in1 = m_in1, in2 = prod(ProdAnalytical(), NormalMeanVariance(mean(m_out) - mean(m_in1), var(m_out)), m_in2))
 end
 
-@marginalrule typeof(+)(:in1_in2) (m_out::MultivariateNormalDistributionsFamily, m_in1::MultivariateNormalDistributionsFamily, m_in2::PointMass) = begin
-    return (in1 = prod(ProdAnalytical(), MvNormalMeanCovariance(mean(m_out) - mean(m_in2), cov(m_out)), m_in1), in2 = m_in2)
+@marginalrule typeof(+)(:in1_in2) (m_out::MvNormalMeanCovariance, m_in1::MultivariateNormalDistributionsFamily, m_in2::PointMass) = begin
+    mout, Vout = mean_cov(m_out)
+    return (in1 = prod(ProdAnalytical(), MvNormalMeanCovariance(mout - mean(m_in2), Vout), m_in1), in2 = m_in2)
 end
 
-@marginalrule typeof(+)(:in1_in2) (m_out::MultivariateNormalDistributionsFamily, m_in1::PointMass, m_in2::MultivariateNormalDistributionsFamily) = begin
-    return (in1 = m_in1, in2 = prod(ProdAnalytical(), MvNormalMeanCovariance(mean(m_out) - mean(m_in1), cov(m_out)), m_in2))
+@marginalrule typeof(+)(:in1_in2) (m_out::MvNormalMeanPrecision, m_in1::MultivariateNormalDistributionsFamily, m_in2::PointMass) = begin
+    mout, Wout = mean_precision(m_out)
+    return (in1 = prod(ProdAnalytical(), MvNormalMeanPrecision(mout - mean(m_in2), Wout), m_in1), in2 = m_in2)
+end
+
+@marginalrule typeof(+)(:in1_in2) (m_out::MvNormalWeightedMeanPrecision, m_in1::MultivariateNormalDistributionsFamily, m_in2::PointMass) = begin
+    xiout, Wout = weightedmean_precision(m_out)
+    return (in1 = prod(ProdAnalytical(), MvNormalWeightedMeanPrecision(xiout - Wout*mean(m_in2), Wout), m_in1), in2 = m_in2)
+end
+
+@marginalrule typeof(+)(:in1_in2) (m_out::MvNormalMeanCovariance, m_in1::PointMass, m_in2::MultivariateNormalDistributionsFamily) = begin
+    mout, Vout = mean_cov(m_out)
+    return (in1 = m_in1, in2 = prod(ProdAnalytical(), MvNormalMeanCovariance(mout - mean(m_in1), Vout), m_in2))
+end
+
+@marginalrule typeof(+)(:in1_in2) (m_out::MvNormalMeanPrecision, m_in1::PointMass, m_in2::MultivariateNormalDistributionsFamily) = begin
+    mout, Wout = mean_precision(m_out)
+    return (in1 = m_in1, in2 = prod(ProdAnalytical(), MvNormalMeanPrecision(mout - mean(m_in1), Wout), m_in2))
+end
+
+@marginalrule typeof(+)(:in1_in2) (m_out::MvNormalWeightedMeanPrecision, m_in1::PointMass, m_in2::MultivariateNormalDistributionsFamily) = begin
+    xiout, Wout = weightedmean_precision(m_out)
+    return (in1 = m_in1, in2 = prod(ProdAnalytical(), MvNormalWeightedMeanPrecision(xiout - Wout*mean(m_in1), Wout), m_in2))
 end
 
 @marginalrule typeof(+)(:in1_in2) (m_out::NormalDistributionsFamily, m_in1::NormalDistributionsFamily, m_in2::NormalDistributionsFamily) = begin
