604243ea17ef5a15d9cf30a60542539e8eb8f598
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-08-18 10:23:49 +0200

    extend marginal rules addition node

--- src/rules/addition/marginals.jl
+++ src/rules/addition/marginals.jl
@@ -3,7 +3,19 @@
     return (in1 = prod(ProdAnalytical(), NormalMeanVariance(mean(m_out) - mean(m_in2), var(m_out)), m_in1), in2 = m_in2)
 end
 
-@marginalrule typeof(+)(:in1_in2) (m_out::UnivariateNormalDistributionsFamily, m_in1::UnivariateNormalDistributionsFamily, m_in2::UnivariateNormalDistributionsFamily) = begin
+@marginalrule typeof(+)(:in1_in2) (m_out::UnivariateNormalDistributionsFamily, m_in1::PointMass, m_in2::UnivariateNormalDistributionsFamily) = begin
+    return (in1 = m_in1, in2 = prod(ProdAnalytical(), NormalMeanVariance(mean(m_out) - mean(m_in1), var(m_out)), m_in2))
+end
+
+@marginalrule typeof(+)(:in1_in2) (m_out::MultivariateNormalDistributionsFamily, m_in1::MultivariateNormalDistributionsFamily, m_in2::PointMass) = begin
+    return (in1 = prod(ProdAnalytical(), MvNormalMeanCovariance(mean(m_out) - mean(m_in2), cov(m_out)), m_in1), in2 = m_in2)
+end
+
+@marginalrule typeof(+)(:in1_in2) (m_out::MultivariateNormalDistributionsFamily, m_in1::PointMass, m_in2::MultivariateNormalDistributionsFamily) = begin
+    return (in1 = m_in1, in2 = prod(ProdAnalytical(), MvNormalMeanCovariance(mean(m_out) - mean(m_in1), cov(m_out)), m_in2))
+end
+
+@marginalrule typeof(+)(:in1_in2) (m_out::NormalDistributionsFamily, m_in1::NormalDistributionsFamily, m_in2::NormalDistributionsFamily) = begin
     xi_out, W_out = weightedmean_precision(m_out)
     xi_in1, W_in1 = weightedmean_precision(m_in1)
     xi_in2, W_in2 = weightedmean_precision(m_in2)
