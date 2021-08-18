06e80ebc201ae3419bd795c1aee5edfebe44553c
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-08-18 11:31:54 +0200

    start for marginal tests

---
+++ test/rules/addition/test_marginals.jl
@@ -0,0 +1,23 @@
+module RulesAdditionMarginalsTest
+
+using Test
+using ReactiveMP
+using Random
+import ReactiveMP: @test_marginalrules
+
+@testset "marginalrules:Addition" begin
+
+    @testset ":in1_in2 (m_out::UnivariateNormalDistributionsFamily, m_in1::UnivariateNormalDistributionsFamily, m_in2::PointMass)" begin
+        
+        @test_marginalrules [ with_float_conversions = false ] typeof(+)(:in1_in2) [
+            (
+                input = (m_out = NormalMeanVariance(3, 4), m_in1 = NormalMeanVariance(2, 2), m_in2 = PointMass(2.0)), 
+                output = (in1 = NormalWeightedMeanPrecision(5/4, 3/4), in2 = PointMass(2.0))
+            ),
+
+        ]
+
+
+    end
+end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -77,14 +77,15 @@ using .ReactiveMPTestingHelpers
 	addtests("nodes/test_subtraction.jl")
     addtests("nodes/test_probit.jl")
 
+    addtests("rules/addition/test_marginals.jl")
     addtests("rules/addition/test_in1.jl")
     addtests("rules/addition/test_in2.jl")
     addtests("rules/addition/test_out.jl")
-	
-	
-	#addtests("rules/subtraction/test_in1.jl")
-    #addtests("rules/subtraction/test_in2.jl")
-    #addtests("rules/subtraction/test_out.jl")
+		
+    addtests("rules/subtraction/test_marginals.jl")
+    addtests("rules/subtraction/test_in1.jl")
+    addtests("rules/subtraction/test_in2.jl")
+    addtests("rules/subtraction/test_out.jl")
 	
     addtests("rules/normal_mean_variance/test_out.jl")
     addtests("rules/normal_mean_variance/test_mean.jl")
