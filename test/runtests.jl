03b5182e7df0e218c90b348897c0a61ffa9390fa
Bart van Erp <b.v.erp@tue.nl>
2021-08-24 18:08:23 +0200

    Restructure helper functions for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,30 +14,6 @@ using .ReactiveMPTestingHelpers
 
 @testset ExtendedTestSet "ReactiveMP" begin
 
-    enabled_tests = lowercase.(ARGS)
-
-    function key_to_filename(key)
-        splitted = split(key, ":")
-        return length(splitted) === 1 ? string("test_", first(splitted), ".jl") : string(join(splitted[1:end - 1], "/"), "/test_", splitted[end], ".jl")
-    end
-
-    function filename_to_key(filename)
-        splitted   = split(filename, "/")
-        if length(splitted) === 1
-            return replace(replace(first(splitted), ".jl" => ""), "test_" => "")
-        else
-            path, name = splitted[1:end - 1], splitted[end]
-            return string(join(path, ":"), ":", replace(replace(name, ".jl" => ""), "test_" => ""))
-        end
-    end
-
-    function addtests(filename)
-        key = filename_to_key(filename)
-        if isempty(enabled_tests) || key in enabled_tests
-            include(filename)
-        end
-    end
-
     @testset "Testset helpers" begin
         @test key_to_filename(filename_to_key("distributions/test_normal_mean_variance.jl")) == "distributions/test_normal_mean_variance.jl"
         @test filename_to_key(key_to_filename("distributions:normal_mean_variance")) == "distributions:normal_mean_variance"
@@ -73,8 +49,13 @@ using .ReactiveMPTestingHelpers
     addtests("test_variable.jl")
 
     addtests("test_node.jl")
+    addtests("nodes/flow/test_flow.jl")
     addtests("nodes/test_addition.jl")
     addtests("nodes/test_probit.jl")
+
+    addtests("rules/flow/test_in.jl")
+    addtests("rules/flow/test_out.jl")
+    addtests("rules/flow/test_marginals.jl")
 
     addtests("rules/normal_mean_variance/test_out.jl")
     addtests("rules/normal_mean_variance/test_mean.jl")
--- test/test_helpers.jl
+++ test/test_helpers.jl
@@ -1,3 +1,29 @@
 module ReactiveMPTestingHelpers
 
+export enabled_tests, addtests, key_to_filename, filename_to_key
+
+enabled_tests = lowercase.(ARGS)
+
+function addtests(filename)
+    key = filename_to_key(filename)
+    if isempty(enabled_tests) || key in enabled_tests
+        include(filename)
+    end
+end
+
+function key_to_filename(key)
+    splitted = split(key, ":")
+    return length(splitted) === 1 ? string("test_", first(splitted), ".jl") : string(join(splitted[1:end - 1], "/"), "/test_", splitted[end], ".jl")
+end
+
+function filename_to_key(filename)
+    splitted   = split(filename, "/")
+    if length(splitted) === 1
+        return replace(replace(first(splitted), ".jl" => ""), "test_" => "")
+    else
+        path, name = splitted[1:end - 1], splitted[end]
+        return string(join(path, ":"), ":", replace(replace(name, ".jl" => ""), "test_" => ""))
+    end
+end
+
 end
