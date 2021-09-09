6d8bd11a3e4b09a471f949f7f7809ef74d264781
Bart van Erp <b.v.erp@tue.nl>
2021-09-09 10:06:41 +0200

    add custom logsumexp function

--- src/algebra/helpers.jl
+++ src/algebra/helpers.jl
@@ -1,4 +1,4 @@
-export diageye
+export diageye, logsumexp
 
 import LinearAlgebra: Diagonal
 
@@ -7,3 +7,15 @@ xT_A_x(x::AbstractVector, A::AbstractMatrix) = dot(x, A * x)
 diageye(::Type{T}, n::Int) where { T <: Real } = Matrix{T}(I, n, n)
 diageye(n::Int)                                = diageye(Float64, n)
 
+function logsumexp(x)
+
+    # calculate mean
+    m = maximum(x)
+
+    # define addmax function
+    addmax(y) = y + m
+
+    # perform log-sum-exp trick
+    mapreduce((i) -> exp(i - m), +, x) |> log |> addmax
+    
+end
