49fef34e541bd03162fed06ed1d795bc0967bb75
A-Vzer <amaanmv@gmail.com>
2021-09-11 19:58:54 +0200

    beta tests

---
+++ test/rules/beta/test_out.jl
@@ -0,0 +1,30 @@
+module RulesBetaOutTest
+
+using Test
+using ReactiveMP
+using Random
+import ReactiveMP: @test_rules
+
+@testset "rules:Beta:out" begin
+
+     @testset "Belief Propagation: (m_a::PointMass, m_b::PointMass)" begin
+
+        @test_rules [ with_float_conversions = true ] Beta(:out, Marginalisation) [
+            (input = (m_a = PointMass(1.0), m_b = PointMass(2.0)), output = Beta(1.0, 2.0)),
+            (input = (m_a = PointMass(2.0), m_b = PointMass(2.0)), output = Beta(2.0, 2.0)),
+            (input = (m_a = PointMass(3.0), m_b = PointMass(3.0)), output = Beta(3.0, 3.0)),
+        ]
+
+    end
+
+    @testset "Belief Propagation: (q_a::PointMass, q_b::PointMass)" begin
+
+        @test_rules [ with_float_conversions = true ] Beta(:out, Marginalisation) [
+            (input = (q_a = PointMass(1.0), q_b = PointMass(2.0)), output = Beta(1.0, 2.0)),
+            (input = (q_a = PointMass(2.0), q_b = PointMass(2.0)), output = Beta(2.0, 2.0)),
+            (input = (q_a = PointMass(3.0), q_b = PointMass(3.0)), output = Beta(3.0, 3.0)),
+        ]
+
+    end
+end
+end
