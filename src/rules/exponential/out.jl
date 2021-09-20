04ac566148d9e8e39d58dff2969119601fa2beb8
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-09-20 10:10:46 +0200

    fix typo

--- src/rules/exponential/out.jl
+++ src/rules/exponential/out.jl
@@ -2,5 +2,5 @@ export rule
 using Distributions
 
 @rule Exponential(:out, Marginalisation) (m_in1::PointMass, ) = begin
-    return PointMass(exp(mean(m_in1))
+    return PointMass(exp(mean(m_in1)))
 end
