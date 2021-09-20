bafaa3653faffcdb00b4545b3bfca9258d700deb
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2021-09-20 10:15:15 +0200

    update test

--- test/rules/exponential/test_out.jl
+++ test/rules/exponential/test_out.jl
@@ -8,10 +8,10 @@ import ReactiveMP: @test_rules
 
 @testset "rules:Exponential:out" begin
 
-     @testset "Belief Propagation: (m_in1::PointMass)" begin
+     @testset "Belief Propagation: (m_in1::PointMass, )" begin
 
-        @test_rules [ with_float_conversions = false ] Exponential(:out, Marginalisation) [
-            (input = (m_in1 = PointMass(1.0), ), output = PointMass(1.0)),
+        @test_rules [ with_float_conversions = true ] Exponential(:out, Marginalisation) [
+            (input = (m_in1 = PointMass(1.0), ), output = PointMass(exp(1.0))),
         ]
 
     end
