7c54c5ebe98df23d0b1a9108c44520f5b1aaa252
Bart van Erp <b.v.erp@tue.nl>
2021-09-21 14:42:07 +0200

    fix ambiguity

--- src/algebra/flatten_tuple.jl
+++ src/algebra/flatten_tuple.jl
@@ -1,7 +1,6 @@
 # type stable flatten function for tuples
 flatten_tuple() = ()
-flatten_tuple(a::Tuple) = Tuple(a)
 flatten_tuple(a) = (a,)
 flatten_tuple(a::Tuple, b...) = tuple(flatten_tuple(a...)..., flatten_tuple(b...)...)
 flatten_tuple(a, b...) = tuple(a, flatten_tuple(b...)...)
-flatten_tuple(x::Tuple) = flatten_tuple(x...)
+flatten_tuple(x::Tuple) = tuple(flatten_tuple(x...)...)
