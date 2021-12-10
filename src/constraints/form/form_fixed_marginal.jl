3429e2fad59d6c8ecc93ad2400e705ed0a71d9ba
albertpod <albert.podusenko@gmail.com>
2021-12-10 10:18:29 +0100

    Add fixed marginal form constraint

---
+++ src/constraints/form/form_fixed_marginal.jl
@@ -0,0 +1,27 @@
+export FixedMarginalConstraint
+
+"""
+FixedMarginalConstraint
+
+One of the form constraint objects. 
+Enables fixing the marginal distribution to remain fixed during inference. 
+Can be viewed as blocking of updates of a specific edge associated with the marginal. 
+
+See also: [`constrain_form`](@ref), [`DistProduct`](@ref)
+"""
+
+mutable struct FixedMarginalConstraint <: ReactiveMP.AbstractFormConstraint
+    fixed_value :: Any
+end
+
+default_form_check_strategy(::FixedMarginalConstraint) = FormConstraintCheckLast()
+
+is_point_mass_form_constraint(::FixedMarginalConstraint) = false
+
+function constrain_form(constraint::FixedMarginalConstraint, something)
+    if constraint.fixed_value !== nothing
+        return Message(constraint.fixed_value, false, false)
+    else 
+        return something
+    end
+end 
