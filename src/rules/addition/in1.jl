0cf94daa73b73da491b723f0505cab30e5ef56e2
bartvanerp <44952318+bartvanerp@users.noreply.github.com>
2022-01-03 17:30:59 +0100

    optimize addition/subtraction rules

--- src/rules/addition/in1.jl
+++ src/rules/addition/in1.jl
@@ -1,4 +1,4 @@
-# distritbutions
+# distributions
 @rule typeof(+)(:in1, Marginalisation) (m_out::UnivariateNormalDistributionsFamily, m_in2::UnivariateNormalDistributionsFamily) = begin
     min2, vin2 = mean_var(m_in2)
     mout, vout = mean_var(m_out)
@@ -57,3 +57,34 @@ end
     return MvNormalMeanCovariance(mout - mean(m_in2), vout)
 end
 
+# specialized
+@rule typeof(+)(:in1, Marginalisation) (m_out::MvNormalWeightedMeanPrecision{T1}, m_in2::MvNormalWeightedMeanPrecision{T2}) where { T1 <: LinearAlgebra.BlasFloat, T2 <: LinearAlgebra.BlasFloat } = begin
+
+    min2, vin2 = mean_cov(m_in2)
+    vout = cov(m_out)
+    T = promote_type(T1, T2)
+    BLAS.gemv!('N', -one(T), vout, weightedmean(m_out), one(T), min2)
+    vin2 .+= vout
+    return MvNormalMeanCovariance(min2, vin2)
+
+end
+
+@rule typeof(+)(:in1, Marginalisation) (m_out::MvNormalWeightedMeanPrecision{T1}, m_in2::PointMass) where { T1 } = begin
+
+    ξout, wout = weightedmean_precision(m_out)
+    ξin1 = wout*mean(m_in2)
+    ξin1 .-= ξout
+    T = promote_type(T1, eltype(m_in2))
+    ξin1 .*= -one(T)
+    return MvNormalWeightedMeanPrecision(ξin1, wout)
+
+end
+
+@rule typeof(+)(:in1, Marginalisation) (m_out::PointMass, m_in2::MvNormalWeightedMeanPrecision) = begin
+
+    ξin2, win2 = weightedmean_precision(m_in2)
+    ξin1 = win2*mean(m_out)
+    ξin1 .-= ξin2
+    return MvNormalWeightedMeanPrecision(ξin1, win2)
+
+end
--- src/rules/addition/out.jl
+++ src/rules/addition/out.jl
@@ -57,3 +57,33 @@ end
 @rule typeof(+)(:out, Marginalisation) (m_in1::PointMass, m_in2::MultivariateNormalDistributionsFamily) = begin
     return @call_rule typeof(+)(:out, Marginalisation) (m_in1 = m_in2, m_in2 = m_in1, meta = meta)
 end
+
+# specialized
+@rule typeof(+)(:out, Marginalisation) (m_in1::MvNormalWeightedMeanPrecision{T1}, m_in2::MvNormalWeightedMeanPrecision{T2}) where { T1 <: LinearAlgebra.BlasFloat, T2 <: LinearAlgebra.BlasFloat } = begin
+
+    min2, vin2 = mean_cov(m_in2)
+    vin1 = cov(m_in1)
+    T = promote_type(T1, T2)
+    BLAS.gemv!('N', one(T), vin1, weightedmean(m_in1), one(T), min2)
+    vin2 .+= vin1
+    return MvNormalMeanCovariance(min2, vin2)
+
+end
+
+@rule typeof(+)(:out, Marginalisation) (m_in1::MvNormalWeightedMeanPrecision, m_in2::PointMass) = begin
+
+    ξin1, win1 = weightedmean_precision(m_in1)
+    ξout = win1*mean(m_in2)
+    ξout .+= ξin1
+    return MvNormalWeightedMeanPrecision(ξout, win1)
+
+end
+
+@rule typeof(+)(:out, Marginalisation) (m_in1::PointMass, m_in2::MvNormalWeightedMeanPrecision) = begin
+
+    ξin2, win2 = weightedmean_precision(m_in2)
+    ξout = win2*mean(m_in1)
+    ξout .+= ξin2
+    return MvNormalWeightedMeanPrecision(ξout, win2)
+
+end
