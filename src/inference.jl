7ab593264998f6c406cb6bbf7156f00d2996fe42
Bagaev Dmitry <bvdmitri@gmail.com>
2022-05-20 14:23:25 +0200

    fix: support arbitrary abstract arrays

--- src/inference.jl
+++ src/inference.jl
@@ -14,11 +14,12 @@ assign_message!(variable::AbstractVariable, message)                       = set
 struct KeepEach end
 struct KeepLast end
 
-make_actor(::RandomVariable, ::KeepEach)                           = keep(Marginal)
-make_actor(::Array{ <: RandomVariable, N}, ::KeepEach) where { N } = keep(Array{Marginal, N})
+make_actor(::RandomVariable, ::KeepEach)                            = keep(Marginal)
+make_actor(::Array{ <: RandomVariable, N }, ::KeepEach) where { N } = keep(Array{Marginal, N})
+make_actor(x::AbstractArray{ <: RandomVariable }, ::KeepEach)       = keep(typeof(similar(x, Marginal)))
 
-make_actor(::RandomVariable, ::KeepLast)                            = storage(Marginal)
-make_actor(x::Array{ <: RandomVariable, N}, ::KeepLast) where { N } = buffer(Marginal, size(x))
+make_actor(::RandomVariable, ::KeepLast)                     = storage(Marginal)
+make_actor(x::AbstractArray{ <: RandomVariable}, ::KeepLast) = buffer(Marginal, size(x))
 
 ## Inference ensure update
 
