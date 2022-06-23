9b2abfe56f4eb3b7907f44d7d17d815950a9bbf4
Albert Podusenko <albert.podusenko@gmail.com>
2022-06-23 16:32:53 +0200

    WIP: Update log expectation

--- src/distributions/inverse_wishart.jl
+++ src/distributions/inverse_wishart.jl
@@ -7,9 +7,9 @@ import SpecialFunctions: digamma
 
 function Distributions.mean(::typeof(logdet), distribution::InverseWishart)
     d    = ndims(distribution)
-    wishart = convert(Wishart, dist)
-    ν, S = params(wishart)
-    return mapreduce(i -> digamma((ν + 1 - i) / 2), +, 1:d) + d * log(2) + logdet(S)
+    ν, S = params(distribution)
+    invS = cholinv(S)
+    return mapreduce(i -> -digamma((ν + 1 - i) / 2), +, 1:d) - d * log(2) - logdet(invS)
 end
 
 vague(::Type{<:InverseWishart}, dims::Int) = InverseWishart(dims, inv(Matrix(Diagonal(huge .* ones(dims)))))
@@ -19,7 +19,13 @@ Base.ndims(dist::InverseWishart) = Distributions.dim(dist)
 prod_analytical_rule(::Type{<:Wishart}, ::Type{<:Wishart}) = ProdAnalyticalRuleAvailable()
 
 function prod(::ProdAnalytical, left::InverseWishart, right::InverseWishart)
-    res = prod(ProdAnalytical(), convert(Wishart, left), convert(Wishart, right))
-    ν, S = params(res)
-    return InverseWishart(ν, inv(S))
+    d = dim(left)
+
+    ldf, lS = params(left)
+    rdf, rS = params(right)
+
+    V  = lS + rS |> Hermitian |> Matrix
+    df = ldf + rdf + d + 1
+
+    return InverseWishart(df, V)
 end
