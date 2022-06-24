92fcab94c080e6136e62087ac0ca6dd7dea010a7
Albert Podusenko <albert.podusenko@gmail.com>
2022-06-24 09:50:50 +0200

    Fix average energy for Wishart

--- src/nodes/wishart.jl
+++ src/nodes/wishart.jl
@@ -13,8 +13,8 @@ import Distributions: Wishart
     m_q_out = mean(q_out)
 
     return 0.5 * (
-        m_q_ν * (logdet(m_q_S) + d * log(2)) -
-        logdet(m_q_out) * (m_q_ν - d - 1.0) +
-        tr(cholinv(m_q_S) * m_q_out) + 0.5 * d * (d - 1) * logπ
+        m_q_ν * (mean(logdet, q_S) + d * log(2)) -
+        mean(logdet, q_out) * (m_q_ν - d - 1.0) +
+        tr(mean(cholinv, q_S) * m_q_out) + 0.5 * d * (d - 1) * logπ
     ) + mapreduce(i -> loggamma(0.5 * (m_q_ν + 1.0 - i)), +, 1:d)
 end
