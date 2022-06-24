ce1a6c087fd9e072a607a0c1ae58482e29758ade
Albert Podusenko <albert.podusenko@gmail.com>
2022-06-24 09:24:31 +0200

    WIP update

--- src/nodes/inverse_wishart.jl
+++ src/nodes/inverse_wishart.jl
@@ -13,7 +13,7 @@ import Distributions: InverseWishart
     m_q_out = mean(q_out)
 
     return 0.5 * (
-        m_q_ν * (logdet(m_q_S) + d * log(2)) -
+        m_q_ν * (logdet(m_q_S) - d * log(2)) -
         logdet(m_q_out) * (m_q_ν - d - 1.0) +
         tr(cholinv(m_q_S) * m_q_out) + 0.5 * d * (d - 1) * logπ
     ) + mapreduce(i -> loggamma(0.5 * (m_q_ν + 1.0 - i)), +, 1:d)
