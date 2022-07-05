40e5a8faa0e839c97a63ee381352d51c0ff92e12
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-07-05 16:02:25 +0200

    feat: add factor prod structure (for implemeting independent joint)

--- src/constraints/prod/factor_prod_generic.jl
+++ src/constraints/prod/factor_prod_generic.jl
@@ -5,10 +5,10 @@ import Base: getindex, show
     FactorProduct
 """
 struct FactorProduct{T}
-    multipliers :: T
+    multipliers::T
 end
 
-getmultipliers(product::FactorProduct)  = product.multipliers
+getmultipliers(product::FactorProduct) = product.multipliers
 
 Base.show(io::IO, product::FactorProduct) = print(io, "FactorProduct(", join(getmultipliers(product), ", ", ", "), ")")
 
