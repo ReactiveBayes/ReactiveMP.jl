9fa3f8432bf3569630f0797dbfe639f4eae9537a
Albert Podusenko <albert.podusenko@gmail.com>
2022-07-06 12:33:53 +0200

    Update src/distributions/wishart.jl
    Co-authored-by: Bagaev Dmitry <bvdmitri@gmail.com>

--- src/distributions/wishart.jl
+++ src/distributions/wishart.jl
@@ -13,7 +13,7 @@ end
 
 function Distributions.mean(::typeof(inv), distribution::Wishart)
     ν, S = params(distribution)
-    return ν * inv(S)
+    return ν * cholinv(S)
 end
 
 vague(::Type{<:Wishart}, dims::Int) = Wishart(dims, huge .* diageye(dims))
