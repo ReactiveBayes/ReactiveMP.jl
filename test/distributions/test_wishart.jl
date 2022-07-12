16065dae84ddeb6235e3f4b70a043471295574bf
Bagaev Dmitry <bvdmitri@gmail.com>
2022-07-12 12:47:49 +0200

    style: make format

--- test/distributions/test_wishart.jl
+++ test/distributions/test_wishart.jl
@@ -41,11 +41,11 @@ using LinearAlgebra
         v3 = [8.1 -2.7; -2.7 9.0]
 
         @test prod(ProdAnalytical(), WishartMessage(3, v1), WishartMessage(3, v2)) ≈
-            WishartMessage(3, [4.776325721474591 -1.6199382410125422; -1.6199382410125422 3.3487476649765537])
+              WishartMessage(3, [4.776325721474591 -1.6199382410125422; -1.6199382410125422 3.3487476649765537])
         @test prod(ProdAnalytical(), WishartMessage(4, v1), WishartMessage(4, v3)) ≈
-            WishartMessage(5, [4.261143738311623 -1.5064864332819319; -1.5064864332819319 4.949867121624725])
+              WishartMessage(5, [4.261143738311623 -1.5064864332819319; -1.5064864332819319 4.949867121624725])
         @test prod(ProdAnalytical(), WishartMessage(5, v2), WishartMessage(4, v3)) ≈
-            WishartMessage(6, [4.51459128065395 -1.4750681198910067; -1.4750681198910067 3.129155313351499])
+              WishartMessage(6, [4.51459128065395 -1.4750681198910067; -1.4750681198910067 3.129155313351499])
     end
 
     @testset "ndims" begin
