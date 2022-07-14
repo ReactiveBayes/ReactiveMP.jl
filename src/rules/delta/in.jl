7f775e4aa41382c2e10bc252e929835794ef1c72
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-07-14 09:50:35 +0200

    feat: add CVI out rule for multiple ins interfaces

--- src/rules/delta/in.jl
+++ src/rules/delta/in.jl
@@ -15,16 +15,3 @@ end
 @rule DeltaFn{f}((:in, k), Marginalisation) (q_ins::FactorProduct, m_in::Any, meta::CVIApproximation) where {f} = begin
     return q_ins[k]
 end
-
-# @rule function DeltaFn{f}((:in, k), Marginalisation)
-#     (msg_out, msg_in, meta::CVIApproximation)
-#     η = naturalParams(msg_in)
-#     λ_init = deepcopy(η)
-
-#     logp_nc(z) = (meta.dataset_size / meta.batch_size) * logPdf(msg_out, f(z))
-#     λ = renderCVI(logp_nc, meta.num_iterations, meta.opt, λ_init, msg_in)
-
-#     λ_message = λ - η
-
-#     return standardDist(λ_message)
-# end
