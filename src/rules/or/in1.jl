082065c41428654bfe2b516b692441c6c501f71c
Adamiat Sepideh <adamiat.sepideh@gmail.com>
2022-07-15 11:29:02 +0200

    Add rules for in1 and in2

---
+++ src/rules/or/in1.jl
@@ -0,0 +1,7 @@
+@rule typeof(OR)(:in1, Marginalisation) (
+    m_out::Bernoulli,
+    m_in2::Bernoulli
+) = begin
+    pin2, pout = mean(m_in2), mean(m_out)
+    return Bernoulli(pout/(1 - pin2 + 2*pin2*pout))
+end
---
+++ src/rules/or/in2.jl
@@ -0,0 +1,3 @@
+@rule typeof(OR)(:in2, Marginalisation) (m_out::Any, m_in1::Any, meta::Any) = begin
+    return @call_rule typeof(OR)(:in1, Marginalisation) (m_out = m_out, m_in2 = m_in1, meta = meta)
+end
