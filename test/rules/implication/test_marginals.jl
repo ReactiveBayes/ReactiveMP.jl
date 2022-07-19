ed56cbaf966248d4c6848df33f1d649e6716163c
Albert Podusenko <albert.podusenko@gmail.com>
2022-07-19 12:00:36 +0200

    Make format

--- test/rules/implication/test_marginals.jl
+++ test/rules/implication/test_marginals.jl
@@ -9,14 +9,23 @@ import ReactiveMP: @test_rules, @test_marginalrules
     @testset ":in1_in2 (m_out::Bernoulli, m_in1::Bernoulli, m_in2::Bernoulli)" begin
         @test_marginalrules [with_float_conversions = false] IMPL(:in1_in2) [
             (
-            input = (
-                m_out = Bernoulli(0.5),
-                m_in1 = Bernoulli(0.5),
-                m_in2 = Bernoulli(0.5)
+                input = (
+                    m_out = Bernoulli(0.5),
+                    m_in1 = Bernoulli(0.5),
+                    m_in2 = Bernoulli(0.5)
+                ),
+                output = (Contingency([0.5^3 0.5^3; 0.5^3 0.5^3])
+                )
             ),
-            output = (Contingency([0.5^3 0.5^3; 0.5^3 0.5^3])
+            (
+                input = (
+                    m_out = Bernoulli(0.2),
+                    m_in1 = Bernoulli(0.8),
+                    m_in2 = Bernoulli(0.4)
+                ),
+                output = (Contingency([0.2*0.2*0.6 0.2*0.2*0.4; 0.8*0.8*0.6 0.2*0.8*0.4])
+                )
             )
-        )
         ]
     end
 end
