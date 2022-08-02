80ff7f8bf031383baeaba55fa47f1d2fab344630
Tim <t.n.nisslbeck@tue.nl>
2022-08-02 16:29:38 +0200

    .

--- test/nodes/test_gamma_inverse.jl
+++ test/nodes/test_gamma_inverse.jl
@@ -12,10 +12,10 @@ import ReactiveMP: make_node
 
         @test functionalform(node) === GammaInverse
         @test sdtype(node) === Stochastic()
-        @test name.(interfaces(node)) === (:out, :α, :β)
+        @test name.(interfaces(node)) === (:out, :α, :θ)
         # XXX ?
         @test factorisation(node) === ((1, 2, 3),)
-        @test localmarginalnames(node) === (:out_α_β,)
+        @test localmarginalnames(node) === (:out_α_θ,)
         # XXX ?
         @test metadata(node) === nothing
 
@@ -25,5 +25,20 @@ import ReactiveMP: make_node
     end
 
     # TODO: Average Energy testset
+    @testset "AverageEnergy" begin
+        begin
+            q_out = GammaInverse(2.0, 1.0)
+            q_α = PointMass(2.0)
+            q_θ = PointMass(1.0)
+
+            marginals = (
+                Marginal(q_out, false, false),
+                Marginal(q_α, false, false),
+                Marginal(q_θ, false, false)
+            )
+
+            @test score(AverageEnergy(), GammaInverse, Val{(:out, :α, :θ)}, marginals, nothing) ≈ 1.1299587008097587
+        end
+    end # testset: AverageEnergy
 end # testset
 end # module
