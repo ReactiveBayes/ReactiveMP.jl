dc10dd29365b74b42d793691255e3d624ae0d425
Tim <t.n.nisslbeck@tue.nl>
2022-08-04 17:11:15 +0200

    +test:nodes:gamma_inverse:average_energy

--- src/nodes/gamma_inverse.jl
+++ src/nodes/gamma_inverse.jl
@@ -3,21 +3,10 @@ import SpecialFunctions: loggamma
 @node GammaInverse Stochastic [out, (α, aliases = [shape]), (θ, aliases = [scale])]
 
 # average energy := U[q] = -𝔼_X log pdf(X)
-# TODO one-liner
+# NOTE: support is x ∈ (0,∞), so 𝔼x = mean(q_out) = mean(x) in θ / 𝔼x will not be undefined
+# NOTE: negative sign in average energy has already been incorporated in the result
 @average_energy GammaInverse (q_out::GammaInverse, q_α::PointMass, q_θ::PointMass) = begin
     # α̂ and β̂ after δ-dirac: mean of pointmasses
     (α, θ) = (mean(q_α), mean(q_θ))
-
-    energy = 0
-    energy += α * log(θ)
-    energy -= loggamma(α)
-    # 𝔼 log X
-    energy -= (α + 1) * mean(log, q_out)
-    # 𝔼 X
-    # TODO: mean(q_out) ?= 0
-    energy -= θ / mean(q_out)
-    # minus sign in front of 𝔼
-    # NOTE: you can also switch all signs above and remove this line
-    energy *= -1
-    return energy
+    return -α * log(θ) + loggamma(α) + (α + 1) * mean(log, q_out) + θ / mean(q_out)
 end
--- test/rules/gamma_inverse/test_out.jl
+++ test/rules/gamma_inverse/test_out.jl
@@ -10,15 +10,32 @@ import ReactiveMP: @test_rules
 @testset "rules:GammaInverse:out" begin
     @testset "Belief Propagation: (m_α::Any, m_θ::Any)" begin
         @test_rules [with_float_conversions = true] GammaInverse(:out, Marginalisation) [
-            (input = (m_α = PointMass(1.0), m_θ = PointMass(2.0)), output = GammaInverse(1.0, 2.0)),
-            (input = (m_α = PointMass(1.0), m_θ = PointMass(2.0)), output = GammaInverse(1.0, 2.0))
+            (
+                input = (
+                    m_α = PointMass(1.0),
+                    m_θ = PointMass(2.0)
+                ),
+                output = GammaInverse(1.0, 2.0)
+            )
         ]
     end
 
     @testset "Variational Message Passing: (q_α::Any, q_θ::Any)" begin
         @test_rules [with_float_conversions = true] GammaInverse(:out, Marginalisation) [
-            (input = (q_α = PointMass(1.0), q_θ = PointMass(2.0)), output = GammaInverse(1.0, 2.0)),
-            (input = (q_α = Gamma(1.0, 1.0), q_θ = Beta(1.0, 1.0)), output = GammaInverse(1.0, 0.5))
+            (
+                input = (
+                    q_α = PointMass(1.0),
+                    q_θ = PointMass(2.0)
+                ),
+                output = GammaInverse(1.0, 2.0)
+            ),
+            (
+                input = (
+                    q_α = Gamma(1.0, 1.0),
+                    q_θ = Beta(1.0, 1.0)
+                ),
+                output = GammaInverse(1.0, 0.5)
+            )
         ]
     end
 end
