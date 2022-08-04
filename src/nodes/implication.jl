afa9870493be3fcb32852905bc6b86fd83236728
Albert Podusenko <albert.podusenko@gmail.com>
2022-08-04 18:15:09 +0200

    Add descriptions

--- src/nodes/implication.jl
+++ src/nodes/implication.jl
@@ -2,4 +2,11 @@ export IMPLY
 
 struct IMPLY end
 
+``` IMPY node implements implication function that can be desribed by the followsing table:
+| in1  in2 | out |
+|  0    0  |  1  |
+|  0    1  |  1  |
+|  1    0  |  0  |
+|  1    1  |  1  |
+```
 @node IMPLY Deterministic [out, in1, in2]
