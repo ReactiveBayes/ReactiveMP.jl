bb38a3584e59a880b43e6b70c4cc839322605ca3
Bagaev Dmitry <bvdmitri@gmail.com>
2022-08-05 15:06:58 +0200

    style: make format

--- test/models/test_aliases.jl
+++ test/models/test_aliases.jl
@@ -9,13 +9,12 @@ using BenchmarkTools, Random, Plots, Dates, LinearAlgebra, StableRNGs
 ## Model definition
 ## -------------------------------------------- ##
 @model function normal_aliases()
-    
     x1 ~ MvNormal(μ = zeros(2), Σ⁻¹ = diageye(2))
     x2 ~ MvNormal(μ = zeros(2), Λ = diageye(2))
     x3 ~ MvNormal(mean = zeros(2), W = diageye(2))
     x4 ~ MvNormal(μ = zeros(2), prec = diageye(2))
     x5 ~ MvNormal(m = zeros(2), precision = diageye(2))
-    
+
     y1 ~ MvNormal(mean = zeros(2), Σ = diageye(2))
     y2 ~ MvNormal(m = zeros(2), Λ⁻¹ = diageye(2))
     y3 ~ MvNormal(μ = zeros(2), V = diageye(2))
@@ -24,7 +23,7 @@ using BenchmarkTools, Random, Plots, Dates, LinearAlgebra, StableRNGs
 
     x ~ x1 + x2 + x3 + x4 + x5
     y ~ y1 + y2 + y3 + y4 + y5
-    
+
     r1 ~ Normal(μ = dot(x + y, ones(2)), τ = 1.0)
     r2 ~ Normal(m = r1, γ = 1.0)
     r3 ~ Normal(mean = r2, σ⁻² = 1.0)
@@ -32,13 +31,13 @@ using BenchmarkTools, Random, Plots, Dates, LinearAlgebra, StableRNGs
     r5 ~ Normal(mean = r4, p = 1.0)
     r6 ~ Normal(mean = r5, prec = 1.0)
     r7 ~ Normal(mean = r6, precision = 1.0)
-    
+
     s1 ~ Normal(μ = r7, σ² = 1.0)
     s2 ~ Normal(m = s1, τ⁻¹ = 1.0)
     s3 ~ Normal(mean = s2, v = 1.0)
     s4 ~ Normal(mean = s3, var = 1.0)
     s5 ~ Normal(mean = s4, variance = 1.0)
-    
+
     d = datavar(Float64)
     d ~ Normal(μ = s5, variance = 1.0)
 end
@@ -48,9 +47,9 @@ end
     x2 ~ Bernoulli(0.5)
     x3 ~ Bernoulli(0.5)
     x4 ~ Bernoulli(0.5)
-    
+
     x ~ x1 -> x2 && x3 || ¬x4
-    
+
     y = datavar(Float64)
     x ~ Bernoulli(y)
 end
@@ -60,8 +59,8 @@ end
 function normal_aliases_inference()
     return inference(
         model = Model(normal_aliases),
-        data = (d = 1.0, ),
-        returnvars = (x1 = KeepLast(), ),
+        data = (d = 1.0,),
+        returnvars = (x1 = KeepLast(),),
         free_energy = true
     )
 end
@@ -69,7 +68,7 @@ end
 function binary_aliases_inference()
     return inference(
         model = Model(binary_aliases),
-        data = (y = 0.5, ),
+        data = (y = 0.5,),
         free_energy = true
     )
 end
@@ -90,7 +89,7 @@ end
         @test first(result.free_energy) ≈ 2.6823450222125444
 
         @test mean(result.posteriors[:x1]) ≈ 0.5
-        @test first(result.free_energy) ≈ 0.6931471805599454    
+        @test first(result.free_energy) ≈ 0.6931471805599454
     end
 end
 
