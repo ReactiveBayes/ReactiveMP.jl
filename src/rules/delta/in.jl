fa60aab738520e0fe5026699d6a923abc87cccd0
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-08-16 13:30:51 +0200

    fix: delete rules for another approximations

--- src/rules/delta/in.jl
+++ src/rules/delta/in.jl
@@ -4,14 +4,6 @@ using Random
     return ContinuousUnivariateLogPdf((x) -> logpdf(m_in, f(x)))
 end
 
-@rule DeltaFn{f}((:in, k), Marginalisation) (
-    q_ins::Any,
-    m_in::Any,
-    meta::LinearApproximationKnownInverse
-) where {f} = begin
-    return NormalMeanVariance(0, 1)
-end
-
 @rule DeltaFn{f}((:in, k), Marginalisation) (q_ins::FactorProduct, m_in::Any, meta::CVIApproximation) where {f} = begin
     return standardDist(naturalParams(q_ins[k]) - naturalParams(m_in))
 end
--- src/rules/delta/marginals.jl
+++ src/rules/delta/marginals.jl
@@ -1,9 +1,5 @@
 using TupleTools
 
-@marginalrule DeltaFn{f}(:ins) (q_out::Any, m_ins::NTuple{N, Any}, meta::SamplingApproximation) where {f, N} = begin
-    return MvNormalMeanPrecision(zeros(N), diageye(N))
-end
-
 @marginalrule DeltaFn{f}(:ins) (m_out::Any, m_ins::NTuple{1, Any}, meta::CVIApproximation) where {f} = begin
     η = naturalParams(m_ins[1])
     logp_nc(z) = logpdf(m_out, f(z))
