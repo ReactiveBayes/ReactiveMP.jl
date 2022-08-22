6ec947d2d2cd2f699af503c4e09b4b78c4cfbbef
Bagaev Dmitry <bvdmitri@gmail.com>
2022-08-22 11:06:11 +0200

    fix tests

--- test/distributions/test_wishart.jl
+++ test/distributions/test_wishart.jl
@@ -38,16 +38,19 @@ import ReactiveMP: WishartMessage
     end
 
     @testset "prod" begin
-        v1 = [9.0 -3.4; -3.4 11.0]
-        v2 = [10.2 -3.3; -3.3 5.0]
-        v3 = [8.1 -2.7; -2.7 9.0]
+        inv_v1 = cholinv([9.0 -3.4; -3.4 11.0])
+        inv_v2 = cholinv([10.2 -3.3; -3.3 5.0])
+        inv_v3 = cholinv([8.1 -2.7; -2.7 9.0])
 
-        @test prod(ProdAnalytical(), WishartMessage(3, v1), WishartMessage(3, v2)) ≈
-              WishartMessage(3, [4.776325721474591 -1.6199382410125422; -1.6199382410125422 3.3487476649765537])
-        @test prod(ProdAnalytical(), WishartMessage(4, v1), WishartMessage(4, v3)) ≈
-              WishartMessage(5, [4.261143738311623 -1.5064864332819319; -1.5064864332819319 4.949867121624725])
-        @test prod(ProdAnalytical(), WishartMessage(5, v2), WishartMessage(4, v3)) ≈
-              WishartMessage(6, [4.51459128065395 -1.4750681198910067; -1.4750681198910067 3.129155313351499])
+        @test prod(ProdAnalytical(), WishartMessage(3, inv_v1), WishartMessage(3, inv_v2)) ≈
+              WishartMessage(
+            3,
+            cholinv([4.776325721474591 -1.6199382410125422; -1.6199382410125422 3.3487476649765537])
+        )
+        @test prod(ProdAnalytical(), WishartMessage(4, inv_v1), WishartMessage(4, inv_v3)) ≈
+              WishartMessage(5, cholinv([4.261143738311623 -1.5064864332819319; -1.5064864332819319 4.949867121624725]))
+        @test prod(ProdAnalytical(), WishartMessage(5, inv_v2), WishartMessage(4, inv_v3)) ≈
+              WishartMessage(6, cholinv([4.51459128065395 -1.4750681198910067; -1.4750681198910067 3.129155313351499]))
     end
 
     @testset "ndims" begin
