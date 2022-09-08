96aa237585a96f44b17095a9b8182ec6dc290e36
polym (Tim) <nisslbeck.tim@gmail.com>
2022-09-08 12:27:49 +0200

    Update src/rules/gamma_inverse/out.jl
    Co-authored-by: Bagaev Dmitry <bvdmitri@gmail.com>

--- src/rules/gamma_inverse/out.jl
+++ src/rules/gamma_inverse/out.jl
@@ -2,7 +2,7 @@ export rule
 
 # Belief propagation
 # case: inbound messages on edges [α, θ] sum-product
-@rule GammaInverse(:out, Marginalisation) (m_α::Any, m_θ::Any) = GammaInverse(mean(m_α), mean(m_θ))
+@rule GammaInverse(:out, Marginalisation) (m_α::PointMass, m_θ::PointMass) = GammaInverse(mean(m_α), mean(m_θ))
 
 # Variational MP
 # case: inbound variational posterior on edges [α, θ]
