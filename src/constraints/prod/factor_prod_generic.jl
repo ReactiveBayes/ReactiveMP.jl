3f8322399812eab659118b9bde7118468218fa1b
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-09-26 16:41:06 +0200

    docs: add FactorProduct docs

--- src/constraints/prod/factor_prod_generic.jl
+++ src/constraints/prod/factor_prod_generic.jl
@@ -4,6 +4,11 @@ import Base: getindex, show, length
 
 """
     FactorProduct
+
+If a rule needs to send a density of the form d1d2...di...dn, it may use `FactorProduct` structure.
+You can create FactorProduct in the following way `FactorProduct((d1, d2, ...))`: the constructor expects to obtain a tuple.
+It is supposed that `entropy` is implemented for the tuple elements (d1, d2, ...): entropy of `FactorProduct` is the sum of input entropies.
+You can access each individual (di) from a factor product (`factor_product = FactorProduct((d1, d2,..., di, ...))`) in the following way `factor_product[i]`.
 """
 struct FactorProduct{T}
     multipliers::T
