24db5d319a3392f88dfa018fa7d6dcfdf303eab1
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-09-27 16:56:53 +0200

    style: :art:

--- test/rules/delta/cvi/test_out.jl
+++ test/rules/delta/cvi/test_out.jl
@@ -13,7 +13,10 @@ struct EmptyOptimizer end
 @testset "rules:Delta:cvi:out" begin
     seed = 123
     rng = MersenneTwister(seed)
-    output = @call_rule DeltaFn{g}(:out, Marginalisation) (q_ins = FactorProduct((NormalMeanVariance(0, tiny),)), meta = CVIApproximation(rng, 1000, 1, EmptyOptimizer))
+    output = @call_rule DeltaFn{g}(:out, Marginalisation) (
+        q_ins = FactorProduct((NormalMeanVariance(0, tiny),)),
+        meta = CVIApproximation(rng, 1000, 1, EmptyOptimizer)
+    )
     @test mean(output.dist) ≈ 2.5797499734233424e-8
 end # testset
 end # module
