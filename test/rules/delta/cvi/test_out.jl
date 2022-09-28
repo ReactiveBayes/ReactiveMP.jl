e866be7f3affb2ee0b3cb18622fa00ba1305812a
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-09-28 11:26:39 +0200

    refactor: cvi out tests function to incapsulate the test logic

--- test/rules/delta/cvi/test_out.jl
+++ test/rules/delta/cvi/test_out.jl
@@ -13,52 +13,27 @@ square_sum(x, y) = x^2 + y^2
 
 struct EmptyOptimizer end
 
+function cvi_out_test(func::Function, factor_product::FactorProduct, meta::CVIApproximation, output)
+    sample_list_output = @call_rule DeltaFn{func}(:out, Marginalisation) (
+        q_ins = factor_product,
+        meta = meta
+    )
+    @test mean(sample_list_output.dist) ≈ output
+end
+
 # test this set with $ make test testset='rules:gamma_inverse:out'
 @testset "rules:Delta:cvi:out" begin
     seed = 123
     rng = MersenneTwister(seed)
     test_meta = CVIApproximation(rng, 100, 1, EmptyOptimizer)
 
-    sample_list_output = @call_rule DeltaFn{id}(:out, Marginalisation) (
-        q_ins = FactorProduct((PointMass(0),)),
-        meta = test_meta
-    )
-    @test mean(sample_list_output.dist) ≈ 0
-
-    sample_list_output = @call_rule DeltaFn{id}(:out, Marginalisation) (
-        q_ins = FactorProduct((PointMass([0, 0]),)),
-        meta = test_meta
-    )
-    @test mean(sample_list_output.dist) ≈ [0, 0]
-
-    sample_list_output = @call_rule DeltaFn{second_argument}(:out, Marginalisation) (
-        q_ins = FactorProduct((PointMass(0), PointMass(1))),
-        meta = test_meta
-    )
-    @test mean(sample_list_output.dist) ≈ 1
-
-    sample_list_output = @call_rule DeltaFn{second_argument}(:out, Marginalisation) (
-        q_ins = FactorProduct((PointMass([0, 0]), PointMass(1))),
-        meta = test_meta
-    )
-    @test mean(sample_list_output.dist) ≈ 1
-
-    sample_list_output = @call_rule DeltaFn{first_argument}(:out, Marginalisation) (
-        q_ins = FactorProduct((PointMass([0, 0]), PointMass(1))),
-        meta = test_meta
-    )
-    @test mean(sample_list_output.dist) ≈ [0, 0]
-
-    sample_list_output = @call_rule DeltaFn{second_argument}(:out, Marginalisation) (
-        q_ins = FactorProduct((NormalMeanVariance(), PointMass(1))),
-        meta = test_meta
-    )
-    @test mean(sample_list_output.dist) ≈ 1
-
-    sample_list_output = @call_rule DeltaFn{square_sum}(:out, Marginalisation) (
-        q_ins = FactorProduct((PointMass(0), PointMass(2))),
-        meta = test_meta
-    )
-    @test mean(sample_list_output.dist) ≈ 4
+    cvi_out_test(id, FactorProduct((PointMass(0),)), test_meta, 0)
+    cvi_out_test(id, FactorProduct((PointMass([0, 0]),)), test_meta, [0, 0])
+    cvi_out_test(first_argument, FactorProduct((PointMass([0, 0]), PointMass(1))), test_meta, [0, 0])
+    cvi_out_test(first_argument, FactorProduct((PointMass(0), PointMass(1))), test_meta, 0)
+    cvi_out_test(second_argument, FactorProduct((PointMass(0), PointMass(1))), test_meta, 1)
+    cvi_out_test(second_argument, FactorProduct((PointMass([0, 0]), PointMass(1))), test_meta, 1)
+    cvi_out_test(second_argument, FactorProduct((NormalMeanVariance(), PointMass(1))), test_meta, 1)
+    cvi_out_test(square_sum, FactorProduct((PointMass(0), PointMass(2))), test_meta, 4)
 end # testset
 end # module
