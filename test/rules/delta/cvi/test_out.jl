28d24f7b8d415e78de0c2d3a4c8f80144110b833
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-09-28 11:15:13 +0200

    test: add test for CVI out rule

--- test/rules/delta/cvi/test_out.jl
+++ test/rules/delta/cvi/test_out.jl
@@ -6,17 +6,59 @@ using Random
 using Distributions
 using StableRNGs
 
-g(x) = x
+id(x) = x
+first_argument(x, y) = x
+second_argument(x, y) = y
+square_sum(x, y) = x^2 + y^2
+
 struct EmptyOptimizer end
 
 # test this set with $ make test testset='rules:gamma_inverse:out'
 @testset "rules:Delta:cvi:out" begin
     seed = 123
     rng = MersenneTwister(seed)
-    output = @call_rule DeltaFn{g}(:out, Marginalisation) (
-        q_ins = FactorProduct((NormalMeanVariance(0, tiny),)),
-        meta = CVIApproximation(rng, 1000, 1, EmptyOptimizer)
+    test_meta = CVIApproximation(rng, 100, 1, EmptyOptimizer)
+
+    sample_list_output = @call_rule DeltaFn{id}(:out, Marginalisation) (
+        q_ins = FactorProduct((PointMass(0),)),
+        meta = test_meta
     )
-    @test mean(output.dist) ≈ 2.5797499734233424e-8
+    @test mean(sample_list_output.dist) ≈ 0
+
+    sample_list_output = @call_rule DeltaFn{id}(:out, Marginalisation) (
+        q_ins = FactorProduct((PointMass([0, 0]),)),
+        meta = test_meta
+    )
+    @test mean(sample_list_output.dist) ≈ [0, 0]
+
+    sample_list_output = @call_rule DeltaFn{second_argument}(:out, Marginalisation) (
+        q_ins = FactorProduct((PointMass(0), PointMass(1))),
+        meta = test_meta
+    )
+    @test mean(sample_list_output.dist) ≈ 1
+
+    sample_list_output = @call_rule DeltaFn{second_argument}(:out, Marginalisation) (
+        q_ins = FactorProduct((PointMass([0, 0]), PointMass(1))),
+        meta = test_meta
+    )
+    @test mean(sample_list_output.dist) ≈ 1
+
+    sample_list_output = @call_rule DeltaFn{first_argument}(:out, Marginalisation) (
+        q_ins = FactorProduct((PointMass([0, 0]), PointMass(1))),
+        meta = test_meta
+    )
+    @test mean(sample_list_output.dist) ≈ [0, 0]
+
+    sample_list_output = @call_rule DeltaFn{second_argument}(:out, Marginalisation) (
+        q_ins = FactorProduct((NormalMeanVariance(), PointMass(1))),
+        meta = test_meta
+    )
+    @test mean(sample_list_output.dist) ≈ 1
+
+    sample_list_output = @call_rule DeltaFn{square_sum}(:out, Marginalisation) (
+        q_ins = FactorProduct((PointMass(0), PointMass(2))),
+        meta = test_meta
+    )
+    @test mean(sample_list_output.dist) ≈ 4
 end # testset
 end # module
