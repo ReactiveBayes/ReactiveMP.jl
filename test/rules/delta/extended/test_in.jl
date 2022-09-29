aabbcfd562a94877ab38f1a8eee602cd6a2e22d3
Chengfeng-Jia <c.jia@tue.nl>
2022-09-29 22:37:02 +0200

    add in test

--- test/rules/delta/extended/test_in.jl
+++ test/rules/delta/extended/test_in.jl
@@ -20,14 +20,44 @@ h_inv_x(z::Vector, y::Vector) = sqrt.(z .+ y)
 
 # g provided in a similar syntax like the N parameter in normal_mixture/test_out.jl
 # normal_mixture is the only example with this syntax (that has a test; gamma_mixture is another candidate but ∄ test)
+
 @testset "rules:Delta:extended:in" begin
-    @testset "Belief Propagation: f(x) (m_ins::NormalMeanCovariance, meta::DeltaExtended) (with known inverse)" begin
-        @test_rules [with_float_conversions = false] DeltaFn{g}((:in, k = 1), Marginalisation) [
+    @testset "Belief Propagation: f(x) (m_ins::NormalMeanVariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{g}(:in, Marginalisation) [
             (
-            input = (m_out = NormalMeanVariance(0.0, 1.0), m_ins = nothing, meta = DeltaExtended(inverse = g_inv)),
-            output = NormalMeanVariance(2.23606797749979, 0.05) # TODO: double check this
+            input = (m_out = ManyOf(NormalMeanVariance(2.0, 3.0),NormalMeanVariance(2.0, 1.0)), meta = DeltaExtended(inverse = nothing)),
+            output = NormalMeanPrecision(14.666666666666666, 5.333333333333333)
         )
         ]
     end
+
+    @testset "Belief Propagation: f(x): (m_ins::MvNormalMeanCovariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{g}(:in, Marginalisation) [
+            (
+            input = (m_out = ManyOf(MvNormalMeanCovariance([2.0], [3.0]),MvNormalMeanCovariance([2.0], [1.0])), meta = DeltaExtended(inverse = nothing)),
+            output = MvNormalMeanPrecision([14.666666666666666], [5.333333333333335])
+        )
+        ]
+    end
+
+    @testset "Belief Propagation: f(x,y) (m_ins::NormalMeanVariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{h}(:in, Marginalisation) [
+            (
+            input = (m_out = ManyOf(NormalMeanVariance(2.0, 3.0), NormalMeanVariance(2.0, 1.0),NormalMeanVariance(5.0, 1.0)), meta = DeltaExtended(inverse = nothing)),
+            output = NormalMeanPrecision(10.999999999999996, 3.9999999999999982)
+        )
+        ]
+    end
+
+    @testset "Belief Propagation: f(x,y) (m_ins::MvNormalMeanCovariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{h}(:in, Marginalisation) [
+            (
+            input = (m_out = ManyOf(MvNormalMeanCovariance([2.0], [3.0])), MvNormalMeanCovariance([2.0], [1.0]),MvNormalMeanCovariance([5.0], [1.0])), meta = DeltaExtended()),
+            output = MvNormalMeanPrecision([10.999999999999996], [3.9999999999999982])
+        )
+        ]
+    end
+
+   
 end # testset
 end # module
--- test/rules/delta/unscented/test_in.jl
+++ test/rules/delta/unscented/test_in.jl
@@ -21,15 +21,53 @@ h_inv_x(z::Vector{Float64}, y::Vector{Float64}) = sqrt.(z .+ y)
 
 # g provided in a similar syntax like the N parameter in normal_mixture/test_out.jl
 # normal_mixture is the only example with this syntax (that has a test; gamma_mixture is another candidate but ∄ test)
+
 @testset "rules:Delta:unscented:in" begin
-    # ForneyLab:test_delta_unscented:SPDeltaUTIn1GG 1-2
-    @testset "Belief Propagation: f(x) (m_ins::NormalMeanCovariance, meta.inverse::Nothing)" begin
-        @test_rules [with_float_conversions = false] DeltaFn{g}((:in, k), Marginalisation) [
+    @testset "Belief Propagation: f(x) (m_ins::NormalMeanVariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{g}(:in, Marginalisation) [
             (
-            input = (m_out = ManyOf(NormalMeanVariance(2.0, 3.0)), m_ins = ManyOf(NormalMeanVariance(2.0, 1.0)), meta = DeltaUnscented(inverse = nothing)),
-            output = NormalMeanVariance(2.499999999868301, 0.3125000002253504)
+                input = (m_out = ManyOf(NormalMeanVariance(2.0, 3.0),NormalMeanVariance(2.0, 1.0)), meta = DeltaUnscented()),
+                output = NormalMeanVariance(2.499999999868301, 0.3125000002253504)
+            ),
+            (
+                input = (m_out = ManyOf(NormalMeanVariance(2.0, 3.0),NormalMeanVariance(2.0, 1.0)), meta = DeltaUnscented(alpha = 1.0)),
+                output = NormalMeanVariance(2.5, 0.3125)
+            )
+        ]
+    end
+    @testset "Belief Propagation: f(x): (m_ins::MvNormalMeanCovariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{g}(:in, Marginalisation) [
+            (
+                input = (m_out = ManyOf(MvNormalMeanCovariance([2.0], [3.0]),NormalMeanVariance([2.0], [1.0])), meta = DeltaUnscented()),
+                output = MvNormalMeanCovariance([2.499999999868301], [0.31250000021807445])
+            ),
+            (
+                input = (m_out = ManyOf(MvNormalMeanCovariance([2.0], [3.0]),NormalMeanVariance([2.0], [1.0])), meta = DeltaUnscented(alpha = 1.0)),
+                output = MvNormalMeanCovariance([2.5], [0.3125])
+            )
+        ]
+    end
+
+    @test ruleSPDeltaUTInGX(h, 1, Message(Univariate, Gaussian{Moments}, m=2.0, v=3.0), Message(Univariate, Gaussian{Moments}, m=2.0, v=1.0), Message(Univariate, Gaussian{Moments}, m=5.0, v=1.0)) == Message(Univariate, Gaussian{Canonical}, xi=6.666665554127903, w=2.6666662216903676)
+    @test ruleSPDeltaUTInGX(h, 1, Message(Multivariate, Gaussian{Moments}, m=[2.0], v=[3.0]), Message(Multivariate, Gaussian{Moments}, m=[2.0], v=[1.0]), Message(Multivariate, Gaussian{Moments}, m=[5.0], v=[1.0])) == Message(Multivariate, Gaussian{Canonical}, xi=[6.666665554127903], w=[2.666666221690368])
+    
+    @testset "Belief Propagation: f(x,y) (m_ins::NormalMeanVariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{h}(:in, Marginalisation) [
+            (
+            input = (m_out = ManyOf(NormalMeanVariance(2.0, 3.0), NormalMeanVariance(2.0, 1.0),NormalMeanVariance(5.0, 1.0)), meta = DeltaUnscented(inverse = nothing)),
+            output = NormalMeanPrecision(6.666665554127903, 2.6666662216903676)
         )
         ]
     end
+
+    @testset "Belief Propagation: f(x,y) (m_ins::MvNormalMeanCovariance, *)" begin
+        @test_rules [with_float_conversions = false] DeltaFn{h}(:in, Marginalisation) [
+            (
+            input = (m_out = ManyOf(MvNormalMeanCovariance([2.0], [3.0]), MvNormalMeanCovariance([2.0], [1.0]),MvNormalMeanCovariance([5.0], [1.0])), meta = DeltaExtended()),
+            output = MvNormalMeanPrecision([6.666665554127903], [2.666666221690368])
+        )
+        ]
+    end
+
 end # testset
 end # module
