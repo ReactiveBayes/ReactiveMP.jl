69d9e3884acc49c6936a48db15ac5625d8fb6a3b
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-10-04 11:37:47 +0200

    test: add @test_marginalrules test for cvi

--- test/rules/delta/cvi/test_marginals.jl
+++ test/rules/delta/cvi/test_marginals.jl
@@ -6,6 +6,8 @@ using Random
 using Distributions
 using Flux
 
+import ReactiveMP: @test_marginalrules
+
 id(x) = x
 
 @testset "marginalrules:CVI" begin
@@ -14,11 +16,23 @@ id(x) = x
         rng = MersenneTwister(seed)
         optimizer = Descent(0.01)
         test_meta = CVIApproximation(rng, 1, 500, optimizer)
-        
+
         m_in = NormalMeanVariance()
-        marginals = @call_marginalrule DeltaFn{id}(:ins) (m_out = NormalMeanVariance(1, 1), m_ins = ManyOf(m_in,), meta = test_meta)
+        m_out = NormalMeanVariance(1, 1)
+        marginals = @call_marginalrule DeltaFn{id}(:ins) (m_out = NormalMeanVariance(1, 1), m_ins = ManyOf(m_in), meta = test_meta)
+
+        @show marginals
+        @show naturalparams(marginals[1])
+        @show naturalparams(m_in) + naturalparams(m_out)
+
         result_marginal = convert(Distribution, naturalparams(marginals[1]) - naturalparams(m_in))
-        @test isapprox(NormalMeanVariance(mean(result_marginal), var(result_marginal)), NormalMeanVariance(1, 1), atol = 0.1)
+
+        @test_marginalrules DeltaFn{id}(:ins) [
+            (
+                input = (m_out = NormalMeanVariance(1, 1), m_ins = ManyOf(NormalMeanVariance()), meta = test_meta),
+                output = (out = convert(Distribution, naturalparams(m_in) + naturalparams(m_out)),)
+            )
+        ]
     end
 end
 end
