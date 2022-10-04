f2db6328c43524dee98c10ce3ea926aa84541a95
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-10-04 11:16:57 +0200

    feat: add isapprox for FactorProduct

--- src/constraints/prod/factor_prod_generic.jl
+++ src/constraints/prod/factor_prod_generic.jl
@@ -1,6 +1,6 @@
 export FactorProduct
 import Distributions: entropy
-import Base: getindex, show, length
+import Base: isapprox, getindex, show, length
 
 """
     FactorProduct
@@ -9,6 +9,7 @@ If a rule needs to send a density of the form d1d2...di...dn, it may use `Factor
 You can create FactorProduct in the following way `FactorProduct((d1, d2, ...))`: the constructor expects to obtain a tuple.
 It is supposed that `entropy` is implemented for the tuple elements (d1, d2, ...): entropy of `FactorProduct` is the sum of input entropies.
 You can access each individual (di) from a factor product (`factor_product = FactorProduct((d1, d2,..., di, ...))`) in the following way `factor_product[i]`.
+It possible to compare factor products with `isapprox`, it supposed that if one will call it `fc1 ≈ fc2` (`fc1 (d1d2...di..dn)`, `fc2 (b1b2..bi..bn)`) each multiplier on the same position (`bi, di`) can be compared with `isapprox`.
 """
 struct FactorProduct{T}
     multipliers::T
@@ -21,5 +22,7 @@ Base.show(io::IO, product::FactorProduct) = print(io, "FactorProduct(", join(get
 Base.getindex(product::FactorProduct, i::Int) = product.multipliers[i]
 
 Base.length(product::FactorProduct) = length(product.multipliers)
+
+Base.isapprox(x::FactorProduct, y::FactorProduct; kwargs...) = all(pair -> isapprox(pair[1], pair[2]; kwargs...), zip(getmultipliers(x), getmultipliers(y)))
 
 Distributions.entropy(product::FactorProduct) = mapreduce(entropy, +, getmultipliers(product))
--- test/test_factor_product.jl
+++ test/test_factor_product.jl
@@ -5,6 +5,10 @@ using ReactiveMP
 using Distributions
 using Random
 
+function isapprox_test(x::FactorProduct, y::FactorProduct, output::Bool; atol::Real = 0)
+    @test isapprox(x, y, atol = atol) === output
+end
+
 @testset "FactorProduct" begin
 
     # Bernoulli comes from Distributions.jl and most of the things should be covered there
@@ -21,6 +25,22 @@ using Random
     @testset "entopy" begin
         product = FactorProduct(multipliers)
         @test entropy(product) ≈ mapreduce(entropy, +, multipliers)
+    end
+
+    @testset "isapprox" begin
+        @test FactorProduct((NormalMeanVariance(),)) ≈ FactorProduct((NormalMeanVariance(),))
+        @test !(FactorProduct((NormalMeanVariance(),)) ≈ FactorProduct((NormalMeanVariance(1, 1),)))
+        isapprox_test(
+            FactorProduct((vague(Gamma), NormalMeanVariance())),
+            FactorProduct((Gamma(1.000001, 1.0e12), NormalMeanVariance())),
+            true,
+            atol = 0.00001
+        )
+        isapprox_test(
+            FactorProduct((vague(Gamma), NormalMeanVariance())),
+            FactorProduct((Gamma(1.000001, 1.0e12), NormalMeanVariance())),
+            false
+        )
     end
 end
 
