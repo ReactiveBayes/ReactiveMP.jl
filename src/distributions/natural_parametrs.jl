45b1faa67f738675ac9b33d82922638161a8a3a1
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-10-06 13:15:29 +0200

    fix: convert natural parameters to distributions appropriatly

--- src/distributions/natural_parametrs.jl
+++ src/distributions/natural_parametrs.jl
@@ -1,3 +1,6 @@
 export NaturalParameters
+import Base: convert
 
 abstract type NaturalParameters end
+
+Base.convert(::Type{T}, params::NaturalParameters) where { T <: Distribution } = convert(T, convert(Distribution, params))
