aa54d1888ad781397b34715ce7bf479067dee902
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-10-06 13:16:47 +0200

    test: add identity cv test for x ~ MvNormal, y ~ MvNormal

--- test/rules/delta/cvi/test_marginals.jl
+++ test/rules/delta/cvi/test_marginals.jl
@@ -9,15 +9,13 @@ using StableRNGs
 
 import ReactiveMP: @test_marginalrules
 
-id(x) = x
-
 @testset "marginalrules:CVI" begin
-    @testset "[id(x) - y] x~Normal, y~Normal" begin
+    @testset "id, x~Normal, y~Normal" begin
         seed = 123
         rng = StableRNG(seed)
         optimizer = Descent(0.01)
         test_meta = CVIApproximation(rng, 1, 500, optimizer)
-        @test_marginalrules [with_float_conversions = false, atol = 0.1] DeltaFn{id}(:ins) [
+        @test_marginalrules [with_float_conversions = false, atol = 0.1] DeltaFn{identity}(:ins) [
             (
                 input = (m_out = NormalMeanVariance(1, 1), m_ins = ManyOf(NormalMeanVariance()), meta = test_meta),
                 output = FactorProduct((NormalWeightedMeanPrecision(1.0, 2.0),))
@@ -29,6 +27,26 @@ id(x) = x
             (
                 input = (m_out = NormalMeanVariance(10, 1), m_ins = ManyOf(NormalMeanVariance()), meta = test_meta),
                 output = FactorProduct((NormalWeightedMeanPrecision(10.0, 2.0),))
+            )
+        ]
+    end
+    @testset "id, x ~ MvNormal, y ~ MvNormal" begin
+        seed = 123
+        rng = StableRNG(seed)
+        optimizer = Descent(0.01)
+        test_meta = CVIApproximation(rng, 1, 1000, optimizer)
+        @test_marginalrules [with_float_conversions = false, atol = 0.1] DeltaFn{identity}(:ins) [
+            (
+                input = (m_out = MvGaussianMeanCovariance(ones(2)), m_ins = ManyOf(MvGaussianMeanCovariance(zeros(2))), meta = test_meta),
+                output = FactorProduct((MvNormalWeightedMeanPrecision(ones(2), diageye(2)*2),))
+            ),
+            (
+                input = (m_out = MvGaussianMeanCovariance(ones(2)*10), m_ins = ManyOf(MvGaussianMeanCovariance(zeros(2))), meta = test_meta),
+                output = FactorProduct((MvNormalWeightedMeanPrecision(ones(2)*10, diageye(2)*2),))
+            ),
+            (
+                input = (m_out = MvGaussianMeanCovariance(ones(2), [2 -1; -1 2]), m_ins = ManyOf(MvGaussianMeanCovariance(zeros(2))), meta = test_meta),
+                output = FactorProduct(((MvNormalWeightedMeanPrecision(ones(2), [1 + 2/3 1/3; 1/3 1 + 2/3])),))
             )
         ]
     end
