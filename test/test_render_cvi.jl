c651b5ea4c61243b035fa4c8e40b206ec84ca44b
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-10-11 14:33:59 +0200

    test: added renderCVI test (count number of the opt iterations)

--- test/test_render_cvi.jl
+++ test/test_render_cvi.jl
@@ -11,6 +11,19 @@ function ReactiveMP.cvi_update!(opt::EmptyOptimizer, λ::T, ∇::T) where {T <: 
     return vec(λ)
 end
 
+mutable struct CountingOptimezer
+    num_its::Int
+end
+
+function CountingOptimezer()
+    CountingOptimezer(0)
+end
+
+function ReactiveMP.cvi_update!(opt::CountingOptimezer, λ::T, ∇::T) where {T <: NaturalParameters}
+    opt.num_its += 1
+    return vec(λ)
+end
+
 @testset "cvi:renderCVI" begin
     @testset "empty optimizer" begin
         m_in = NormalMeanVariance(0, 1)
@@ -21,5 +34,16 @@ end
         λ = ReactiveMP.renderCVI(logp_nc, meta.num_iterations, meta.opt, meta.rng, deepcopy(η), m_in)
         @test convert(NormalMeanVariance, λ) ≈ NormalMeanVariance(0, 1)
     end
+    @testset "counting optimizer" begin
+        m_in = NormalMeanVariance(0, 1)
+        m_out = NormalMeanVariance(0, 1)
+        η = naturalparams(m_in)
+        logp_nc = (z) -> logpdf(m_out, z)
+        opt = CountingOptimezer()
+        meta = CVIApproximation(1, 100, opt)
+        λ = ReactiveMP.renderCVI(logp_nc, meta.num_iterations, meta.opt, meta.rng, deepcopy(η), m_in)
+        @test convert(NormalMeanVariance, λ) ≈ NormalMeanVariance(0, 1)
+        @test opt.num_its === 100
+    end
 end
-end 
+end
