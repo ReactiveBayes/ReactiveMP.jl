816a1b258f0bf0774b1aca242c5dff302091d454
Bagaev Dmitry <bvdmitri@gmail.com>
2022-10-20 13:51:08 +0200

    fix unscented marginals

--- src/rules/delta/unscented/marginals.jl
+++ src/rules/delta/unscented/marginals.jl
@@ -7,31 +7,22 @@
     meta::DeltaMeta{M}
 ) where {f, N, M <: Unscented} = begin
     # Approximate joint inbounds
-    (μs_fw_in, Σs_fw_in) = collectStatistics(m_ins...) # Returns arrays with individual means and covariances
-    (μ_tilde, Σ_tilde, C_tilde) = unscented_statistics(getmethod(meta), μs_fw_in, Σs_fw_in, f)
+    statistics = mean_cov.(m_ins)
+    μs_fw_in = first.(statistics)
+    Σs_fw_in = last.(statistics)
+    sizes    = size.(m_ins)
+    
+    (μ_tilde, Σ_tilde, C_tilde) = unscented_statistics(getmethod(meta), f, μs_fw_in, Σs_fw_in)
 
-    # RTS smoother
     joint              = convert(JointNormal, μs_fw_in, Σs_fw_in)
     (μ_fw_in, Σ_fw_in) = mean_cov(joint)
     ds                 = dimensionalities(joint)
 
+    # Apply the RTS smoother
     (μ_bw_out, Σ_bw_out) = mean_cov(m_out)
-    (μ_in, Σ_in) = smoothRTS(μ_tilde, Σ_tilde, C_tilde, μ_fw_in, Σ_fw_in, μ_bw_out, Σ_bw_out)
-    return JointNormal(MvNormalMeanCovariance(μ_in, Σ_in), ds)
+    (μ_in, Σ_in)         = smoothRTS(μ_tilde, Σ_tilde, C_tilde, μ_fw_in, Σ_fw_in, μ_bw_out, Σ_bw_out)
+
+    dist = convert(promote_variate_type(variate_form(μ_in), NormalMeanVariance), μ_in, Σ_in)
+
+    return JointNormal(dist, sizes)
 end
-
-@marginalrule DeltaFn{f}(:ins) (m_out::NormalDistributionsFamily, m_ins::ManyOf{1, NormalDistributionsFamily}, meta::DeltaMeta{M}) where {f, M <: Unscented} =
-    begin
-        # Approximate joint inbounds
-        μ_fw_in, Σ_fw_in = collectStatistics(m_ins...)
-
-        (μ_tilde, Σ_tilde, C_tilde) = unscented_statistics(getmethod(meta), μ_fw_in, Σ_fw_in, f)
-        (μ_bw_out, Σ_bw_out) = mean_cov(m_out)
-
-        (μ_in, Σ_in) = smoothRTS(μ_tilde, Σ_tilde, C_tilde, μ_fw_in, Σ_fw_in, μ_bw_out, Σ_bw_out)
-
-        dist = convert(promote_variate_type(variate_form(μ_in), NormalMeanVariance), μ_in, Σ_in)
-        ds   = [(length(μ_in),)]
-
-        return JointNormal(dist, ds)
-    end
