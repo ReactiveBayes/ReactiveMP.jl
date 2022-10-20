bc256303a812391de511994d7f05665e4148fa9c
Bagaev Dmitry <bvdmitri@gmail.com>
2022-10-20 11:59:55 +0200

    feat: fix `out` rule

--- src/rules/delta/linearization/out.jl
+++ src/rules/delta/linearization/out.jl
@@ -1,10 +1,5 @@
 
-
 @rule DeltaFn{f}(:out, Marginalisation) (m_ins::ManyOf{N, NormalDistributionsFamily}, meta::DeltaMeta{M}) where {f, N, M <: Linearization} = begin
-    (μs_in, Σs_in) = mean_cov.(m_ins...) # Returns arrays with individual means and covariances
-    (A, b) = approximate(f, μs_in)
-    (m_fw_in, V_fw_in) = mean_cov(convert(JointNormal, μs_in, Σs_in))
-    m = A * m_fw_in + b
-    V = A * V_fw_in * A'
-    return convert(promote_variate_type(variate_form(m), NormalMeanVariance), m, V)
+    # Collect statistics with individual means and covariances
+    return approximate(getmethod(meta), f, m_ins)
 end
