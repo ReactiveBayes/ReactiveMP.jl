fb3aaca0901779c4da92d2da0f9cd0b9ca99ec3e
Bart van Erp <b.v.erp@tue.nl>
2022-10-21 09:26:30 +0200

    finalize rules switch

--- src/rules/switch/out.jl
+++ src/rules/switch/out.jl
@@ -1,7 +1,23 @@
 @rule Switch(:out, Marginalisation) (
     m_switch::Any,
-    m_inputs::Tuple{Any}
+    m_inputs::NTuple{N, Any}
 ) where {N} = begin
-    @logscale 1
-    return Beta(1, 1)
+
+    # get logscales of different inputs
+    logscales_inputs = map(getlogscale, messages[2])
+
+    # get logscales of Categorical/Bernoulli
+    logscales_switch = getlogscale(messages[1]) .+ log.(probvec(m_switch))
+
+    # compute logscales of individual components
+    logscales = logscales_inputs .+ logscales_switch
+
+    @logscale logsumexp(logscales)
+
+    # compute weights
+    w = Categorical(softmax(collect(logscales)))
+
+    # return mixture 
+    return MixtureModel(collect(m_inputs), w)
+
 end
