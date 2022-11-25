c644e81a423141104a69e4f9c84d65bf2cedd49c
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2022-11-25 14:30:41 +0100

    test: add Normal prod(meta:CVI, ...) tests

--- test/nodes/delta/cvi/test_cvi.jl
+++ test/nodes/delta/cvi/test_cvi.jl
@@ -6,6 +6,7 @@ using Random
 using StableRNGs
 using Distributions
 using Zygote
+using Flux
 
 import ReactiveMP: naturalparams, NaturalParameters
 
@@ -73,6 +74,32 @@ end
             λ = prod(meta, (z) -> logpdf(m_out, z), m_in)
             @test all(mean_var(convert(Distribution, λ)) .== mean_var(m_in))
             @test num_its === 100
+        end
+    end
+
+    @testset "Normal x Normal (Specific prod)" begin
+        seed = 123
+        rng = StableRNG(seed)
+        optimizer = Descent(0.01)
+        meta = CVI(rng, 1, 1000, optimizer, ForwardDiffGrad(), false, false)
+
+        for i in 1:10
+            m_out, m_in = NormalMeanVariance(i, 1), NormalMeanVariance(0, 1)
+            λ = prod(meta, (z) -> logpdf(m_out, z), m_in)
+            @test isapprox(convert(Distribution, λ), NormalWeightedMeanPrecision(i, 2), atol = 0.1)
+        end
+    end
+
+    @testset "Normal x Normal (Generic prod)" begin
+        seed = 123
+        rng = StableRNG(seed)
+        optimizer = Descent(0.001)
+        meta = CVI(rng, 1, 5000, optimizer, ForwardDiffGrad(), false, false)
+
+        for i in 1:3
+            m_out, m_in = NormalMeanVariance(i, 1), NormalMeanVariance(0, 1)
+            λ = Base.@invoke prod(meta::CVI, ((z) -> logpdf(m_out, z))::Function, m_in::Any)
+            @test isapprox(convert(Distribution, λ), NormalWeightedMeanPrecision(i, 2), atol = 0.5)
         end
     end
 end
