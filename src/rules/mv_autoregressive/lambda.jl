6795aff07c6e74604ec50a901a55f9088d7dc549
Albert Podusenko <albert.podusenko@gmail.com>
2022-12-30 19:11:52 +0100

    Update rules

--- src/rules/mv_autoregressive/lambda.jl
+++ src/rules/mv_autoregressive/lambda.jl
@@ -24,15 +24,22 @@
     Fs = [mask_mar(order, ds, i) for i in 1:order]
 
     S = mar_shift(order, ds)
-    G₁ = S*Vx*S'
-    G₂ = sum(S*Vx*Fs[i]*ma*es[i]' for i in 1:order)
+    # G₁ = S*Vx*S'
+    # G₂ = sum(S*Vx*Fs[i]*ma*es[i]' for i in 1:order)
+    # G₃ = transpose(G₂)
+    # G₄ = sum(sum(es[i]*ma'*Fs[i]'*Vx*Fs[j]*ma*es[j]' for i in 1:order) for j in 1:order)
+    # G₅ = sum(sum(es[i]*mx'*Fs[j]*Va*Fs[i]'*mx*es[j]' for i in 1:order) for j in 1:order)
+    # G₆ = sum(sum(es[i]*tr(Va*Fs[i]'*Vx*Fs[j])*es[j]' for i in 1:order) for j in 1:order)
+    G₁ = Vy[1:order, 1:order]
+    G₂ = (my*mx'*mA')[1:order, 1:order]
     G₃ = transpose(G₂)
-    G₄ = sum(sum(es[i]*ma'*Fs[i]'*Vx*Fs[j]*ma*es[j]' for i in 1:order) for j in 1:order)
-    G₅ = sum(sum(es[i]*mx'*Fs[j]*Va*Fs[i]'*mx*es[j]' for i in 1:order) for j in 1:order)
-    G₆ = sum(sum(es[i]*tr(Va*Fs[i]'*Vx*Fs[j])*es[j]' for i in 1:order) for j in 1:order)
-    G = G₁ + G₂ + G₃ + G₄ + G₅ + G₆
+    Ex_xx = mx*mx' + Vx
+    G₅ = sum(sum(es[i]*ma'*Fs[j]'Ex_xx*Fs[i]*ma*es[j]' for i in 1:order) for j in 1:order)[1:order, 1:order]
+    G₆ = sum(sum(es[i]*tr(Va*Fs[i]'*Ex_xx*Fs[j])*es[j]' for i in 1:order) for j in 1:order)[1:order, 1:order]
+    Δ = G₁ + G₂ + G₃ + G₅ + G₆
+    # G = G₁ + G₂ + G₃ + G₄ + G₅ + G₆
     
-    Δ = (my - mA*mx)*(my - mA*mx)' - mA*Vyx' - Vyx*mA + S*Vx*S' + G
+    # Δ = (my - mA*mx)*(my - mA*mx)' - mA*Vyx' - Vyx*mA + S*Vx*S' + G
 
-    return WishartMessage(n+2, Δ[1:order, 1:order])
+    return WishartMessage(n+2, Δ)
 end
