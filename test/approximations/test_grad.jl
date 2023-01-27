bc5b02bd96b6d8e421e3a4f91808c40df1c1cfec
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2023-01-27 14:31:59 +0100

    style: :art:

--- test/approximations/test_grad.jl
+++ test/approximations/test_grad.jl
@@ -13,10 +13,9 @@ import ReactiveMP: convert_eltype
     grad = ForwardDiffGrad()
 
     for i in 1:100
-        @test ReactiveMP.compute_gradient(grad, (x) -> sum(x)^2, [i]) ≈ [2*i]
+        @test ReactiveMP.compute_gradient(grad, (x) -> sum(x)^2, [i]) ≈ [2 * i]
         @test ReactiveMP.compute_hessian(grad, (x) -> sum(x)^2, [i]) ≈ [2;;]
     end
-   
 end
 
 end
