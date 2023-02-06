661b4726dd2ffb85b8c747c5eaac31d70df48294
Bart van Erp <b.v.erp@tue.nl>
2023-02-06 12:43:25 +0100

    add extra bernoulli rule

--- src/rules/bernoulli/p.jl
+++ src/rules/bernoulli/p.jl
@@ -16,3 +16,10 @@ end
     r = succprob(q_out)
     return Beta(one(r) + r, 2one(r) - r)
 end
+
+@rule Bernoulli(:p, Marginalisation) (q_out::DiscreteNonParametric,) = begin
+    p = probvec(q_out)
+    @assert length(p) == 2 "Bernoulli is only defined over its support {0,1}. It has received a Categorical message containing a probability vector unequal to 2."
+    r = p[2]
+    return Beta(one(r) + r, 2one(r) - r)
+end
