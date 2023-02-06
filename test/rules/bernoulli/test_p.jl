84592dc3b2654d6ce21cb5458c207a620d5a6454
Bart van Erp <b.v.erp@tue.nl>
2023-02-06 12:56:06 +0100

    add tests new rule bernoulli:p

--- test/rules/bernoulli/test_p.jl
+++ test/rules/bernoulli/test_p.jl
@@ -12,9 +12,15 @@ import ReactiveMP: @test_rules
         ]
     end
 
-    @testset "Variational Message Passing: (q_out::PointMass)" begin
+    @testset "Variational Message Passing: (q_out::Bernoulli)" begin
         @test_rules [with_float_conversions = true] Bernoulli(:p, Marginalisation) [
             (input = (q_out = Bernoulli(1.0),), output = Beta(2.0, 1.0)), (input = (q_out = Bernoulli(0.3),), output = Beta(13 / 10, 17 / 10))
+        ]
+    end
+
+    @testset "Variational Message Passing: (q_out::DiscreteNonParametric)" begin
+        @test_rules [with_float_conversions = false] Bernoulli(:p, Marginalisation) [
+            (input = (q_out = Categorical([0.0, 1.0]),), output = Beta(2.0, 1.0)), (input = (q_out = Categorical([0.7, 0.3]),), output = Beta(13 / 10, 17 / 10))
         ]
     end
 end
