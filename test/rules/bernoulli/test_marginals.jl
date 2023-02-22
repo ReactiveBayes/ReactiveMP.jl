7b0363029cd8a5c24b8117a8bb567f4ff399b098
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2023-02-22 21:39:20 +0100

    add tests bernoulli marginals

--- test/rules/bernoulli/test_marginals.jl
+++ test/rules/bernoulli/test_marginals.jl
@@ -13,5 +13,12 @@ import ReactiveMP: @test_marginalrules
             (input = (m_out = PointMass(0.0), m_p = Beta(1.0, 2.0)), output = (out = PointMass(0.0), p = Beta(1.0, 3.0)))
         ]
     end
+    @testset "out_p: (m_out::Bernoulli, m_p::PointMass)" begin
+        @test_marginalrules [with_float_conversions = true] Bernoulli(:out_p) [
+            (input = (m_out = Bernoulli(0.8), m_p = PointMass(1.0)), output = (out = Bernoulli(1.0), p = PointMass(1.0))), 
+            (input = (m_out = Bernoulli(0.2), m_p = PointMass(1.0)), output = (out = Bernoulli(1.0), p = PointMass(1.0))),
+            (input = (m_out = Bernoulli(0.2), m_p = PointMass(0.0)), output = (out = Bernoulli(0.0), p = PointMass(0.0)))
+        ]
+    end
 end
 end
