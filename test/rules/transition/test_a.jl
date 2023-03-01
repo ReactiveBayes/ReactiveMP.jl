40b0b0f1a646370b6c64317f1620bb082c4d9788
Wouter Nuijten <wouternuijten@gmail.com>
2023-03-01 13:43:06 +0100

    Add test cases for A in transition node
    Also reimplemented '''probvec''' for Bernoulli node to support linalg operations

---
+++ test/rules/transition/test_a.jl
@@ -0,0 +1,26 @@
+module RulesTransitionATest
+
+using Test
+using ReactiveMP
+using Random
+
+import ReactiveMP: @test_rules
+
+@testset "rules:Transition:a" begin
+    @testset "Variational Bayes: (q_out::Any, q_in::Categorical)" begin
+        @test_rules [with_float_conversions = false] Transition(:a, Marginalisation) [
+            (input = (q_out = Categorical([0.2, 0.5, 0.3]), q_in = Categorical([0.1, 0.4, 0.5])), output = MatrixDirichlet([1.02 1.08 1.1; 1.05 1.2 1.25; 1.03 1.12 1.15])),
+            (input = (q_out = PointMass([0.2, 0.5, 0.3]), q_in = Categorical([0.1, 0.4, 0.5])), output = MatrixDirichlet([1.02 1.08 1.1; 1.05 1.2 1.25; 1.03 1.12 1.15])),
+            (input = (q_out = Dirichlet([0.2, 0.5, 0.3]), q_in = Categorical([0.1, 0.4, 0.5])), output = MatrixDirichlet([1.02 1.08 1.1; 1.05 1.2 1.25; 1.03 1.12 1.15])),
+            (input = (q_out = Bernoulli(0.3), q_in = Categorical([0.4, 0.6])), output = MatrixDirichlet([1.28 1.42 ; 1.12 1.18 ]))
+        ]
+    end
+
+    @testset "Variational Bayes: (q_out_in::Contingency)" begin
+        @test_rules [with_float_conversions = false] Transition(:a, Marginalisation) [
+            (input = (q_out_in = Contingency(diageye(3)),), output = MatrixDirichlet([1.333333333333333 1 1; 1 1.3333333333333 1; 1 1 1.33333333333333333])),
+        ]
+    end
+end
+
+end
