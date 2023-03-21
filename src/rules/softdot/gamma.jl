09da8b20305325f75d29267f573509b04710972f
Tim <t.n.nisslbeck@tue.nl>
2023-03-21 18:04:16 +0100

    📄: sofdot: optimized

--- src/rules/softdot/gamma.jl
+++ src/rules/softdot/gamma.jl
@@ -6,8 +6,8 @@
     mx, Vx = mean_cov(q_x)
     mθ, Vθ = mean_cov(q_θ)
     α = 1.5
-    β = 0.5 * (Vy + my*my')
+    β = 0.5(Vy + my*my')
     β -= my*mθ'*mx
-    β += 0.5 * ( mul_trace(Vx, Vθ) + mθ'Vx*mθ + mx'Vθ*mx + mθ'mx*mx'mθ )
+    β += 0.5(mul_trace(Vx, Vθ) + mθ'Vx*mθ + mx'Vθ*mx + mθ'mx*mx'mθ)
     return GammaShapeRate(α, β)
 end
