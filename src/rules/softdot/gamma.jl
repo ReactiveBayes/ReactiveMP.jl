7aa3547efe1692a305f8bfd2f825d3051aeee225
Tim <t.n.nisslbeck@tue.nl>
2023-03-27 16:44:26 +0200

    remove float literals

--- src/rules/softdot/gamma.jl
+++ src/rules/softdot/gamma.jl
@@ -5,8 +5,8 @@
     my, Vy = mean_cov(q_y)
     mx, Vx = mean_cov(q_x)
     mθ, Vθ = mean_cov(q_θ)
-    α = 1.5
-    β = 0.5(Vy + my*my')
+    α = 3/2
+    β = Vy + my*my') / 2
     β -= my*mθ'*mx
     β += 0.5(mul_trace(Vx, Vθ) + mθ'Vx*mθ + mx'Vθ*mx + mθ'mx*mx'mθ)
     return GammaShapeRate(α, β)
