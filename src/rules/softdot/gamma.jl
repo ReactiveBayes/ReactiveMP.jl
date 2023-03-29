a14f12f515b4279145e63e70c40774a46e1f293a
Tim <t.n.nisslbeck@tue.nl>
2023-03-29 12:50:35 +0200

    📄: fix floattype check

--- src/rules/softdot/gamma.jl
+++ src/rules/softdot/gamma.jl
@@ -5,7 +5,8 @@
     my, Vy = mean_cov(q_y)
     mx, Vx = mean_cov(q_x)
     mθ, Vθ = mean_cov(q_θ)
-    α = 3/2
+    T = promote_samplefloattype(q_y, q_x, q_θ)
+    α = convert(T, 3/2)
     β = (Vy + my*my') / 2
     β -= my*mθ'*mx
     β += (mul_trace(Vx, Vθ) + mθ'Vx*mθ + mx'Vθ*mx + mθ'mx*mx'mθ) / 2
--- test/rules/softdot/test_gamma.jl
+++ test/rules/softdot/test_gamma.jl
@@ -8,7 +8,7 @@ import ReactiveMP: @test_rules
 # TODO: add combinations of multiple node inputs
 @testset "rules:SoftDot:γ" begin
     @testset "Variational Message Passing: (q_y::Any, q_x::Any, q_θ::Any)" begin
-        @test_rules [with_float_conversions = false] SoftDot(:γ, Marginalisation) [
+        @test_rules [with_float_conversions = true] SoftDot(:γ, Marginalisation) [
             (input = (q_y = PointMass(3.0), q_x = PointMass(5.0), q_θ = PointMass(2.0)), output = GammaShapeRate(3/2, 49/2)),
             (input = (q_y = NormalMeanVariance(3.0, 7.0), q_x = NormalMeanVariance(5.0, 9.0), q_θ = PointMass(2.0)), output = GammaShapeRate(3/2, 46.0)),
             (input = (q_y = NormalMeanVariance(3.0, 7.0), q_x = PointMass(2.0), q_θ = NormalMeanVariance(5.0, 9.0)), output = GammaShapeRate(3/2, 46.0)),
