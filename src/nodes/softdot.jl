e2e1d9c81f241fd45cba4d51f6509f68ce62ef5c
Tim <t.n.nisslbeck@tue.nl>
2023-04-06 10:24:55 +0200

    📄: softdot: #305: make format

--- src/nodes/softdot.jl
+++ src/nodes/softdot.jl
@@ -12,5 +12,5 @@ const softdot = SoftDot
     m_θ, V_θ = mean_cov(q_θ)
     m_x, V_x = mean_cov(q_x)
     m_γ = mean(q_γ)
-    return (- mean(log, q_γ) + log2π + m_γ * (V_y + m_y^2 - 2m_γ * m_y * m_θ'm_x + mul_trace(V_θ, V_x) + m_x'V_θ * m_x + m_θ' * (V_x + m_x * m_x') * m_θ))/2
+    return (-mean(log, q_γ) + log2π + m_γ * (V_y + m_y^2 - 2m_γ * m_y * m_θ'm_x + mul_trace(V_θ, V_x) + m_x'V_θ * m_x + m_θ' * (V_x + m_x * m_x') * m_θ)) / 2
 end
