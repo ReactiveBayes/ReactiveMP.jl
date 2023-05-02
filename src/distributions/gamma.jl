7fc8e350f7b86902afef933e70381cee717c1ade
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2023-05-02 17:06:52 +0200

    fix: implement GammaShapeRate pdf as exp(logpdf)

--- src/distributions/gamma.jl
+++ src/distributions/gamma.jl
@@ -9,6 +9,8 @@ const GammaDistributionsFamily{T} = Union{GammaShapeScale{T}, GammaShapeRate{T}}
 
 Distributions.cov(dist::GammaDistributionsFamily) = var(dist)
 
+Distributions.pdf(dist::GammaShapeRate, x::Real)  = exp(logpdf(dist,x))
+
 function mean(::typeof(log), dist::GammaShapeScale)
     k, θ = params(dist)
     return digamma(k) + log(θ)
