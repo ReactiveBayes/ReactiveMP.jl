332d5d214c30e21ed868b2c985f30359dbadcaf2
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2023-05-03 09:49:13 +0200

    simplify bernoulli update rule

--- src/rules/bernoulli/out.jl
+++ src/rules/bernoulli/out.jl
@@ -1,16 +1,17 @@
 export rule
 
-@rule Bernoulli(:out, Marginalisation) (m_p::Beta,) = begin
+@rule Bernoulli(:out, Marginalisation) (m_p::Any,) = begin
     @logscale 0
     return Bernoulli(mean(m_p))
 end
 
-@rule Bernoulli(:out, Marginalisation) (m_p::PointMass,) = Bernoulli(mean(m_p))
+@rule Bernoulli(:out, Marginalisation) (q_p::Any,) = begin
+    rho_1 = mean(log, q_p)          # E[ln(x)]
+    rho_2 = mean(mirrorlog, q_p)    # E[log(1-x)]
+    m = max(rho_1, rho_2)
+    tmp = exp(rho_1 - m)
+    p = clamp(tmp / (tmp + exp(rho_2 - m)), tiny, one(m))
+    return Bernoulli(p)
+end
 
 @rule Bernoulli(:out, Marginalisation) (q_p::PointMass,) = Bernoulli(mean(q_p))
-
-@rule Bernoulli(:out, Marginalisation) (q_p::Beta,) = begin
-    rho_1 = clamp(exp(mean(log, q_p)), tiny, huge)
-    rho_2 = clamp(exp(mean(mirrorlog, q_p)), tiny, huge)
-    return Bernoulli(rho_1 / (rho_1 + rho_2))
-end
