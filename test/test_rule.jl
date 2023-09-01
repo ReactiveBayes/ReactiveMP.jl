f356001d88f680e2153b003705833064c52cdc0b
Bagaev Dmitry <bvdmitri@gmail.com>
2023-09-01 11:25:39 +0200

    Allow returning addons from the `@call_rule`

--- test/test_rule.jl
+++ test/test_rule.jl
@@ -690,6 +690,24 @@ import MacroTools: inexpr
 
         @test_throws ReactiveMP.RuleMethodError (@call_rule DummyNode(:out, Marginalisation) (q_x = vague(NormalMeanPrecision), q_y = vague(NormalMeanPrecision), meta = 3))
     end
+
+    @testset "Check the `return_addons` option" begin 
+        # Enable LogScale addon
+        dist_and_addons = @call_rule [ return_addons = true ] Bernoulli(:out, Marginalisation) (m_p = Beta(1, 2), addons = (AddonLogScale(), ))
+
+        @test dist_and_addons isa Tuple
+        @test length(dist_and_addons) === 2
+        @test dist_and_addons[1] isa Bernoulli
+        @test dist_and_addons[2] isa Tuple{AddonLogScale}
+
+        # Without addons but with the option
+        dist_and_nothing = @call_rule [ return_addons = true ] Bernoulli(:out, Marginalisation) (m_p = Beta(1, 2), )
+
+        @test dist_and_nothing isa Tuple
+        @test length(dist_and_nothing) === 2
+        @test dist_and_nothing[1] isa Bernoulli
+        @test dist_and_nothing[2] isa Nothing
+    end
 end
 
 end
