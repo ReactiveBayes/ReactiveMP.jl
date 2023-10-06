95a28fc6efeb6bc5052d1780feebd0226cdaefb1
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2023-10-06 14:09:20 +0200

    add rules input multiplication node

--- src/rules/multiplication/in.jl
+++ src/rules/multiplication/in.jl
@@ -66,3 +66,45 @@ end
 @rule typeof(*)(:in, Marginalisation) (m_out::UnivariateDistribution, m_A::UnivariateDistribution, meta::Union{<:AbstractCorrectionStrategy, Nothing}) = begin
     return @call_rule typeof(*)(:A, Marginalisation) (m_out = m_out, m_in = m_A, meta = meta)
 end
+
+#------------------------
+# Real * NormalDistributions
+#------------------------
+@rule typeof(*)(:in, Marginalisation) (m_A::PointMass{<:Real}, m_out::UnivariateNormalDistributionsFamily, meta::Union{<:AbstractCorrectionStrategy, Nothing}) = begin
+    @logscale 0
+    a = mean(m_A)
+    μ_out, v_out = mean_var(m_out)
+    return NormalMeanVariance(μ_out / a, v_out / a^2)
+end
+
+@rule typeof(*)(:in, Marginalisation) (m_A::PointMass{<:Real}, m_out::MvNormalMeanCovariance, meta::Union{<:AbstractCorrectionStrategy, Nothing}) = begin
+    @logscale 0
+    a = mean(m_A)
+    μ_out, v_out = mean_cov(m_out)
+    return MvNormalMeanCovariance(1 / a * μ_out, 1 / a^2 * v_out)
+end
+
+@rule typeof(*)(:in, Marginalisation) (m_A::PointMass{<:Real}, m_out::MvNormalMeanPrecision, meta::Union{<:AbstractCorrectionStrategy, Nothing}) = begin
+    @logscale 0
+    a = mean(m_A)
+    μ_out, w_out = mean_precision(m_out)
+    return MvNormalMeanPrecision(1 / a * μ_out, a^2 * w_out)
+end
+
+@rule typeof(*)(:in, Marginalisation) (m_A::PointMass{<:Real}, m_out::MvNormalWeightedMeanPrecision, meta::Union{<:AbstractCorrectionStrategy, Nothing}) = begin
+    @logscale 0
+    a = mean(m_A)
+    ξ_out, w_out = weightedmean_precision(m_out)
+    return MvNormalWeightedMeanPrecision(ξ_out, 1 / a^2 * w_out)
+end
+
+@rule typeof(*)(:in, Marginalisation) (m_A::NormalDistributionsFamily, m_out::PointMass{<:Real}, meta::Union{<:AbstractCorrectionStrategy, Nothing}) = begin
+    return @call_rule typeof(*)(:in, Marginalisation) (m_A = m_out, m_out = m_A, meta = meta, addons = getaddons()) # symmetric rule
+end
+
+#------------------------
+# UniformScaling * NormalDistributions
+#------------------------
+@rule typeof(*)(:in, Marginalisation) (m_A::PointMass{<:UniformScaling}, m_out::NormalDistributionsFamily, meta::Union{<:AbstractCorrectionStrategy, Nothing}) = begin
+    return @call_rule typeof(*)(:in, Marginalisation) (m_A = PointMass(mean(m_A).λ), m_out = m_out, meta = meta, addons = getaddons()) # dispatch to real * normal
+end
