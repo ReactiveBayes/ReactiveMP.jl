2183a799e0a7222b878f7aee014ac21492405654
Bagaev Dmitry <bvdmitri@gmail.com>
2023-10-31 10:05:37 +0100

    fixes for rules

--- src/nodes/transition.jl
+++ src/nodes/transition.jl
@@ -15,7 +15,7 @@ end
 end
 
 @average_energy Transition (q_out_in::Contingency, q_a::PointMass) = begin
-    # `map(clamplog, mean(q_a))` is an equivalent of `mean(log, q_a)` with an extra `clamp(el, tiny, Inf)` operation
+    # `map(clamplog, mean(q_a))` is an equivalent of `mean(BroadcastFunction(log), q_a)` with an extra `clamp(el, tiny, Inf)` operation
     # The reason is that we don't want to take log of zeros in the matrix `q_a` (if there are any)
     # The trick here is that if RHS matrix has zero inputs, than the corresponding entries of the `contingency_matrix` matrix 
     # should also be zeros (see corresponding @marginalrule), so at the end `log(tiny) * 0` should not influence the result.
