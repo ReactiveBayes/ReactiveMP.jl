030acdadca290aa6eb1a5082f2f75f09b3c263e7
Albert Podusenko <albert.podusenko@gmail.com>
2023-11-03 12:17:05 +0100

    Modify MatNormal

--- src/nodes/matrix_normal.jl
+++ src/nodes/matrix_normal.jl
@@ -1,7 +1,8 @@
 @node MatrixNormal Stochastic [out, M, U, V]
 
-# default method for mean-field assumption
+# we use equivalence of ``
 @average_energy MatrixNormal (q_out::Any, q_M::Any, q_U::Any, q_V::Any) = begin
     q_Σ = PointMass(kron(mean(q_U), mean(q_V)))
-    -score(AverageEnergy(), MvNormalMeanCovariance, Val{(:out, :μ, :Σ)}(), map((q) -> Marginal(q, false, false, nothing), (q_out, q_M, q_Σ)), nothing)
+    q_m = PointMass(vec(mean(q_M)))
+    -score(AverageEnergy(), MvNormalMeanCovariance, Val{(:out, :μ, :Σ)}(), map((q) -> Marginal(q, false, false, nothing), (q_out, q_m, q_Σ)), nothing)
 end
