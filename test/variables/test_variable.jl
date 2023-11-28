5e5614b2b4c3a4d8d0f6ac8f859af1170a71593d
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2023-11-28 13:41:58 +0100

    test: add setmessage! test

--- test/variables/test_variable.jl
+++ test/variables/test_variable.jl
@@ -2,6 +2,69 @@ module ReactiveMPVariableTest
 
 using Test, ReactiveMP, Rocket, BayesBase, Distributions, ExponentialFamily
 
+struct CustomDeterministicNode end
+
+@node CustomDeterministicNode Deterministic [out, (x, aliases = [xx])]
+
+function test_variable_set_method(variable, dist::T) where {T}
+    flag = false
+
+    activate!(variable, TestOptions())
+
+    test_out_var = randomvar(:out)
+
+    # messages could be initialized only when the node is created
+    test_node = make_node(CustomDeterministicNode, ReactiveMP.FactorNodeCreationOptions(), test_out_var, variable)
+
+    setmessage!(variable, 1, dist)
+    setmarginal!(variable, dist)
+
+    subscription = subscribe!(getmarginal(variable, IncludeAll()), (marginal) -> begin
+        @test typeof(marginal) <: Marginal{T}
+        @test mean(marginal) === mean(dist)
+        @test var(marginal) === var(dist)
+        flag = true
+    end)
+
+    subscription = subscribe!(ReactiveMP.messageout(variable, 1), (message) -> begin
+        @test typeof(message) <: Message{T}
+        @test mean(message) === mean(dist)
+        @test var(message) === var(dist)
+    end)
+
+    # Test that subscription happenend
+    @test flag === true
+
+    unsubscribe!(subscription)
+end
+
+struct TestNodeMetaData end
+
+function test_variables_set_methods(variables, dist::T) where {T}
+    flag = false
+
+    activate!.(variables, TestOptions())
+
+    @test_throws AssertionError setmarginals!(variables, Iterators.repeated(dist, length(variables) - 1))
+
+    setmarginals!(variables, dist)
+
+    subscription = subscribe!(getmarginals(variables, IncludeAll()), (marginals) -> begin
+        @test length(marginals) === length(variables)
+        foreach(marginals) do marginal
+            @test typeof(marginal) <: Marginal{T}
+            @test mean(marginal) === mean(dist)
+            @test var(marginal) === var(dist)
+        end
+        flag = true
+    end)
+
+    # Test that subscription happenend
+    @test flag === true
+
+    unsubscribe!(subscription)
+end
+
 @testset "Variable" begin
     import ReactiveMP: activate!
     import Rocket: getscheduler
@@ -13,71 +76,9 @@ using Test, ReactiveMP, Rocket, BayesBase, Distributions, ExponentialFamily
 
     @testset "setmarginal! tests for randomvar" begin
         for dist in (NormalMeanVariance(-2.0, 3.0), NormalMeanPrecision(-2.0, 3.0), PointMass(2.0))
-            T = typeof(dist)
-            variable = randomvar(:r)
-            flag = false
-
-            activate!(variable, TestOptions())
-
-            setmarginal!(variable, dist)
-
-            subscription = subscribe!(getmarginal(variable, IncludeAll()), (marginal) -> begin
-                @test typeof(marginal) <: Marginal{T}
-                @test mean(marginal) === mean(dist)
-                @test var(marginal) === var(dist)
-                flag = true
-            end)
-
-            # Test that subscription happenend
-            @test flag === true
-
-            unsubscribe!(subscription)
-
-            variablesmv = randomvar(:r, 2)
-            flagmv = false
-
-            activate!.(variablesmv, TestOptions())
-
-            setmarginals!(variablesmv, dist)
-            @test_throws AssertionError setmarginals!(variablesmv, Iterators.repeated(dist, length(variablesmv) - 1))
-
-            subscriptionmv = subscribe!(getmarginals(variablesmv, IncludeAll()), (marginals) -> begin
-                @test length(marginals) === 2
-                foreach(marginals) do marginal
-                    @test typeof(marginal) <: Marginal{T}
-                    @test mean(marginal) === mean(dist)
-                    @test var(marginal) === var(dist)
-                end
-                flagmv = true
-            end)
-
-            # Test that subscription happenend
-            @test flagmv === true
-
-            unsubscribe!(subscriptionmv)
-
-            variablesmx = randomvar(:r, 2, 2)
-            flagmx = false
-
-            activate!.(variablesmx, TestOptions())
-
-            setmarginals!(variablesmx, dist)
-            @test_throws AssertionError setmarginals!(variablesmx, Iterators.repeated(dist, length(variablesmx) - 1))
-
-            subscriptionmx = subscribe!(getmarginals(variablesmx, IncludeAll()), (marginals) -> begin
-                @test length(marginals) === 4
-                foreach(marginals) do marginal
-                    @test typeof(marginal) <: Marginal{T}
-                    @test mean(marginal) === mean(dist)
-                    @test var(marginal) === var(dist)
-                end
-                flagmx = true
-            end)
-
-            # Test that subscription happenend
-            @test flagmx === true
-
-            unsubscribe!(subscriptionmx)
+            test_variable_set_method(randomvar(:r), dist)
+            test_variables_set_methods(randomvar(:r, 2), dist)
+            test_variables_set_methods(randomvar(:r, 2, 2), dist)
         end
     end
 end
