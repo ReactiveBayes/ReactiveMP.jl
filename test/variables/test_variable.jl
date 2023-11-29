e403fb23b1c5ad6df397f79df0edb1d89fb4b4f6
Bagaev Dmitry <bvdmitri@gmail.com>
2023-11-29 12:50:02 +0100

    add more tests and comments

--- test/variables/test_variable.jl
+++ test/variables/test_variable.jl
@@ -4,11 +4,9 @@ using Test, ReactiveMP, Rocket, BayesBase, Distributions, ExponentialFamily
 
 struct CustomDeterministicNode end
 
-@node CustomDeterministicNode Deterministic [out, (x, aliases = [xx])]
+@node CustomDeterministicNode Deterministic [out, x]
 
 function test_variable_set_method(variable, dist::T, k) where {T}
-    flag = false
-
     activate!(variable, TestOptions())
 
     test_out_var = randomvar(:out)
@@ -18,31 +16,44 @@ function test_variable_set_method(variable, dist::T, k) where {T}
         make_node(identity, ReactiveMP.FactorNodeCreationOptions(ReactiveMP.DeltaFn, TestNodeMetaData(), nothing), test_out_var, variable)
     end
 
-    for node_index in 1:k
-        setmessage!(variable, node_index, dist)
-    end
+    @test degree(variable) === k
+
+    # Check that before calling the `setmarginals!` all marginals are `nothing`
+    @test isnothing(Rocket.getrecent(getmarginal(variable, IncludeAll())))
 
     setmarginal!(variable, dist)
 
+    marginal_subscription_flag = false
+    # After calling the `setmarginals!` the marginal should be equal to `dist`
     subscription = subscribe!(getmarginal(variable, IncludeAll()), (marginal) -> begin
         @test typeof(marginal) <: Marginal{T}
         @test mean(marginal) === mean(dist)
         @test var(marginal) === var(dist)
-        flag = true
+        marginal_subscription_flag = true
     end)
+    @test marginal_subscription_flag === true
+    unsubscribe!(subscription)
+
+    # Check that before calling the `setmessages!` all messages are `nothing`
+    for node_index in 1:k
+        @test isnothing(Rocket.getrecent(ReactiveMP.messageout(variable, node_index)))
+    end
 
     for node_index in 1:k
+        setmessage!(variable, node_index, dist)
+    end
+
+    for node_index in 1:k
+        message_subscription_flag = false
         subscription = subscribe!(ReactiveMP.messageout(variable, node_index), (message) -> begin
             @test typeof(message) <: Message{T}
             @test mean(message) === mean(dist)
             @test var(message) === var(dist)
+            message_subscription_flag = true
         end)
+        @test message_subscription_flag === true
+        unsubscribe!(subscription)
     end
-
-    # Test that subscription happenend
-    @test flag === true
-
-    unsubscribe!(subscription)
 end
 
 struct TestNodeMetaData end
@@ -67,14 +78,17 @@ function test_variables_set_methods(variables, dist::T, k::Int) where {T}
     @test all(degree.(variables) .== k)
 
     @test_throws AssertionError setmessages!(variables, Iterators.repeated(dist, length(variables) - 1))
+    @test_throws AssertionError setmessages!(variables, Iterators.repeated(dist, length(variables) - 1))
+
+    # Test `setmarginals!`
+
+    # Check that before calling the `setmarginals!` all marginals are `nothing`
+    @test all(isnothing, Rocket.getrecent.(getmarginal.(variables, IncludeAll())))
 
     setmarginals!(variables, dist)
 
-    @test_throws AssertionError setmessages!(variables, Iterators.repeated(dist, length(variables) - 1))
-
-    setmessages!(variables, dist)
-
-    subscription = subscribe!(getmarginals(variables, IncludeAll()) |> take(1), (marginals) -> begin
+    # After calling the `setmarginals!` all marginals should be equal to `dist`
+    subscription = subscribe!(getmarginals(variables, IncludeAll()), (marginals) -> begin
         @test length(marginals) === length(variables)
         foreach(marginals) do marginal
             @test typeof(marginal) <: Marginal{T}
@@ -86,19 +100,28 @@ function test_variables_set_methods(variables, dist::T, k::Int) where {T}
 
     # Test that subscription happenend
     @test marginal_subscription_flag === true
-
     unsubscribe!(subscription)
 
+    # Check that before calling the `setmessages!` all messages are `nothing`
     for node_index in 1:k
-        subscription = subscribe!(collectLatest(ReactiveMP.messageout.(variables, node_index)) |> take(1), (messages) -> begin
+        @test all(isnothing, Rocket.getrecent.(ReactiveMP.messageout.(variables, node_index)))
+    end
+
+    # After calling the `setmessages!` all marginals should be equal to `dist`
+    setmessages!(variables, dist)
+    # For each outbound index
+    for node_index in 1:k
+        messages_subscription_flag = false
+        subscription = subscribe!(collectLatest(ReactiveMP.messageout.(variables, node_index)), (messages) -> begin
             @test length(messages) === length(variables)
             foreach(messages) do message
                 @test typeof(message) <: Message{T}
                 @test mean(message) === mean(dist)
                 @test var(message) === var(dist)
             end
+            messages_subscription_flag = true
         end)
-
+        @test messages_subscription_flag === true
         unsubscribe!(subscription)
     end
 end
