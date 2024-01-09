b3444a2c4b628e4e288a13e4d15cda9ca03696eb
Albert <albert.podusenko@gmail.com>
2024-01-09 12:00:58 +0100

    Make format

--- src/nodes/mv_normal_weighted_mean_precision.jl
+++ src/nodes/mv_normal_weighted_mean_precision.jl
@@ -5,5 +5,5 @@ import StatsFuns: log2π
 @average_energy MvNormalWeightedMeanPrecision (q_out::Any, q_ξ::Any, q_Λ::Any) = begin
     m_ξ, v_ξ     = mean_cov(q_ξ)
     m_out, v_out = mean_cov(q_out)
-    return (ndims(q_out) * log2π - mean(chollogdet, q_Λ) + tr(mean(q_Λ) * (m_out * m_out' + v_out)) - 2*dot(m_out,m_ξ) + tr(mean(cholinv, q_Λ) * (m_ξ * m_ξ' + v_ξ))) / 2
+    return (ndims(q_out) * log2π - mean(chollogdet, q_Λ) + tr(mean(q_Λ) * (m_out * m_out' + v_out)) - 2 * dot(m_out, m_ξ) + tr(mean(cholinv, q_Λ) * (m_ξ * m_ξ' + v_ξ))) / 2
 end
