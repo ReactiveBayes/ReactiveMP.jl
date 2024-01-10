382eb632654b6062d83a4c0cc074c0006a61f238
bvdmitri <bvdmitri@gmail.com>
2024-01-10 15:07:56 +0100

    Change uninformative rule

--- src/nodes/uninformative.jl
+++ src/nodes/uninformative.jl
@@ -5,3 +5,27 @@ struct Uninformative end
 @node Uninformative Stochastic [out]
 
 @average_energy Uninformative (q_out::Any,) = entropy(q_out)
+
+function BayesBase.default_prod_rule(::Type{<:Uninformative}, ::Type{T}) where {T}
+    return PreserveTypeProd(T)
+end
+
+function BayesBase.default_prod_rule(::Type{T}, ::Type{<:Uninformative}) where {T}
+    return PreserveTypeProd(T)
+end
+
+function BayesBase.default_prod_rule(::Type{<:Uninformative}, ::Type{<:Uninformative})
+    return PreserveTypeProd(Uninformative)
+end
+
+function Base.prod(::PreserveTypeProd{T}, left::Uninformative, right::T) where {T}
+    return right
+end
+
+function Base.prod(::PreserveTypeProd{T}, left::T, right::Uninformative) where {T}
+    return left
+end
+
+function Base.prod(::PreserveTypeProd{Uninformative}, left::Uninformative, right::Uninformative)
+    return Uninformative()
+end
---
+++ test/nodes/test_uninformative.jl
@@ -0,0 +1,24 @@
+module UninformativeNodeTest
+
+using Test, ReactiveMP, Random, BayesBase, ExponentialFamily
+
+@testset "UninformativeNode" begin
+
+    @testset "Creation" begin
+        node = make_node(Uninformative)
+
+        @test functionalform(node) === Uninformative
+        @test sdtype(node) === Stochastic()
+        @test name.(interfaces(node)) === (:out, )
+        @test factorisation(node) === ((1, ),)
+        @test localmarginalnames(node) === (:out,)
+        @test metadata(node) === nothing
+    end
+
+    @testset "Product must not be affected" begin 
+        @test prod(GenericProd(), Uninformative(), NormalMeanVariance(0, 1)) == NormalMeanVariance(0, 1)
+        @test prod(GenericProd(), NormalMeanVariance(3, 4), Uninformative()) == NormalMeanVariance(3, 4)
+        @test prod(GenericProd(), Uninformative(), Uninformative()) === Uninformative()
+    end
+end
+end
