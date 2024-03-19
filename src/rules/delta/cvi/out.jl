f49549a65500d6b85f0d375fd5356ca85dc08014
Bagaev Dmitry <bvdmitri@gmail.com>
2024-03-19 15:57:13 +0100

    revert CVI rule

--- src/rules/delta/cvi/out.jl
+++ src/rules/delta/cvi/out.jl
@@ -4,13 +4,8 @@
     q_sample_friendly = sampling_optimized(q_ins[1])
     rng               = something(method.rng, Random.default_rng())
     samples           = map(x -> rand(rng, q_sample_friendly), 1:(method.n_samples))
-    weights           = fill(one(BayesBase.deep_eltype(typeof(samples))) / method.n_samples, method.n_samples)
     q_out             = map(getnodefn(meta, Val(:out)), samples)
-    # This is a hacky way to by-pass entropy computations of the corresponding variable 
-    # By default the entropy of the sample-list is not being computed and equals to minus infinity
-    # We manually set the entropy to zero but that probably needs to be revised in the future
-    sample_list_meta = SampleListMeta(nothing, 0, nothing, nothing)
-    return TerminalProdArgument(SampleList(q_out, weights, sample_list_meta))
+    return TerminalProdArgument(SampleList(q_out))
 end
 
 @rule DeltaFn(:out, Marginalisation) (q_ins::FactorizedJoint, meta::DeltaMeta{M}) where {M <: CVI} = begin
