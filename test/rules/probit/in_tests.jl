32953455b23ca427ae86f5e1e6cd32e4880d8b42
Bagaev Dmitry <bvdmitri@gmail.com>
2024-03-20 14:20:45 +0100

    move tests to GraphPPL

--- test/rules/probit/in_tests.jl
+++ test/rules/probit/in_tests.jl
@@ -15,7 +15,7 @@
     end
 
     @testset "Expectation Propagation: (m_out::PointMass, m_in::UnivariateNormalDistributionsFamily)" begin
-        @test_rules [check_type_promotion = true] Probit(:in, MomentMatching) [
+        @test_rules [check_type_promotion = true] Probit(:in, Marginalisation) [
             (input = (m_out = PointMass(1.0), m_in = NormalMeanVariance(1.0, 0.5)), output = NormalWeightedMeanPrecision(0.6723616582693972, 0.32950039939606945)),
             (input = (m_out = PointMass(1.0), m_in = NormalMeanPrecision(1.0, 2.0)), output = NormalWeightedMeanPrecision(0.6723616582693972, 0.32950039939606945)),
             (input = (m_out = PointMass(1.0), m_in = NormalWeightedMeanPrecision(2.0, 2.0)), output = NormalWeightedMeanPrecision(0.6723616582693972, 0.32950039939606945)),
@@ -26,7 +26,7 @@
     end
 
     @testset "Expectation Propagation: (m_out::Bernoulli, m_in::UnivariateNormalDistributionsFamily)" begin
-        @test_rules [check_type_promotion = true] Probit(:in, MomentMatching) [
+        @test_rules [check_type_promotion = true] Probit(:in, Marginalisation) [
             (input = (m_out = Bernoulli(1.0), m_in = NormalMeanVariance(1.0, 0.5)), output = NormalWeightedMeanPrecision(0.6723616582693972, 0.32950039939606945)),
             (input = (m_out = Bernoulli(1.0), m_in = NormalMeanVariance(1.0, 0.5)), output = NormalWeightedMeanPrecision(0.6723616582693972, 0.32950039939606945)),
             (input = (m_out = Bernoulli(1.0), m_in = NormalMeanVariance(1.0, 0.5)), output = NormalWeightedMeanPrecision(0.6723616582693972, 0.32950039939606945)),
@@ -40,19 +40,19 @@
 
         # Test against an extreme case with m_out = Bernoulli(0.5)
 
-        @test_rules [check_type_promotion = false, atol = 1e-13] Probit(:in, MomentMatching) [
+        @test_rules [check_type_promotion = false, atol = 1e-13] Probit(:in, Marginalisation) [
             (input = (m_out = Bernoulli(0.5), m_in = NormalMeanVariance(1.0, 0.5)), output = NormalWeightedMeanPrecision(0.0, 1.0 * tiny)),
             (input = (m_out = Bernoulli(0.5), m_in = NormalMeanPrecision(1.0, 2.0)), output = NormalWeightedMeanPrecision(0.0, 1.0 * tiny)),
             (input = (m_out = Bernoulli(0.5), m_in = NormalWeightedMeanPrecision(2.0, 2.0)), output = NormalWeightedMeanPrecision(0.0, 1.0 * tiny))
         ]
 
-        @test_rules [check_type_promotion = false, atol = 1e-7] Probit(:in, MomentMatching) [
+        @test_rules [check_type_promotion = false, atol = 1e-7] Probit(:in, Marginalisation) [
             (input = (m_out = Bernoulli(0.5f0), m_in = NormalMeanVariance(1.0f0, 0.5f0)), output = NormalWeightedMeanPrecision(0.0f0, 1.0f0 * tiny)),
             (input = (m_out = Bernoulli(0.5f0), m_in = NormalMeanPrecision(1.0f0, 2.0f0)), output = NormalWeightedMeanPrecision(0.0f0, 1.0f0 * tiny)),
             (input = (m_out = Bernoulli(0.5f0), m_in = NormalWeightedMeanPrecision(2.0f0, 2.0f0)), output = NormalWeightedMeanPrecision(0.0f0, 1.0f0 * tiny))
         ]
 
-        @test_rules [check_type_promotion = false, atol = 1e-25] Probit(:in, MomentMatching) [
+        @test_rules [check_type_promotion = false, atol = 1e-25] Probit(:in, Marginalisation) [
             (input = (m_out = Bernoulli(big"0.5"), m_in = NormalMeanVariance(big"1.0", big"0.5")), output = NormalWeightedMeanPrecision(big"0.0", big"1.0" * tiny)),
             (input = (m_out = Bernoulli(big"0.5"), m_in = NormalMeanPrecision(big"1.0", big"2.0")), output = NormalWeightedMeanPrecision(big"0.0", big"1.0" * tiny)),
             (input = (m_out = Bernoulli(big"0.5"), m_in = NormalWeightedMeanPrecision(big"2.0", big"2.0")), output = NormalWeightedMeanPrecision(big"0.0", big"1.0" * tiny))
