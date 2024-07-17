0bbdc4c5c78063afc2d90cf4d5263610b027ccfb
Bagaev Dmitry <bvdmitri@gmail.com>
2024-07-17 12:53:35 +0200

    2prev

--- ext/ReactiveMPProjectionExt/rules/in.jl
+++ ext/ReactiveMPProjectionExt/rules/in.jl
@@ -1,5 +1,5 @@
 
 @rule DeltaFn((:in, k), Marginalisation) (q_ins::FactorizedJoint, m_in::Any, meta::DeltaMeta{M}) where {M <: CVIProjection} = begin
     q_ins_k = component(q_ins, k)
-    return DivisionOf(q_ins_k_ef, m_in_ef)
+    return DivisionOf(q_ins_k, m_in)
 end
---
+++ test/ext/ReactiveMPProjectionExt/rules/in_tests.jl
@@ -0,0 +1,17 @@
+@testitem "Basic check for in rule" begin
+    using ExponentialFamily, ExponentialFamilyProjection, BayesBase
+
+    ext = Base.get_extension(ReactiveMP, :ReactiveMPProjectionExt)
+
+    @test !isnothing(ext)
+
+    using .ext
+
+    meta = DeltaMeta(method = CVIProjection(), inverse = nothing)
+    m_in_incoming = NormalMeanVariance(0, 1)
+    m_in_outbound_real = NormalMeanVariance(0, 1)
+    q_ins = FactorizedJoint((prod(GenericProd(), m_in_incoming, m_in_outbound_real),))
+
+    m_in_outbound_approximated = @call_rule DeltaFn{identity}((:in, k = 1), Marginalisation) (q_ins = q_ins, m_in = m_in_incoming, meta = meta)
+    @test prod(GenericProd(), m_in_outbound_approximated, m_in_incoming) ≈ q_ins[1]
+end
