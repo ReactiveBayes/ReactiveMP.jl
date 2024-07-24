543359352ea613a7c6b2125b79bf0a1fd20210c5
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-07-24 13:33:35 +0300

    more marginal tests and fallback to drop based rules in case hmc cant sample discrete variables

--- ext/ReactiveMPProjectionExt/ReactiveMPProjectionExt.jl
+++ ext/ReactiveMPProjectionExt/ReactiveMPProjectionExt.jl
@@ -5,6 +5,8 @@ using ForwardDiff
 
 export DivisionOf, CVIProjection, CVIProjectionEssentials, CVIProjectionOptional, LogTargetDensity
 
+struct DropIndicesBased end
+struct HMCBased end
 
 Base.@kwdef struct CVIProjection{CVIPE, CVIPO} <: ReactiveMP.AbstractApproximationMethod 
     projection_essentials::CVIPE = CVIProjectionEssentials()
@@ -18,7 +20,7 @@ getcviprojectiontypes(cvi::CVIProjection) = getcviprojectiontypes(getcviprojecti
 getcviprojectiondims(cvi::CVIProjection) = getcviprojectiondims(getcviprojectionessentials(cvi))
 getcviprojectionparameters(cvi::CVIProjection) = getcviprojectionparameters(getcviprojectionessentials(cvi))
 getcvioutsamplesno(cvi::CVIProjection) = getcvioutsamplesno(getcviprojectionoptional(cvi))
-getcvimarginalsamplesno(cvi::CVIProjection) = getcvimarginalsamplesno(getcviprojectionoptional(cvi))
+getcvimarginalsamplesno(t::T, cvi::CVIProjection) where {T} = getcvimarginalsamplesno(t,getcviprojectionoptional(cvi))
 getcvirng(cvi::CVIProjection) = getcvirng(getcviprojectionoptional(cvi))
 getcviinitialsamples(cvi::CVIProjection) = getcviinitialsamples(getcviprojectionessentials(cvi))
 getcviinitialnaturalparameters(cvi::CVIProjection) = getcviinitialnaturalparameters(getcviprojectionessentials(cvi))
@@ -42,14 +44,13 @@ getcviinitialnaturalparameters(cvipe::CVIProjectionEssentials) = cvipe.initial_n
 
 Base.@kwdef struct CVIProjectionOptional{OS, MS, R} 
     out_samples_no::OS = 1000
-    marginal_samples_no::MS = 1000
+    marginal_samples_no::MS = (1000, 5)
     rng::R = Random.MersenneTwister(42)
 end
 getcvioutsamplesno(cvipo::CVIProjectionOptional) = cvipo.out_samples_no
-getcvimarginalsamplesno(cvipo::CVIProjectionOptional) = cvipo.marginal_samples_no
+getcvimarginalsamplesno(::HMCBased, cvipo::CVIProjectionOptional) = first(cvipo.marginal_samples_no)
+getcvimarginalsamplesno(::DropIndicesBased, cvipo::CVIProjectionOptional) = last(cvipo.marginal_samples_no)
 getcvirng(cvipo::CVIProjectionOptional) = cvipo.rng
-
-
 
 struct DivisionOf{A, B}
     numerator::A
@@ -59,13 +60,11 @@ end
 BayesBase.insupport(d::DivisionOf, p) = insupport(d.numerator, p) && insupport(d.denumerator, p)
 BayesBase.logpdf(d::DivisionOf, p) = logpdf(d.numerator, p) - logpdf(d.denumerator, p)
 
-
 # cost function
 function targetfn(M, p, data)
     ef = convert(ExponentialFamilyDistribution, M, p)
     return -mean(logpdf(ef, data))
 end
-
 
 function grad_targetfn(M, p, data)
     ef = convert(ExponentialFamilyDistribution, M, p)
@@ -79,28 +78,22 @@ struct LogTargetDensity{I, F}
     μ   :: F
 end
 
-
 LogDensityProblems.logdensity(p::LogTargetDensity, x) = p.μ(x)
 LogDensityProblems.capabilities(::LogTargetDensity) = LogDensityProblems.LogDensityOrder{1}()
 LogDensityProblems.dimension(p::LogTargetDensity)   = p.dim
 
-
 function log_target_adjusted_log_pdf(::Type{Univariate}, m_in::Union{Distribution, ExponentialFamilyDistribution, DivisionOf}, _)
     return x -> logpdf(m_in, first(x))
 end
-
 function log_target_adjusted_log_pdf(::Type{Multivariate}, m_in::Union{Distribution, ExponentialFamilyDistribution, DivisionOf}, _)
     return x -> logpdf(m_in, x)
 end
-
 function log_target_adjusted_log_pdf(::Type{Matrixvariate}, m_in::Union{Distribution, ExponentialFamilyDistribution, DivisionOf}, dims)
     return x -> logpdf(m_in, reshape(x,dims))
 end
-
 function log_target_adjusted_log_pdf(::Type{Univariate}, logmeasure, _)
     return x -> logmeasure(first(x))
 end
-
 log_target_adjusted_log_pdf(::Type{Multivariate}, logmeasure, _) = logmeasure
 function log_target_adjusted_log_pdf(::Type{Matrixvariate}, logmeasure, dims)
     return x -> logmeasure(reshape(x,dims))
@@ -112,8 +105,8 @@ function hmc_samples(rng, d, log_target_density, initial_x; no_samples = 2_000, 
     initial_ϵ = AdvancedHMC.find_good_stepsize(hamiltonian, initial_x)
     integrator = AdvancedHMC.Leapfrog(initial_ϵ)
     
-    kernel = AdvancedHMC.HMCKernel(Trajectory{MultinomialTS}(integrator, GeneralisedNoUTurn()))
-    adaptor = AdvancedHMC.StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(acceptance_probability, integrator))
+    kernel     = AdvancedHMC.HMCKernel(Trajectory{MultinomialTS}(integrator, GeneralisedNoUTurn()))
+    adaptor    = AdvancedHMC.StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(acceptance_probability, integrator))
     samples, _ = AdvancedHMC.sample(rng, hamiltonian, kernel, initial_x, no_samples+1, adaptor, n_adapts; verbose = false, progress=false)
 
     return samples[2:end]
@@ -127,15 +120,12 @@ vectorized_rand_with_variate_type(::Type{Univariate}, rng, m_in) = [rand(rng, m_
 vectorized_rand_with_variate_type(::Type{Multivariate}, rng, m_in) = rand(rng, m_in)
 vectorized_rand_with_variate_type(::Type{Matrixvariate}, rng, m_in)  = vec(rand(rng, m_in))
 
-
 modify_vectorized_samples_with_variate_type(::Type{Univariate}, samples, _) = map(sample ->first(sample) ,samples)
 modify_vectorized_samples_with_variate_type(::Type{Multivariate}, samples,_) = samples
 modify_vectorized_samples_with_variate_type(::Type{Matrixvariate}, samples,dims) = map(sample -> reshape(sample, dims), samples)
 
 initialize_cvi_samples(method, rng, m_in, k, symbol) = !isnothing(getcviinitialsamples(method)[symbol]) ? getindex(getcviinitialsamples(method)[symbol], k) : rand(rng, m_in) 
 initialize_cvi_natural_parameters(method, rng, manifold, k, symbol) = !isnothing(getcviinitialnaturalparameters(method)[symbol]) ? ExponentialFamilyProjection.partition_point(manifold,getindex(getcviinitialnaturalparameters(method)[symbol], k)) : rand(rng, manifold)
-
-
 
 function BayesBase.prod(::GenericProd, something, division::DivisionOf) 
     return prod(GenericProd(), division, something)
@@ -150,6 +140,49 @@ function BayesBase.prod(::GenericProd, division::DivisionOf, something)
 end
 
 BayesBase.prod(::GenericProd, division_left::DivisionOf, division_right::DivisionOf) = ProductOf(division_left, division_right)
+
+function __auxiliary_variables(rng, m_ins, method, N)
+
+    m_ins_efs = try
+        map(component -> convert(ExponentialFamilyDistribution, component), m_ins)
+    catch
+        nothing
+    end
+
+    if !isnothing(m_ins_efs)
+        var_form_ins           = map(d -> variate_form(typeof(d)), m_ins)
+        dims_in                = map(size, m_ins)
+        prod_dims_in           = map(prod, dims_in)
+        sum_dim_in             = sum(prod_dims_in)
+        cum_lengths            = mapreduce(d -> d+1, vcat, cumsum(prod_dims_in))
+        start_indices          = append!([1], cum_lengths[1:N-1])
+        Ts                     = map(ExponentialFamily.exponential_family_typetag, m_ins_efs)
+        conditioners           = map(getconditioner, m_ins_efs)
+        manifolds              = map((T, conditioner,m_in_ef) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, size(mean(m_in_ef)), conditioner), Ts, conditioners, m_ins_efs)
+        natural_parameters_efs = map((m, p) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.partition_point(m,p) ,manifolds, map(getnaturalparameters, m_ins_efs))
+        initial_sample         = mapreduce((m_in,k) -> initialize_cvi_samples(method, rng, m_in, k, :in),vcat, m_ins, 1:N)
+    else
+        var_form_ins        = map(variate_form, getcviprojectiontypes(method)[:in])
+        dims_in             = getcviprojectiondims(method)[:in]
+        prod_dims_in        = map(prod, dims_in)
+        sum_dim_in          = sum(prod_dims_in)
+        cum_lengths         = mapreduce(d -> d+1, vcat, cumsum(prod_dims_in))
+        start_indices       = append!([1], cum_lengths[1:N-1])
+        conditioners        = getcviprojectionconditioners(method)[:in]
+        Ts                  = getcviprojectiontypes(method)[:in]
+        manifolds = if !isnothing(conditioners)
+                map((T, conditioner, dim) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, dim, conditioner), Ts, conditioners, dims_in)
+            else
+                map((T, dim) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, dim), Ts, dims_in)
+            end
+        natural_parameters_efs = map((manifold,k) -> initialize_cvi_natural_parameters(method, rng, manifold, k, :in), manifolds, 1:N)
+        initial_sample         = rand(rng, sum_dim_in)
+    end
+
+    return var_form_ins, dims_in, sum_dim_in, start_indices, manifolds, natural_parameters_efs, initial_sample
+
+end
+
 
 include("layout/cvi_projection.jl")
 include("rules/in.jl")
--- ext/ReactiveMPProjectionExt/rules/marginals.jl
+++ ext/ReactiveMPProjectionExt/rules/marginals.jl
@@ -3,8 +3,6 @@ using TupleTools, AdvancedHMC, LogDensityProblems
 import Distributions: Distribution
 import BayesBase: AbstractContinuousGenericLogPdf
 
-
-
 @marginalrule DeltaFn(:ins) (m_out::Any, m_ins::ManyOf{1, Any}, meta::DeltaMeta{M}) where {M <: CVIProjection} = begin
     method = ReactiveMP.getmethod(meta)
     g      = getnodefn(meta, Val(:out))
@@ -21,54 +19,30 @@ import BayesBase: AbstractContinuousGenericLogPdf
     return FactorizedJoint((q,))
 end
 
-
+#We would need a mixed HMC method to be able to sample from mixed continuous and discrete 
 @marginalrule DeltaFn(:ins) (m_out::Any, m_ins::ManyOf{N, Any}, meta::DeltaMeta{M}) where {N, M <: CVIProjection} = begin
-    node_function       = getnodefn(meta, Val(:out))
+    nodefunction = getnodefn(meta, Val(:out))
+    marginal = try
+        #hmc based method will fail if there are discrete rvs
+        cvi_compute_in_marginals(HMCBased(), nodefunction, m_out, m_ins, meta, N)
+    catch
+        #drop indices based method will fail if m_ins are not sampleable
+        cvi_compute_in_marginals(DropIndicesBased(),nodefunction, m_out, m_ins, meta, N)
+    end
+    return marginal
+end
+
+function cvi_compute_in_marginals(based::HMCBased,node_function, m_out, m_ins, meta::DeltaMeta{M}, N) where {M <: CVIProjection} 
     method              = ReactiveMP.getmethod(meta)
     rng                 = getcvirng(method)
-    number_marginal_samples  = getcvimarginalsamplesno(method)
+    number_marginal_samples  = getcvimarginalsamplesno(based, method)
 
-    m_ins_efs = try
-        map(component -> convert(ExponentialFamilyDistribution, component), m_ins)
-    catch
-        nothing
-    end
-
-    if !isnothing(m_ins_efs)
-        var_form_ins           = map(d -> variate_form(typeof(d)), m_ins)
-        dims_in                = map(size, m_ins)
-        prod_dims_in           = map(prod, dims_in)
-        sum_dim_in             = sum(prod_dims_in)
-        cum_lengths            = mapreduce(d -> d+1, vcat, cumsum(prod_dims_in))
-        start_indices          = append!([1], cum_lengths[1:N-1])
-        Ts                     = map(ExponentialFamily.exponential_family_typetag, m_ins_efs)
-        conditioners           = map(getconditioner, m_ins_efs)
-        manifolds              = map((T, conditioner,m_in_ef) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, size(mean(m_in_ef)), conditioner), Ts, conditioners, m_ins_efs)
-        natural_parameters_efs = map((m, p) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.partition_point(m,p) ,manifolds, map(getnaturalparameters, m_ins_efs))
-        initial_sample         = mapreduce((m_in,k) -> initialize_cvi_samples(method, rng, m_in, k, :in),vcat, m_ins, 1:N)
-    else
-        var_form_ins        = map(variate_form, getcviprojectiontypes(method)[:in])
-        dims_in             = getcviprojectiondims(method)[:in]
-        prod_dims_in        = map(prod, dims_in)
-        sum_dim_in          = sum(prod_dims_in)
-        cum_lengths         = mapreduce(d -> d+1, vcat, cumsum(prod_dims_in))
-        start_indices       = append!([1], cum_lengths[1:N-1])
-        conditioners        = getcviprojectionconditioners(method)[:in]
-        Ts                  = getcviprojectiontypes(method)[:in]
-        manifolds = if !isnothing(conditioners)
-                map((T, conditioner, dim) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, dim, conditioner), Ts, conditioners, dims_in)
-            else
-                map((T, dim) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, dim), Ts, dims_in)
-            end
-        natural_parameters_efs = map((manifold,k) -> initialize_cvi_natural_parameters(method, rng, manifold, k, :in), manifolds, 1:N)
-        initial_sample         = rand(rng, sum_dim_in)
-    end
-
+    var_form_ins, dims_in, sum_dim_in, start_indices, manifolds, natural_parameters_efs, initial_sample = __auxiliary_variables(rng, m_ins, method, N)
     joint_logpdf = (x) -> logpdf(m_out, node_function(ReactiveMP.__splitjoin(x, dims_in)...)) + mapreduce((m_in,k,T) -> log_target_adjusted_log_pdf(T, m_in, getindex(dims_in, k))(ReactiveMP.__splitjoinelement(x, getindex(start_indices, k), getindex(dims_in, k))), +, m_ins, 1:N, var_form_ins)
     log_target_density  = LogTargetDensity(sum_dim_in, joint_logpdf)
 
     samples            = hmc_samples(rng, sum_dim_in, log_target_density, initial_sample; no_samples = number_marginal_samples + 1)
-    samples_collection     = map(k -> map((sample) -> ReactiveMP.__splitjoinelement(sample, getindex(start_indices,k), getindex(dims_in, k)), samples), 1:N)
+    samples_collection = map(k -> map((sample) -> ReactiveMP.__splitjoinelement(sample, getindex(start_indices,k), getindex(dims_in, k)), samples), 1:N)
     
     function projection_to_ef(i)
         manifold = getindex(manifolds, i)
@@ -93,48 +67,12 @@ end
 
 end
 
-
-@marginalrule DeltaFn(:ins) (m_out::FactorizedJoint, m_ins::ManyOf{N, Any}, meta::DeltaMeta{M}) where {N, M <: CVIProjection} = begin
-    node_function       = getnodefn(meta, Val(:out))
+function cvi_compute_in_marginals(based::HMCBased, node_function, m_out::FactorizedJoint, m_ins, meta::DeltaMeta{M}, N) where {M <: CVIProjection} 
     method              = ReactiveMP.getmethod(meta)
     rng                 = getcvirng(method)
-    number_marginal_samples  = getcvimarginalsamplesno(method)
-
-    m_ins_efs = try
-        map(component -> convert(ExponentialFamilyDistribution, component), m_ins)
-    catch
-        nothing
-    end
-
-    if !isnothing(m_ins_efs)
-        var_form_ins           = map(d -> variate_form(typeof(d)), m_ins)
-        dims_in                = map(size, m_ins)
-        prod_dims_in           = map(prod, dims_in)
-        sum_dim_in             = sum(prod_dims_in)
-        cum_lengths            = mapreduce(d -> d+1, vcat, cumsum(prod_dims_in))
-        start_indices          = append!([1], cum_lengths[1:N-1])
-        Ts                     = map(ExponentialFamily.exponential_family_typetag, m_ins_efs)
-        conditioners           = map(getconditioner, m_ins_efs)
-        manifolds              = map((T, conditioner,m_in_ef) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, size(mean(m_in_ef)), conditioner), Ts, conditioners, m_ins_efs)
-        natural_parameters_efs = map((m, p) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.partition_point(m,p) ,manifolds, map(getnaturalparameters, m_ins_efs))
-        initial_sample         = mapreduce((m_in,k) -> initialize_cvi_samples(method, rng, m_in, k, :in),vcat, m_ins, 1:N)
-    else
-        var_form_ins        = map(variate_form, getcviprojectiontypes(method)[:in])
-        dims_in             = getcviprojectiondims(method)[:in]
-        prod_dims_in        = map(prod, dims_in)
-        sum_dim_in          = sum(prod_dims_in)
-        cum_lengths         = mapreduce(d -> d+1, vcat, cumsum(prod_dims_in))
-        start_indices       = append!([1], cum_lengths[1:N-1])
-        conditioners        = getcviprojectionconditioners(method)[:in]
-        Ts                  = getcviprojectiontypes(method)[:in]
-        manifolds = if !isnothing(conditioners)
-                map((T, conditioner, dim) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, dim, conditioner), Ts, conditioners, dims_in)
-            else
-                map((T, dim) -> ExponentialFamilyProjection.ExponentialFamilyManifolds.get_natural_manifold(T, dim), Ts, dims_in)
-            end
-        natural_parameters_efs = map((manifold,k) -> initialize_cvi_natural_parameters(method, rng, manifold, k, :in), manifolds, 1:N)
-        initial_sample         = rand(rng, sum_dim_in)
-    end
+    number_marginal_samples  = getcvimarginalsamplesno(based, method)
+    var_form_ins, dims_in, sum_dim_in, start_indices, manifolds, natural_parameters_efs, initial_sample = __auxiliary_variables(rng, m_ins, method, N)
+    
     components_out   = components(m_out)
     components_length = length(components_out)
     @assert components_length > 1 "Length of the FactorizedJoint $m_out should be greater than 1."
@@ -142,7 +80,7 @@ end
     prod_dims_components  = map(prod, components_sizes)
     
     cum_lengths_components  = mapreduce(d -> d+1, vcat, cumsum(prod_dims_components))
-    start_indices_components = append!([1], cum_lengths[1:N-1])
+    start_indices_components = append!([1], cum_lengths_components[1:N-1])
 
     joint_logpdf_components = (x) -> mapreduce((component,sz,k) -> logpdf(component, ReactiveMP.__splitjoinelement(x, getindex(start_indices_components,k) ,sz)), +,components_out, components_sizes, 1:components_length)
 
@@ -150,7 +88,7 @@ end
     log_target_density  = LogTargetDensity(sum_dim_in, joint_logpdf)
 
     samples            = hmc_samples(rng, sum_dim_in, log_target_density, initial_sample; no_samples = number_marginal_samples + 1)
-    samples_collection     = map(k -> map((sample) -> ReactiveMP.__splitjoinelement(sample, getindex(start_indices,k), getindex(dims_in, k)), samples), 1:N)
+    samples_collection = map(k -> map((sample) -> ReactiveMP.__splitjoinelement(sample, getindex(start_indices,k), getindex(dims_in, k)), samples), 1:N)
     
     function projection_to_ef(i)
         manifold = getindex(manifolds, i)
@@ -174,5 +112,88 @@ end
     return result
 
 end
+
+
+function cvi_compute_in_marginals(based::DropIndicesBased, nodefunction, m_out, m_ins, meta::DeltaMeta{M}, N) where {M <: CVIProjection} 
+    method = ReactiveMP.getmethod(meta)
+    rng = getcvirng(method)
+    number_marginal_samples  = getcvimarginalsamplesno(based, method)
+    prj  = getcviprojectionparameters(method)
+
+    pre_samples = zip(map(m_in_k -> ReactiveMP.cvilinearize(rand(rng, m_in_k, number_marginal_samples)), m_ins)...)
+
+    logp_nc_drop_index = let g = nodefunction, pre_samples = pre_samples
+        (z, i, pre_samples) -> begin
+            samples = map(ttuple -> ReactiveMP.TupleTools.insertat(ttuple, i, (z,)), pre_samples)
+            t_samples = map(s -> g(s...), samples)
+            logpdfs = map(out -> logpdf(m_out, out), t_samples)
+            return mean(logpdfs)
+        end
+    end
+
+    optimize_natural_parameters = let m_ins = m_ins, logp_nc_drop_index = logp_nc_drop_index
+        (i, pre_samples) -> begin
+            # Create an `AbstractContinuousGenericLogPdf` with an unspecified domain and the transformed `logpdf` function
+            df = let i = i, pre_samples = pre_samples, logp_nc_drop_index = logp_nc_drop_index
+                (z) -> logp_nc_drop_index(z, i, pre_samples)
+            end
+            logp = convert(promote_variate_type(variate_form(typeof(first(m_ins))), BayesBase.AbstractContinuousGenericLogPdf), UnspecifiedDomain(), df)
+
+            T = ExponentialFamily.exponential_family_typetag(m_ins[i])
+            prj = ProjectedTo(T, size(m_ins[i])...; parameters = something(ExponentialFamilyProjection.DefaultProjectionParameters()))
+
+            return project_to(prj, logp, m_ins[i])
+        end
+    end
+
+    return FactorizedJoint(ntuple(i -> optimize_natural_parameters(i, pre_samples), length(m_ins)))
+end
+
+
+function cvi_compute_in_marginals(based::DropIndicesBased,nodefunction,m_out::FactorizedJoint, m_ins, meta::DeltaMeta{M}, N) where {M <: CVIProjection}
+    method              = ReactiveMP.getmethod(meta)
+    rng                 = getcvirng(method)
+    number_marginal_samples  = getcvimarginalsamplesno(based, method)
+
+    components_out   = components(m_out)
+    components_length = length(components_out)
+    @assert components_length > 1 "Length of the FactorizedJoint $m_out should be greater than 1."
+    components_sizes = map(size, components_out)
+    prod_dims_components  = map(prod, components_sizes)
+    joint_logpdf_components = (x) -> mapreduce((component,sz,k) -> logpdf(component, ReactiveMP.__splitjoinelement(x, getindex(start_indices_components,k) ,sz)), +,components_out, components_sizes, 1:components_length)
+    cum_lengths_components  = mapreduce(d -> d+1, vcat, cumsum(prod_dims_components))
+    start_indices_components = append!([1], cum_lengths_components[1:N-1])
+
+    pre_samples = zip(map(m_in_k -> ReactiveMP.cvilinearize(rand(rng, m_in_k, number_marginal_samples)), m_ins)...)
+
+
+    logp_nc_drop_index = let g = nodefunction, pre_samples = pre_samples
+        (z, i, pre_samples) -> begin
+            samples = map(ttuple -> ReactiveMP.TupleTools.insertat(ttuple, i, (z,)), pre_samples)
+            t_samples = map(s -> g(s...), samples)
+            logpdfs = map(out -> joint_logpdf_components(out), t_samples)
+            return mean(logpdfs)
+        end
+    end
+
+    optimize_natural_parameters = let m_ins = m_ins, logp_nc_drop_index = logp_nc_drop_index
+        (i, pre_samples) -> begin
+            # Create an `AbstractContinuousGenericLogPdf` with an unspecified domain and the transformed `logpdf` function
+            df = let i = i, pre_samples = pre_samples, logp_nc_drop_index = logp_nc_drop_index
+                (z) -> logp_nc_drop_index(z, i, pre_samples)
+            end
+            logp = convert(promote_variate_type(variate_form(typeof(first(m_ins))), BayesBase.AbstractContinuousGenericLogPdf), UnspecifiedDomain(), df)
+
+            T = ExponentialFamily.exponential_family_typetag(m_ins[i])
+            prj = ProjectedTo(T, size(m_ins[i])...; parameters = something(ExponentialFamilyProjection.DefaultProjectionParameters()))
+
+            return project_to(prj, logp, m_ins[i])
+        end
+    end
+
+    return FactorizedJoint(ntuple(i -> optimize_natural_parameters(i, pre_samples), length(m_ins)))
+
+end
+
 
 
