367445466f53f06384a234e802599d42b966d9ea
Bagaev Dmitry <bvdmitri@gmail.com>
2024-10-07 10:51:01 +0200

    fix cvi projection after 4.4.2

--- src/message.jl
+++ src/message.jl
@@ -140,7 +140,7 @@ constrain_form_as_message(message::Message, form_constraint) =
     Message(constrain_form(form_constraint, getdata(message)), is_clamped(message), is_initial(message), getaddons(message))
 
 # Note: we need extra Base.Generator(as_message, messages) step here, because some of the messages might be VMP messages
-# We want to cast it explicitly to a Message structure (which as_message does in case of DefferedMessage)
+# We want to cast it explicitly to a Message structure (which as_message does in case of DeferredMessage)
 # We use with Base.Generator to reduce an amount of memory used by this procedure since Generator generates items lazily
 prod_foldl_reduce(prod_constraint, form_constraint, ::FormConstraintCheckEach) =
     (messages) -> foldl((left, right) -> constrain_form_as_message(multiply_messages(prod_constraint, left, right), form_constraint), Base.Generator(as_message, messages))
