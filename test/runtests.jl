9d0f70dff7124dabce426a27cfdb37caca583cfd
Tim <t.n.nisslbeck@tue.nl>
2024-12-18 08:26:06 +0100

    fix #434: determine threads/cores via extra package Hwloc, if CpuId fails.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
-using Aqua, CpuId, ReTestItems, ReactiveMP
+using Aqua, CpuId, ReTestItems, ReactiveMP, Hwloc
 
 Aqua.test_all(ReactiveMP; ambiguities = false, piracies = false, deps_compat = (; check_extras = false, check_weakdeps = true))
 
-nthreads = max(cputhreads(), 1)
-ncores = max(cpucores(), 1)
+nthreads, ncores = cputhreads(), cpucores()
+nthreads = nthreads == 0 ? Hwloc.num_virtual_cores() : nthreads
+ncores = ncores == 0 ? Hwloc.num_physical_cores() : ncores
+nthreads, ncores = max(nthreads, 1), max(ncores, 1)
 
 runtests(ReactiveMP; nworkers = ncores, nworker_threads = Int(nthreads / ncores), memory_threshold = 1.0)
