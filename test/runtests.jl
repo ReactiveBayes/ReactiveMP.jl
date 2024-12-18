bf9f217c0e7b0eb19b5371cd8d2014f6c0c1f686
Tim <t.n.nisslbeck@tue.nl>
2024-12-18 15:41:34 +0100

    #434 remove CpuId dependency, only use Hwloc.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
-using Aqua, CpuId, ReTestItems, ReactiveMP, Hwloc
+using Aqua, ReTestItems, ReactiveMP, Hwloc
 
 Aqua.test_all(ReactiveMP; ambiguities = false, piracies = false, deps_compat = (; check_extras = false, check_weakdeps = true))
 
-nthreads, ncores = cputhreads(), cpucores()
-nthreads = nthreads == 0 ? Hwloc.num_virtual_cores() : nthreads
-ncores = ncores == 0 ? Hwloc.num_physical_cores() : ncores
+nthreads, ncores = Hwloc.num_virtual_cores(), Hwloc.num_physical_cores()
 nthreads, ncores = max(nthreads, 1), max(ncores, 1)
 
 runtests(ReactiveMP; nworkers = ncores, nworker_threads = Int(nthreads / ncores), memory_threshold = 1.0)
