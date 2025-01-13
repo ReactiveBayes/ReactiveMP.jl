33f5e26bcae888982def474770009a9472a267bc
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2025-01-13 14:17:40 +0100

    modification to docstring

--- src/nodes/predefined/binomial_polya.jl
+++ src/nodes/predefined/binomial_polya.jl
@@ -3,9 +3,9 @@ export BinomialPolya, BinomialPolyaMeta
 """
     BinomialPolya
 
-A node type representing a Binomial likelihood with a Normal prior on the parameters. The prior is augmented with a PolyaGamma distribution, 
-which is used for modeling count data with overdispersion. This implementation follows the PolyaGamma augmentation 
-scheme for Bayesian inference. Can be used for Binomial regression. 
+A node type representing a Binomial likelihood with linear predictor through logistic. A Normal prior on the weights is used. 
+The prior is augmented with a PolyaGamma distribution, which is used for modeling count data with overdispersion. 
+This implementation follows the PolyaGamma augmentation scheme for Bayesian inference. Can be used for Binomial regression. 
 """
 struct BinomialPolya end
 
