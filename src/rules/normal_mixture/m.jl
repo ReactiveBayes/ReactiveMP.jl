2c0e22cce963c6a4afb0ebd44d87de2eccfaead3
Wouter Nuijten <wouternuijten@gmail.com>
2025-01-24 13:21:03 +0100

    Improve error msg for gaussian mixture

--- src/rules/normal_mixture/m.jl
+++ src/rules/normal_mixture/m.jl
@@ -1,6 +1,6 @@
 export rule
 
-@rule NormalMixture((:m, k), Marginalisation) (q_out::Any, q_switch::Any, q_p::Any) = begin
+@rule NormalMixture((:m, k), Marginalisation) (q_out::Any, q_switch::Union{Categorical, PointMass{<:Vector}}, q_p::Any) = begin
     pv    = probvec(q_switch)
     T     = eltype(pv)
     z_bar = clamp.(pv, tiny, one(T) - tiny)
@@ -8,4 +8,10 @@ export rule
     F = variate_form(typeof(q_out))
 
     return convert(promote_variate_type(F, NormalMeanPrecision), mean(q_out), z_bar[k] * mean(q_p))
+end
+
+@rule NormalMixture((:m, k), Marginalisation) (q_out::Any, q_switch::PointMass{<:Real}, q_p::Any) = begin
+    error(
+        "Cannot handle switch with Integer values. The switch variable should be a one-hot encoded vector where each element represents the probability of being in that state. Please convert your integer switch values to one-hot encoded vectors before using this rule."
+    )
 end
