335be7584cbfc260d4f4e9477027e61a7e2030cf
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2025-02-04 09:28:49 +0100

    format

--- src/rules/binomial_polya/beta.jl
+++ src/rules/binomial_polya/beta.jl
@@ -1,6 +1,8 @@
 using PolyaGammaHybridSamplers
 
-@rule BinomialPolya(:β, Marginalisation) (q_y::Union{PointMass,Multinomial}, q_x::PointMass, q_n::PointMass, m_β::GaussianDistributionsFamily, meta::Union{BinomialPolyaMeta, Nothing}) = begin
+@rule BinomialPolya(:β, Marginalisation) (
+    q_y::Union{PointMass, Multinomial}, q_x::PointMass, q_n::PointMass, m_β::GaussianDistributionsFamily, meta::Union{BinomialPolyaMeta, Nothing}
+) = begin
     y = mean(q_y)
     x = mean(q_x)
     n = mean(q_n)
