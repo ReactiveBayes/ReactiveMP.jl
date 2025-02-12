da31cb5786dc0db62b8a57e3afe4913ce1324589
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2025-02-12 11:19:52 +0100

    accept any for the x rule

--- src/rules/multinomial_polya/x.jl
+++ src/rules/multinomial_polya/x.jl
@@ -1,5 +1,5 @@
 
-@rule MultinomialPolya(:x, Marginalisation) (q_N::PointMass, q_ψ::GaussianDistributionsFamily, meta::Union{MultinomialPolyaMeta, Nothing}) = begin
+@rule MultinomialPolya(:x, Marginalisation) (q_N::PointMass, q_ψ::Any, meta::Union{MultinomialPolyaMeta, Nothing}) = begin
     N = mean(q_N)
     m_ψ = mean(q_ψ)
     p = logistic_stick_breaking(m_ψ)
