2249d1321e1f3ee3ddd1e1ccda09f4d501bfadd0
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2025-02-20 17:29:33 +0100

    format

--- src/rules/multinomial_polya/x.jl
+++ src/rules/multinomial_polya/x.jl
@@ -1,5 +1,5 @@
 
-@rule MultinomialPolya(:x, Marginalisation) (q_N::Union{PointMass,Poisson, Binomial,Categorical}, q_ψ::Any, meta::Union{MultinomialPolyaMeta, Nothing}) = begin
+@rule MultinomialPolya(:x, Marginalisation) (q_N::Union{PointMass, Poisson, Binomial, Categorical}, q_ψ::Any, meta::Union{MultinomialPolyaMeta, Nothing}) = begin
     N = mode(q_N)
     m_ψ = mean(q_ψ)
     p = logistic_stick_breaking(m_ψ)
