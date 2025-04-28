01e300442825a86492d68f3d7164db1074876f7c
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2025-04-28 12:08:18 +0200

    Revert "Add GenUT extension"

--- ext/ReactiveMPGenUTExt/rules/out.jl
+++
@@ -1,3 +0,0 @@
-@rule DeltaFn(:out, Marginalisation) (m_ins::ManyOf{N, NormalDistributionsFamily}, meta::DeltaMeta{M}) where {N, M <: GenUnscented} = begin
-    return approximate(AsDistribution(NormalDistributionsFamily), ReactiveMP.getmethod(meta), getnodefn(meta, Val(:out)), m_ins)
-end
