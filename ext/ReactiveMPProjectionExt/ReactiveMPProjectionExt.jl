fb8a84dfc0318e3be5c37101928a4bb18ee85adf
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2025-07-14 13:50:12 +0200

    fix: don't check for proper in division of

--- ext/ReactiveMPProjectionExt/ReactiveMPProjectionExt.jl
+++ ext/ReactiveMPProjectionExt/ReactiveMPProjectionExt.jl
@@ -5,14 +5,6 @@ using ReactiveMP, ExponentialFamily, Distributions, ExponentialFamilyProjection,
 struct DivisionOf{A, B}
     numerator::A
     denumerator::B
-
-    function DivisionOf(numerator::A, denumerator::B) where {A, B}
-        if variate_form(A) == variate_form(B)
-            return new{A, B}(numerator, denumerator)
-        else
-            error(lazy"DivisionOf does not support arguments of different variate forms: $(variate_form(A)) and $(variate_form(B))")
-        end
-    end
 end
 
 (divisionof::DivisionOf)(x) = logpdf(divisionof, x)
@@ -33,7 +25,7 @@ function BayesBase.prod(::GenericProd, something, division::DivisionOf)
     return prod(GenericProd(), division, something)
 end
 
-function BayesBase.prod(::GenericProd, division::DivisionOf, something::Any)
+function BayesBase.prod(::GenericProd, division::DivisionOf, something)
     if division.denumerator == something
         return division.numerator
     else
@@ -52,8 +44,6 @@ include("layout/cvi_projection.jl")
 include("rules/in.jl")
 include("rules/out.jl")
 include("rules/marginals.jl")
-include("divisionof/univariate_gaussian.jl")
-include("divisionof/multivariate_gaussian.jl")
 
 # This will enable the extension and make `CVIProjection` compatible with delta nodes 
 # Otherwise it should throw an error suggesting users to install `ExponentialFamilyProjection`
