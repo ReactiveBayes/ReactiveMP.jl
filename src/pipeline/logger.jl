078c92fc2a3f4f9a229aa98efb66973120ad1ef1
Bagaev Dmitry <bvdmitri@gmail.com>
2025-07-24 12:57:05 +0200

    do not use lazy, just string is good enough

--- src/pipeline/logger.jl
+++ src/pipeline/logger.jl
@@ -17,14 +17,13 @@ LoggerPipelineStage()               = LoggerPipelineStage(Core.stdout, "Log")
 LoggerPipelineStage(output::IO)     = LoggerPipelineStage(output, "Log")
 LoggerPipelineStage(prefix::String) = LoggerPipelineStage(Core.stdout, prefix)
 
-logger_pipeline_stage_println(logger::LoggerPipelineStage, something::Any) = logger_pipeline_stage_println(
-    logger, logger.output, logger_pipeline_stage_append_prefix(logger, something)
-)
+logger_pipeline_stage_println(logger::LoggerPipelineStage, something::Any) =
+    logger_pipeline_stage_println(logger, logger.output, logger_pipeline_stage_append_prefix(logger, something))
 
 logger_pipeline_stage_println(logger::LoggerPipelineStage, output::Core.CoreSTDOUT, something) = Core.println(output, something)
 logger_pipeline_stage_println(logger::LoggerPipelineStage, output, something) = println(output, something)
 
-logger_pipeline_stage_append_prefix(logger::LoggerPipelineStage, something) = lazy"[$(logger.prefix)]$something"
+logger_pipeline_stage_append_prefix(logger::LoggerPipelineStage, something) = string("[", logger.prefix, "]: ", something)
 
-apply_pipeline_stage(logger::LoggerPipelineStage, factornode, tag::Val{T}, stream) where {T}             = stream |> tap((v) -> logger_pipeline_stage_println(logger, lazy"[$(functionalform(factornode))][$(T)]: $v"))
-apply_pipeline_stage(logger::LoggerPipelineStage, factornode, tag::Tuple{Val{T}, Int}, stream) where {T} = stream |> tap((v) -> logger_pipeline_stage_println(logger, lazy"[$(functionalform(factornode))][$(T):$(tag[2])]: $v"))
+apply_pipeline_stage(logger::LoggerPipelineStage, factornode, tag::Val{T}, stream) where {T}             = stream |> tap((v) -> logger_pipeline_stage_println(logger, string("[", functionalform(factornode), "][", T, "]: ", v)))
+apply_pipeline_stage(logger::LoggerPipelineStage, factornode, tag::Tuple{Val{T}, Int}, stream) where {T} = stream |> tap((v) -> logger_pipeline_stage_println(logger, string("[", functionalform(factornode), "][", T, ":", tag[2], "]: ", v)))
