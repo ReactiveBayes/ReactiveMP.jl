b476ffbbb0f64082db05c8bab60b554d8b487736
Bagaev Dmitry <bvdmitri@gmail.com>
2025-07-28 13:13:07 +0200

    move some of the functions to a separate package

--- src/nodes/nodes.jl
+++ src/nodes/nodes.jl
@@ -4,159 +4,19 @@ export functionalform, getinterfaces, factorisation, localmarginals, localmargin
 export FactorNode, factornode
 export @node
 
-using Rocket
+using MessagePassingRulesBase
+
+export MessagePassingRulesBase
+
+import MessagePassingRulesBase: PredefinedNodeFunctionalForm, UndefinedNodeFunctionalForm, is_predefined_node
+import MessagePassingRulesBase: Deterministic, Stochastic, isdeterministic, isstochastic, sdtype
+import MessagePassingRulesBase: nodefunction
+import MessagePassingRulesBase: as_node_symbol
+import MessagePassingRulesBase: collect_factorisation, collect_meta, default_meta
+import MessagePassingRulesBase: AbstractFactorNode, functionalform, getinterfaces, getinterface, getinboundinterfaces, getlocalclusters, interfaceindex, interfaceindices
+import MessagePassingRulesBase: interfaces, inputinterfaces, alias_interface
+
 using TupleTools
-using MacroTools
-
-import Rocket: getscheduler
-
-import Base: show, +, push!, iterate, IteratorSize, IteratorEltype, eltype, length, size
-import Base: getindex, setindex!, firstindex, lastindex
-
-## Node traits
-
-"""
-    PredefinedNodeFunctionalForm
-
-Trait specification for an object that has been marked as a valid factor node with the `@node` macro.
-
-See also: [`ReactiveMP.is_predefined_node`](@ref), [`ReactiveMP.UndefinedNodeFunctionalForm`](@ref)
-"""
-struct PredefinedNodeFunctionalForm end
-
-"""
-    UndefinedNodeFunctionalForm
-
-Trait specification for an object that has **not** been marked as a factor node with the `@node` macro.
-Note that it does not necessarily mean that the object is not a valid factor node, but rather that it has not been marked as such.
-The ReactiveMP inference engine support arbitrary deterministic function as factor nodes, but they require manual specification of the approximation method.
-
-See also: [`ReactiveMP.is_predefined_node`](@ref), [`ReactiveMP.PredefinedNodeFunctionalForm`](@ref)
-"""
-struct UndefinedNodeFunctionalForm end
-
-"""
-    is_predefined_node(object)
-
-Determines if the `object` has been marked as a factor node with the `@node` macro.
-Returns either `PredefinedNodeFunctionalForm()` or `UndefinedNodeFunctionalForm()`.
-
-See also: [`ReactiveMP.PredefinedNodeFunctionalForm`](@ref), [`ReactiveMP.UndefinedNodeFunctionalForm`](@ref)
-"""
-function is_predefined_node end
-
-# By default all objects are not marked as nodes
-is_predefined_node(some) = UndefinedNodeFunctionalForm()
-
-## Node types
-
-"""
-    Deterministic
-
-`Deterministic` object used to parametrize factor node object with determinstic type of relationship between variables.
-
-See also: [`Stochastic`](@ref), [`isdeterministic`](@ref), [`isstochastic`](@ref), [`sdtype`](@ref)
-"""
-struct Deterministic end
-
-"""
-    Stochastic
-
-`Stochastic` object used to parametrize factor node object with stochastic type of relationship between variables.
-
-See also: [`Deterministic`](@ref), [`isdeterministic`](@ref), [`isstochastic`](@ref), [`sdtype`](@ref)
-"""
-struct Stochastic end
-
-"""
-    isdeterministic(node)
-
-Function used to check if factor node object is deterministic or not. Returns true or false.
-
-See also: [`Deterministic`](@ref), [`Stochastic`](@ref), [`isstochastic`](@ref), [`sdtype`](@ref)
-"""
-function isdeterministic end
-
-"""
-    isstochastic(node)
-
-Function used to check if factor node object is stochastic or not. Returns true or false.
-
-See also: [`Deterministic`](@ref), [`Stochastic`](@ref), [`isdeterministic`](@ref), [`sdtype`](@ref)
-"""
-function isstochastic end
-
-isdeterministic(::Deterministic)       = true
-isdeterministic(::Type{Deterministic}) = true
-isdeterministic(::Stochastic)          = false
-isdeterministic(::Type{Stochastic})    = false
-
-isstochastic(::Stochastic)          = true
-isstochastic(::Type{Stochastic})    = true
-isstochastic(::Deterministic)       = false
-isstochastic(::Type{Deterministic}) = false
-
-"""
-    nodefunction(::Type{T}) where {T}
-
-Returns a function that represents a node of type `T`. 
-The function typically takes arguments that represent the node's input and output variables in the same order as defined in the `@node` macro.
-"""
-function nodefunction end
-
-"""
-    sdtype(object)
-
-Returns either `Deterministic` or `Stochastic` for a given object (if defined).
-
-See also: [`Deterministic`](@ref), [`Stochastic`](@ref), [`isdeterministic`](@ref), [`isstochastic`](@ref)
-"""
-sdtype(any) = error("Unknown if an object of type `$(typeof(any))` is stochastic or deterministic.")
-
-# Any `Type` is considered to be a deterministic mapping unless stated otherwise (By convention, any `Distribution` type is not deterministic)
-# E.g. `Matrix` is not an instance of the `Function` abstract type, however we would like to pretend it is a deterministic function
-sdtype(::Type{T}) where {T}    = Deterministic()
-sdtype(::Function)             = Deterministic()
-sdtype(::Type{<:Distribution}) = Stochastic()
-sdtype(::Distribution)         = Stochastic()
-
-"""
-    as_node_symbol(type)
-
-Returns a symbol associated with a node `type`.
-"""
-function as_node_symbol end
-
-as_node_symbol(fn::F) where {F <: Function} = Symbol(fn)
-
-"""
-    collect_factorisation(nodetype, factorisation)
-
-This function converts given factorisation to a correct internal factorisation representation for a given node.
-"""
-function collect_factorisation end
-
-"""
-    collect_meta(nodetype, meta)
-
-This function converts given meta object to a correct internal meta representation for a given node.
-Fallbacks to `default_meta` in case if meta is `nothing`.
-
-See also: [`default_meta`](@ref), [`FactorNode`](@ref)
-"""
-function collect_meta end
-
-collect_meta(fform::F, ::Nothing) where {F} = default_meta(fform)
-collect_meta(fform::F, meta::Any) where {F} = meta
-
-"""
-    default_meta(nodetype)
-
-Returns default meta object for a given node type.
-
-See also: [`collect_meta`](@ref), [`FactorNode`](@ref)
-"""
-default_meta(fform) = nothing
 
 struct Marginalisation end
 struct MomentMatching end
@@ -165,10 +25,8 @@ include("interfaces.jl")
 include("clusters.jl")
 include("dependencies.jl")
 
-abstract type AbstractFactorNode end
-
 """
-    GenericFactorNode(functionalform, interfaces)
+    FactorNode(functionalform, interfaces, localclusters)
 
 Generic factor node object that represents a factor node with a given `functionalform` and `interfaces`.
 """
@@ -182,35 +40,35 @@ struct FactorNode{F, I, C} <: AbstractFactorNode
 end
 
 function factornode(fform::F, interfaces::I, factorization) where {F, I}
-    return factornode(is_predefined_node(fform), fform, interfaces, factorization)
+    return factornode(MessagePassingRulesBase.is_predefined_node(fform), fform, interfaces, factorization)
 end
 
 # `PredefinedNodeFunctionalForm` are generally the nodes that are defined with the `@node` macro
 # The `UndefinedNodeFunctionalForm` nodes can be created as well, but only if the `fform` is a `Function` (see `predefined/delta.jl`)
 function factornode(::PredefinedNodeFunctionalForm, fform::F, interfaces::I, factorization) where {F, I}
     processed_interfaces = prepare_interfaces_generic(fform, interfaces)
-    localclusters = FactorNodeLocalClusters(processed_interfaces, collect_factorisation(fform, factorization))
+    localclusters = FactorNodeLocalClusters(processed_interfaces, MessagePassingRulesBase.collect_factorisation(fform, factorization))
     return FactorNode(fform, processed_interfaces, localclusters)
 end
 
-functionalform(factornode::FactorNode) = factornode.fform
-getinterfaces(factornode::FactorNode) = factornode.interfaces
-getinterface(factornode::FactorNode, index) = factornode.interfaces[index]
+MessagePassingRulesBase.functionalform(factornode::FactorNode) = factornode.fform
+MessagePassingRulesBase.getinterfaces(factornode::FactorNode) = factornode.interfaces
+MessagePassingRulesBase.getinterface(factornode::FactorNode, index) = factornode.interfaces[index]
 # `getinboundinterfaces` skips the first interface, which is assumed to be the output interface
-getinboundinterfaces(factornode::FactorNode) = view(factornode.interfaces, (firstindex(factornode.interfaces) + 1):lastindex(factornode.interfaces))
-getlocalclusters(factornode::FactorNode) = factornode.localclusters
-sdtype(factornode::FactorNode) = sdtype(functionalform(factornode))
+MessagePassingRulesBase.getinboundinterfaces(factornode::FactorNode) = view(factornode.interfaces, (firstindex(factornode.interfaces) + 1):lastindex(factornode.interfaces))
+MessagePassingRulesBase.getlocalclusters(factornode::FactorNode) = factornode.localclusters
+MessagePassingRulesBase.sdtype(factornode::FactorNode) = MessagePassingRulesBase.sdtype(MessagePassingRulesBase.functionalform(factornode))
 
-interfaceindex(factornode::FactorNode, iname::Symbol)                         = findfirst(interface -> name(interface) === iname, getinterfaces(factornode))
-interfaceindices(factornode::FactorNode, iname::Symbol)                       = (interfaceindex(factornode, iname),)
-interfaceindices(factornode::FactorNode, inames::NTuple{N, Symbol}) where {N} = map(iname -> interfaceindex(factornode, iname), inames)
+MessagePassingRulesBase.interfaceindex(factornode::FactorNode, iname::Symbol)                         = findfirst(interface -> name(interface) === iname, getinterfaces(factornode))
+MessagePassingRulesBase.interfaceindices(factornode::FactorNode, iname::Symbol)                       = (interfaceindex(factornode, iname),)
+MessagePassingRulesBase.interfaceindices(factornode::FactorNode, inames::NTuple{N, Symbol}) where {N} = map(iname -> interfaceindex(factornode, iname), inames)
 
 function prepare_interfaces_generic(fform::F, interfaces::AbstractVector) where {F}
     prepare_interfaces_check_nonempty(fform, interfaces)
     prepare_interfaces_check_adjacent_duplicates(fform, interfaces)
     prepare_interfaces_check_numarguments(fform, interfaces)
     return map(enumerate(interfaces)) do (index, (name, variable))
-        return NodeInterface(alias_interface(fform, index, name), variable)
+        return NodeInterface(MessagePassingRulesBase.alias_interface(fform, index, name), variable)
     end
 end
 
@@ -236,7 +94,7 @@ function prepare_interfaces_check_adjacent_duplicates(fform, interfaces)
 end
 
 function prepare_interfaces_check_numarguments(fform::F, interfaces) where {F}
-    prepare_interfaces_check_num_inputarguments(fform, inputinterfaces(fform), interfaces)
+    prepare_interfaces_check_num_inputarguments(fform, MessagePassingRulesBase.inputinterfaces(fform), interfaces)
 end
 
 function prepare_interfaces_check_num_inputarguments(fform, inputinterfaces::Val{Input}, interfaces) where {Input}
@@ -264,182 +122,7 @@ getrulefallback(options::FactorNodeActivationOptions) = options.rulefallback
 collect_functional_dependencies(fform::F, options::FactorNodeActivationOptions) where {F} = collect_functional_dependencies(fform, getdependecies(options))
 
 function activate!(factornode::FactorNode, options::FactorNodeActivationOptions)
-    dependencies = collect_functional_dependencies(functionalform(factornode), options)
-    initialize_clusters!(getlocalclusters(factornode), dependencies, factornode, options)
+    dependencies = collect_functional_dependencies(MessagePassingRulesBase.functionalform(factornode), options)
+    initialize_clusters!(MessagePassingRulesBase.getlocalclusters(factornode), dependencies, factornode, options)
     return activate!(dependencies, factornode, options)
-end
-
-import .MacroHelpers
-
-"""
-    interfaces(fform)
-
-Returns a `Val` object with a tuple of interface names for a given factor node type. Returns `nothing` for unknown functional form.
-"""
-interfaces(fform) = nothing
-
-"""
-    inputinterfaces(fform)
-
-Similar to `interfaces`, but returns a `Val` object with a tuple of **input** interface names for a given factor node type. Returns `nothing` for unknown functional form.
-"""
-inputinterfaces(fform) = nothing
-
-"""
-    alias_interface(factor_type, index, name)
-
-Converts the given `name` to a correct interface name for a given factor node type and index.
-"""
-function alias_interface end
-
-node_expression_extract_interface(s::Symbol) = (s, [])
-
-function node_expression_extract_interface(e::Expr)
-    if @capture(e, (s_, aliases = [aliases__]))
-        if !all(alias -> alias isa Symbol, aliases)
-            error(lazy"Aliases should be pure symbols. Got expression in $(aliases).")
-        end
-        return (s, aliases)
-    else
-        error(lazy"Unknown interface specification: $(e)")
-    end
-end
-
-function generate_node_expression(node_fform, node_type, node_interfaces)
-    # Assert that the node type is either Stochastic or Deterministic, and that all interfaces are symbols
-    @assert node_type ∈ [:Stochastic, :Deterministic]
-    @assert length(node_interfaces.args) > 0
-
-    interfaces = map(node_expression_extract_interface, node_interfaces.args)
-
-    # Determine whether we should dispatch on `typeof($fform)` or `Type{$node_fform}`
-    dispatch_type = if @capture(node_fform, typeof(fform_))
-        :(typeof($fform))
-    else
-        :(Type{<:$node_fform})
-    end
-
-    foreach(interfaces) do (name, aliases)
-        @assert !occursin('_', string(name)) "Node interfaces names (and aliases) must not contain `_` symbol in them, found in `$(name)`."
-        foreach(aliases) do alias
-            @assert !occursin('_', string(alias)) "Node interfaces names (and aliases) must not contain `_` symbol in them, found in `$(alias)`."
-        end
-    end
-
-    alias_corrections = Expr(:block)
-    alias_corrections.args = map(enumerate(interfaces)) do (index, (name, aliases))
-        # The `index` and `name` variables are defined further in the `alias_interface` function
-        quote
-            # TODO: (bvdmitri) maybe reserving `in` here is not a good idea, discuss with Wouter
-            if index === $index && (name === :in || name === $(QuoteNode(name)) || Base.in(name, ($(map(QuoteNode, aliases)...),)))
-                return $(QuoteNode(name))
-            end
-        end
-    end
-
-    collect_factorisation_fn = if node_type == :Stochastic
-        :(ReactiveMP.collect_factorisation(::$dispatch_type, factorisation::Tuple) = factorisation)
-    else
-        :(ReactiveMP.collect_factorisation(::$dispatch_type, factorisation::Tuple) = ($(ntuple(identity, length(interfaces))),))
-    end
-
-    doctype   = rpad(dispatch_type, 30)
-    docsdtype = rpad(node_type, 15)
-    docedges  = join(map(((name, aliases),) -> string(name, !isempty(aliases) ? string(" (or ", join(aliases, ", "), ")") : ""), interfaces), ", ")
-    doc       = """    
-        $doctype : $docsdtype : $docedges
-    The `$(node_fform)` has been marked as a valid `$(node_type)` factor node with the `@node` macro with `[ $(docedges) ]` interfaces.
-    """
-
-    # For `Stochastic` nodes the `nodefunctions` are pre-generated automatically 
-    #   by calling the `corresponding` logpdf
-    nodefunctions = if node_type == :Stochastic
-        nodefunctionargnames = first.(interfaces)
-
-        # The very first function is a generic method that only accepts type and returns 
-        # a function that fallbacks to calculate the logpdf of the distribution
-        fncollection = [
-            :(
-                ReactiveMP.nodefunction(::$dispatch_type) =
-                    (; $(nodefunctionargnames...)) -> ReactiveMP.BayesBase.logpdf(($node_fform)($(nodefunctionargnames[2:end]...)), $(nodefunctionargnames[1]))
-            )
-        ]
-
-        # The rest are individual node functions in each direction
-        for interface in interfaces
-            interfacename = first(interface)
-            edgespecificfn = :(
-                ReactiveMP.nodefunction(::$dispatch_type, ::Val{$(QuoteNode(interfacename))}; kwargs...) = begin
-                    return let ckwargs = kwargs
-                        ($interfacename) -> ReactiveMP.nodefunction($node_fform)(; $interfacename = $interfacename, ckwargs...)
-                    end
-                end
-            )
-            push!(fncollection, edgespecificfn)
-        end
-
-        _block = Expr(:block)
-        _block.args = fncollection
-        _block
-    else
-        :(nothing)
-    end
-
-    # Define the necessary function types
-    result = quote
-        @doc $doc ReactiveMP.is_predefined_node(::$dispatch_type) = ReactiveMP.PredefinedNodeFunctionalForm()
-
-        ReactiveMP.sdtype(::$dispatch_type)          = (ReactiveMP.$node_type)()
-        ReactiveMP.interfaces(::$dispatch_type)      = Val($(Tuple(map(first, interfaces))))
-        ReactiveMP.inputinterfaces(::$dispatch_type) = Val($(Tuple(map(first, skipindex(interfaces, 1)))))
-
-        $collect_factorisation_fn
-        $nodefunctions
-
-        function ReactiveMP.alias_interface(dispatch_type::$dispatch_type, index, name)
-            $alias_corrections
-            # If we do not return from the `alias_corrections` we throw an error
-            error(lazy"Don't know how to alias interface $(name) in $(index) for $(dispatch_type)")
-        end
-    end
-
-    return result
-end
-
-"""
-    @node(fformtype, sdtype, interfaces_list)
-
-
-`@node` macro creates a node for a `fformtype` type object. To obtain a list of predefined nodes use `?is_predefined_node`.
-
-# Arguments
-
-- `fformtype`: Either an existing type identifier, e.g. `Normal` or a function type identifier, e.g. `typeof(+)`
-- `sdtype`: Either `Stochastic` or `Deterministic`. Defines the type of the functional relationship
-- `interfaces_list`: Defines a fixed list of edges of a factor node, by convention the first element should be `out`. Example: `[ out, mean, variance ]`
-
-Note: `interfaces_list` must not include names that contain `_` symbol in them, as it is reserved to identify joint posteriors around the node object.
-
-# Examples
-```julia
-
-struct MyNormalDistribution
-    mean :: Float64
-    var  :: Float64
-end
-
-@node MyNormalDistribution Stochastic [ out, mean, var ]
-```
-
-```julia
-
-@node typeof(+) Deterministic [ out, in1, in2 ]
-```
-
-# List of available nodes
-
-See `?is_predefined_node`.
-"""
-macro node(node_fform, node_type, node_interfaces)
-    return esc(generate_node_expression(node_fform, node_type, node_interfaces))
 end
