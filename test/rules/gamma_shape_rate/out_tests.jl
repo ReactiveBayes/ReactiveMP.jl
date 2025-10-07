a741cbc9ec8e2e2ce729357a9a6790f43256f274
Wouter Nuijten <wouternuijten@gmail.com>
2025-10-07 14:18:46 +0200

    Update test cases for `GammaShapeScale`

--- test/rules/gamma_shape_rate/out_tests.jl
+++ test/rules/gamma_shape_rate/out_tests.jl
@@ -4,7 +4,7 @@
 
     import ReactiveMP: @test_rules
 
-    @testset "Belief Propagation: (m_α::Any, m_θ::Any)" begin
+    @testset "Belief Propagation: (m_α::Any, m_β::Any)" begin
         @test_rules [check_type_promotion = true] GammaShapeRate(:out, Marginalisation) [
             (input = (m_α = PointMass(1.0), m_β = PointMass(2.0)), output = GammaShapeRate(1.0, 2.0)),
             (input = (m_α = PointMass(3.0), m_β = PointMass(3.0)), output = GammaShapeRate(3.0, 3.0)),
@@ -15,7 +15,8 @@
     @testset "Variational Message Passing: (q_α::Any, q_β::Any)" begin
         @test_rules [check_type_promotion = true] GammaShapeRate(:out, Marginalisation) [
             (input = (q_α = PointMass(1.0), q_β = PointMass(2.0)), output = GammaShapeRate(1.0, 2.0)),
-            (input = (q_α = GammaShapeScale(1.0, 1.0), q_β = GammaShapeRate(1.0, 1.0)), output = GammaShapeRate(1.0, 1.0))
+            (input = (q_α = GammaShapeScale(1.0, 1.0), q_β = GammaShapeRate(1.0, 1.0)), output = GammaShapeRate(1.0, 1.0)),
+            (input = (q_α = GammaShapeScale(Inf, 1.0), q_β = GammaShapeRate(1.0, Inf)), output = GammaShapeRate(Inf, 0.0))
         ]
     end
 end # testset
