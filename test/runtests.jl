97eb4508d0e9b11035d878ef5be7dc445eaee8d1
Tim <t.n.nisslbeck@tue.nl>
2025-10-11 09:49:59 +0200

    Remove commented out original code.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,6 @@ using Aqua, ReTestItems, ReactiveMP, Hwloc
 if get(ENV, "RUN_AQUA", "true") == "true"
     Aqua.test_all(ReactiveMP; ambiguities = false, piracies = false, deps_compat = (; check_extras = false, check_weakdeps = true))
 end
-#Aqua.test_all(ReactiveMP; ambiguities = false, piracies = false, deps_compat = (; check_extras = false, check_weakdeps = true))
 
 nthreads, ncores = Hwloc.num_virtual_cores(), Hwloc.num_physical_cores()
 nthreads, ncores = max(nthreads, 1), max(ncores, 1)
