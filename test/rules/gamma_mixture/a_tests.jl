abba0a6626857458e1ec5fbb8d3f5f601ff361c3
Tim <t.n.nisslbeck@tue.nl>
2025-10-27 05:14:19 +0100

    make format

--- test/rules/gamma_mixture/a_tests.jl
+++ test/rules/gamma_mixture/a_tests.jl
@@ -11,22 +11,8 @@
         println(typeof(p1), " ", typeof(γ1))
         println(typeof(p2), " ", typeof(γ2))
         @test_rules [check_type_promotion = false] GammaMixture{2}((:a, k = 1), Marginalisation) [
-            (
-                input = (
-                    q_out = GammaShapeRate(2.0, 1.0),
-                    q_switch = Categorical([0.7, 0.3]),
-                    q_b = GammaShapeRate(1.0, 2.0)
-                ),
-                output = GammaShapeLikelihood(p1, γ1)
-            ),
-            (
-                input = (
-                    q_out = GammaShapeRate(3.0, 2.0),
-                    q_switch = Bernoulli(0.5),
-                    q_b = GammaShapeRate(1.0, 3.0)
-                ),
-                output = GammaShapeLikelihood(p2, γ2)
-            )
+            (input = (q_out = GammaShapeRate(2.0, 1.0), q_switch = Categorical([0.7, 0.3]), q_b = GammaShapeRate(1.0, 2.0)), output = GammaShapeLikelihood(p1, γ1)),
+            (input = (q_out = GammaShapeRate(3.0, 2.0), q_switch = Bernoulli(0.5), q_b = GammaShapeRate(1.0, 3.0)), output = GammaShapeLikelihood(p2, γ2))
         ]
     end
 end
