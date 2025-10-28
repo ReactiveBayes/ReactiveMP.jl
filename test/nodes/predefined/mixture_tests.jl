95fb4d3757f7dc0962ee121d7ea9f8835b929d44
Tim <t.n.nisslbeck@tue.nl>
2025-10-28 05:22:31 +0100

    Mixture does not have an @average_energy yet defined in its node

--- test/nodes/predefined/mixture_tests.jl
+++ test/nodes/predefined/mixture_tests.jl
@@ -97,32 +97,3 @@
         @test length(marg) == 1
     end
 end
-
-@testitem "AverageEnergy" skip=true begin
-    using ReactiveMP, BayesBase, ExponentialFamily, Random, Test
-
-    import ReactiveMP: ManyOf, Mixture
-    import ExponentialFamily: NormalMeanVariance, NormalMeanPrecision, GammaShapeRate
-    import BayesBase: score
-
-    @testset "Mixture AverageEnergy" begin
-        q_out = NormalMeanVariance(0.0, 1.0)
-        q_switch = Categorical([0.3, 0.7])
-        q_m = (NormalMeanVariance(1.0, 2.0), NormalMeanVariance(3.0, 4.0))
-        q_p = (GammaShapeRate(2.0, 3.0), GammaShapeRate(4.0, 5.0))
-
-        marginals = (
-            Marginal(q_out, false, false, nothing),
-            Marginal(q_switch, false, false, nothing),
-            ManyOf(map(q_m_ -> Marginal(q_m_, false, false, nothing), q_m)),
-            ManyOf(map(q_p_ -> Marginal(q_p_, false, false, nothing), q_p))
-        )
-
-        z = probvec(q_switch)
-        ref_val =
-            z[1] * score(AverageEnergy(), NormalMeanPrecision, Val{(:out, :μ, :τ)}(), map((q) -> Marginal(q, false, false, nothing), (q_out, q_m[1], q_p[1])), nothing) +
-            z[2] * score(AverageEnergy(), NormalMeanPrecision, Val{(:out, :μ, :τ)}(), map((q) -> Marginal(q, false, false, nothing), (q_out, q_m[2], q_p[2])), nothing)
-
-        @test score(AverageEnergy(), Mixture, Val{(:out, :switch, :inputs)}(), marginals, nothing) ≈ ref_val
-    end
-end
