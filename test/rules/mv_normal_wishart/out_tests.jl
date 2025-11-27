e028206fcd13adc54d4cf4c3f289b65a53e487bc
Esther van Pelt <esthervanp@gmail.com>
2025-11-27 13:35:02 +0100

    make format

--- test/rules/mv_normal_wishart/out_tests.jl
+++ test/rules/mv_normal_wishart/out_tests.jl
@@ -5,8 +5,10 @@
     import ReactiveMP: @test_rules
 
     @testset "Variational Message Passing: (q_μ::PointMass, q_λ::PointMass, q_W::PointMass, q_ν::PointMass)" begin
-        @test_rules [check_type_promotion = false] MvNormalWishart(:out, Marginalisation) [ # Type promotion is false because λ and ν will also be promoted but not necessarily promote μ and W.
-            (input = (q_μ = PointMass([1.0, 2.0]), q_W = PointMass([1.0 0.0;0.0 1.0]), q_λ = PointMass(1.0), q_ν = PointMass(1.0)), output = MvNormalWishart([1.0, 2.0], [1.0 0.0;0.0 1.0], 1.0, 1.0)),
-        ]
+        # Type promotion is false because λ and ν will also be promoted but not necessarily promote μ and W.
+        @test_rules [check_type_promotion = false] MvNormalWishart(:out, Marginalisation) [(
+            input = (q_μ = PointMass([1.0, 2.0]), q_W = PointMass([1.0 0.0; 0.0 1.0]), q_λ = PointMass(1.0), q_ν = PointMass(1.0)),
+            output = MvNormalWishart([1.0, 2.0], [1.0 0.0; 0.0 1.0], 1.0, 1.0)
+        ),]
     end
 end # testset
