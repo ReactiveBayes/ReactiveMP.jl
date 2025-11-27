b065df4909a6bb99eff788b53a17968a5307dd1e
Bagaev Dmitry <bvdmitri@gmail.com>
2025-11-27 14:29:01 +0100

    remove Requires all together (used for Julia <1.9, we don't support this anymore)

--- ext/ReactiveMPRequiresExt/ReactiveMPRequiresExt.jl
+++
@@ -1,4 +0,0 @@
-module ReactiveMPRequiresExt
-# This file is supposed to be empty and is only used for backward compatibility with Julia < 1.9
-# Requires.jl is used on older versions of julia for extensions, but Julia 1.9 uses native extensions feature
-end
